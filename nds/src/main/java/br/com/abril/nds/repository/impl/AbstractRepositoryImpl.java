92e8451597fb6c5cf62a8f5c0b2faacbbe1f6b18
chicaogarcia <chicaogarcia@gmail.com>
2012-01-31 15:44:11 -0200

    Commit inicial
    Change-Id: Ib26c1afe6373f8a47d7de9c66792262e666a6002

---
+++ nds/src/main/java/br/com/abril/nds/repository/impl/AbstractRepositoryImpl.java
@@ -0,0 +1,16 @@
+package br.com.abril.nds.repository.impl;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public abstract class AbstractRepositoryImpl {
+
+	//@Autowired
+	protected SessionFactory sf;
+
+	public Session getCurrentSession() {
+		return sf.getCurrentSession();
+	}
+	
+}
---
+++ nds/src/main/java/br/com/abril/nds/view/controller/IndexController.java
@@ -0,0 +1,14 @@
+package br.com.abril.nds.view.controller;
+
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Resource;
+
+@Resource
+public class IndexController {
+	
+	@Path("/")
+	public void index() {
+		
+	}
+
+}
