8f97a19d9c8fbc4d35851d0408571562ab750ed8
foxlol <foxlol@gmail.com>
2012-02-16 16:27:43 -0200

    Implementação de carregamento de combos

--- nds-client/src/main/java/br/com/abril/nds/controllers/estoque/DiferencaEstoqueController.java
+++ nds-client/src/main/java/br/com/abril/nds/controllers/estoque/DiferencaEstoqueController.java
@@ -1,5 +1,11 @@
 package br.com.abril.nds.controllers.estoque;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import br.com.abril.nds.dto.ItemDTO;
+import br.com.abril.nds.model.estoque.TipoDiferenca;
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Resource;
@@ -12,19 +18,58 @@ public class DiferencaEstoqueController {
 	private Result result;
 	
 	public DiferencaEstoqueController(Result result) {
+		
 		this.result = result;
 	}
 	
 	@Get
 	public void lancamento() {
 		
+		this.carregarCombos();
 	}
 	
 	@Get
 	@Path("/lancamento/pesquisa")
-	public void pesquisarLancamentos() {
-	
+	public void pesquisarLancamentos(Date dataMovimento, String tipoDiferenca) {
+		
+		
+		
 		result.forwardTo(DiferencaEstoqueController.class).lancamento();
+	}
+	
+	/**
+	 * Método responsável por carregar todos os combos.
+	 */
+	private void carregarCombos() {
+		
+		this.carregarComboTiposDiferenca();
+	}
+	
+	/**
+	 * Método responsável por carregar o combo de tipos de diferença.
+	 */
+	private void carregarComboTiposDiferenca() {
+
+		List<ItemDTO<TipoDiferenca, String>> listaTiposDiferenca =
+			new ArrayList<ItemDTO<TipoDiferenca, String>>();
+		
+		listaTiposDiferenca.add(
+			new ItemDTO<TipoDiferenca, String>(TipoDiferenca.FALTA_DE, TipoDiferenca.FALTA_DE.getDescricao())
+		);
+		
+		listaTiposDiferenca.add(
+			new ItemDTO<TipoDiferenca, String>(TipoDiferenca.FALTA_EM, TipoDiferenca.FALTA_EM.getDescricao())
+		);
+		
+		listaTiposDiferenca.add(
+			new ItemDTO<TipoDiferenca, String>(TipoDiferenca.SOBRA_DE, TipoDiferenca.SOBRA_DE.getDescricao())
+		);
+		
+		listaTiposDiferenca.add(
+			new ItemDTO<TipoDiferenca, String>(TipoDiferenca.SOBRA_EM, TipoDiferenca.SOBRA_EM.getDescricao())
+		);
+		
+		result.include("listaTiposDiferenca", listaTiposDiferenca);
 	}
 	
 }
