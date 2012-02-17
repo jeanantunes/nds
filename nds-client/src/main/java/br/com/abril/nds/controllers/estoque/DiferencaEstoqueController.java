3609c383ff3c8b70fcae65d5f75461a8953bfd11
murilolocatelli <murilolocatelli@gmail.com>
2012-02-17 11:56:29 -0200

    EMS 0082

--- nds-client/src/main/java/br/com/abril/nds/controllers/estoque/DiferencaEstoqueController.java
+++ nds-client/src/main/java/br/com/abril/nds/controllers/estoque/DiferencaEstoqueController.java
@@ -4,8 +4,12 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import br.com.abril.nds.dto.ItemDTO;
+import br.com.abril.nds.model.cadastro.Fornecedor;
 import br.com.abril.nds.model.estoque.TipoDiferenca;
+import br.com.abril.nds.service.FornecedorService;
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Resource;
@@ -16,6 +20,9 @@ import br.com.caelum.vraptor.Result;
 public class DiferencaEstoqueController {
 
 	private Result result;
+	
+	@Autowired
+	private FornecedorService fornecedorService;
 
 	public DiferencaEstoqueController(Result result) {
 		
@@ -25,7 +32,7 @@ public class DiferencaEstoqueController {
 	@Get
 	public void lancamento() {
 		
-		this.carregarCombos();
+		this.carregarCombosLancamento();
 	}
 	
 	@Get
@@ -37,13 +44,21 @@ public class DiferencaEstoqueController {
 	
 	@Get
 	public void consulta() {
-		
+		this.carregarCombosConsulta();
 	}
 
 	/**
-	 * Método responsável por carregar todos os combos.
+	 * Método responsável por carregar todos os combos da tela de consulta.
 	 */
-	private void carregarCombos() {
+	public void carregarCombosConsulta() {
+		this.carregarComboTiposDiferenca();
+		this.carregarComboFornecedores();
+	}
+	
+	/**
+	 * Método responsável por carregar todos os combos da tela de lançamento.
+	 */
+	private void carregarCombosLancamento() {
 		
 		this.carregarComboTiposDiferenca();
 	}
@@ -73,6 +88,25 @@ public class DiferencaEstoqueController {
 		);
 		
 		result.include("listaTiposDiferenca", listaTiposDiferenca);
+	}
+	
+	/**
+	 * Método responsável por carregar o combo de fornecedores.
+	 */
+	private void carregarComboFornecedores() {
+
+		List<Fornecedor> listaFornecedor = fornecedorService.obterFornecedores();
+		
+		List<ItemDTO<Fornecedor, String>> listaFornecedoresCombo =
+			new ArrayList<ItemDTO<Fornecedor,String>>();
+		
+		for (Fornecedor fornecedor : listaFornecedor) {
+			listaFornecedoresCombo.add(
+				new ItemDTO<Fornecedor, String>(fornecedor, fornecedor.getJuridica().getNomeFantasia())
+			);
+		}
+		
+		result.include("listaFornecedores", listaFornecedoresCombo);
 	}
 	
 }
