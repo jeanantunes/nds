c38078b643c63526ddfc30e88629127d45a3077b
murilolocatelli <murilolocatelli@gmail.com>
2012-02-22 15:58:07 -0200

    EMS 0082

--- nds-client/src/main/java/br/com/abril/nds/repository/ProdutoRepository.java
+++ nds-client/src/main/java/br/com/abril/nds/repository/ProdutoRepository.java
@@ -7,4 +7,6 @@ import br.com.abril.nds.model.cadastro.Produto;
 public interface ProdutoRepository extends Repository<Produto, Long> {
 
 	List<Produto> obterProdutoPorNomeProduto(String nome);
+	
+	Produto obterProdutoPorCodigo(String codigoProduto);
 }
