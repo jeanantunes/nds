b82f6de24b13c8239cec98e567dfa4e87f8e47ca
michel.jader <fiateternan@gmail.com>
2012-02-23 10:54:43 -0200

    DataLoader com - Massa de dados para testes MovimentoEstoque Refactor TableModel

--- nds-model/src/main/java/br/com/abril/nds/model/fiscal/NotaFiscalFornecedor.java
+++ nds-model/src/main/java/br/com/abril/nds/model/fiscal/NotaFiscalFornecedor.java
@@ -1,4 +1,6 @@
 package br.com.abril.nds.model.fiscal;
+import javax.persistence.Entity;
+
 import br.com.abril.nds.model.cadastro.Fornecedor;
 
 /**
@@ -6,6 +8,7 @@ import br.com.abril.nds.model.cadastro.Fornecedor;
  * @version 1.0
  * @created 14-fev-2012 11:35:32
  */
+@Entity
 public class NotaFiscalFornecedor extends NotaFiscal {
 
 	public Fornecedor fornecedor;
