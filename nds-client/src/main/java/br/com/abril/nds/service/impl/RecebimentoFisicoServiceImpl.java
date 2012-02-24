7221402591afd594abcc8a68a4b3774d9d1d5331
luismaschietto <gumaschietto@hotmail.com>
2012-02-24 08:39:46 -0200

    RecebimentoFisicoServiceImpl.java

--- nds-client/src/main/java/br/com/abril/nds/service/impl/RecebimentoFisicoServiceImpl.java
+++ nds-client/src/main/java/br/com/abril/nds/service/impl/RecebimentoFisicoServiceImpl.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import br.com.abril.nds.dto.RecebimentoFisicoDTO;
+import br.com.abril.nds.model.estoque.ItemRecebimentoFisico;
 import br.com.abril.nds.model.estoque.RecebimentoFisico;
 import br.com.abril.nds.repository.RecebimentoFisicoRepository;
 import br.com.abril.nds.service.RecebimentoFisicoService;
@@ -15,14 +17,19 @@ public class RecebimentoFisicoServiceImpl implements RecebimentoFisicoService {
 
 	@Autowired
 	private RecebimentoFisicoRepository recebimentoFisicoRepository;
-	
 
-	@Transactional
-	public List<RecebimentoFisico> obterRecebimentoFisico(){
-		return recebimentoFisicoRepository.obterRecebimentoFisico();
-	}
+	
 	@Transactional
 	public void adicionarRecebimentoFisico(RecebimentoFisico recebimentoFisico){
+		
+	}
+	@Transactional
+	public List<RecebimentoFisicoDTO> obterRecebimentoFisico(String cnpj, String numeroNota, String serieNota ){
+		return recebimentoFisicoRepository.obterRecebimentoFisico(cnpj, numeroNota, serieNota);
+	}
+	@Transactional
+	 public void alterarOrSalvarDiferencaRecebimentoFisico(List<RecebimentoFisicoDTO> listaRecebimentoFisicoDTO,
+				ItemRecebimentoFisico itemRecebimentoFisico){
 		
 	}
 	
