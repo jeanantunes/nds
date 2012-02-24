ff481b63aa69aae978c6da707e2377553b3df6c6
luismaschietto <gumaschietto@hotmail.com>
2012-02-24 08:38:02 -0200

    Alteracao no NotaFiscalService.java

--- nds-client/src/main/java/br/com/abril/nds/service/NotaFiscalService.java
+++ nds-client/src/main/java/br/com/abril/nds/service/NotaFiscalService.java
@@ -1,12 +1,8 @@
 package br.com.abril.nds.service;
 
-import java.util.Date;
-import java.util.List;
-
 import br.com.abril.nds.model.fiscal.NotaFiscal;
 
 public interface NotaFiscalService {
-
-	List<NotaFiscal> buscarNotaFiscal(Date emissao, Date entradaRecebimento,Long valorBruto, Long valorLiquido, Long valorDescontado, String tipoNota, String cfop);
+	
 	void inserirNotaFiscal(NotaFiscal notaFiscal);
 }
