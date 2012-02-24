5d2fa95ecc207ff27350cae21a35b05cd1572cd0
luismaschietto <gumaschietto@hotmail.com>
2012-02-24 08:39:17 -0200

    Alteracao em NotaFiscalServiceImpl.java

--- nds-client/src/main/java/br/com/abril/nds/service/impl/NotaFiscalServiceImpl.java
+++ nds-client/src/main/java/br/com/abril/nds/service/impl/NotaFiscalServiceImpl.java
@@ -1,29 +1,18 @@
 package br.com.abril.nds.service.impl;
 
-import java.util.Date;
-import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.com.abril.nds.model.fiscal.NotaFiscal;
-import br.com.abril.nds.repository.NotaFiscalRepository;
 import br.com.abril.nds.service.NotaFiscalService;
 
 @Service
 public class NotaFiscalServiceImpl implements NotaFiscalService {
 
-	@Autowired
-	private NotaFiscalRepository notaFiscalRepository;
-	
 		
-	@Transactional
-	public List<NotaFiscal> buscarNotaFiscal(Date emissao, Date entradaRecebimento,Long valorBruto, Long valorLiquido, Long valorDescontado, String tipoNota, String cfop) {
-		return notaFiscalRepository.buscarNotaFiscal(emissao, entradaRecebimento, valorBruto, valorLiquido, valorDescontado, tipoNota, cfop);
-				
-	}
-	
 	@Transactional
 	public void inserirNotaFiscal(NotaFiscal notaFiscal){
 		
