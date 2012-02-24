77b4404b400fb2d5981e061fd5606531921bf3e8
luismaschietto <gumaschietto@hotmail.com>
2012-02-24 08:33:49 -0200

    Mudanças no RecebimentoFisicoRepository.java

--- nds-client/src/main/java/br/com/abril/nds/repository/RecebimentoFisicoRepository.java
+++ nds-client/src/main/java/br/com/abril/nds/repository/RecebimentoFisicoRepository.java
@@ -2,9 +2,13 @@ package br.com.abril.nds.repository;
 
 import java.util.List;
 
+import br.com.abril.nds.dto.RecebimentoFisicoDTO;
+import br.com.abril.nds.model.estoque.ItemRecebimentoFisico;
 import br.com.abril.nds.model.estoque.RecebimentoFisico;
 
 public interface RecebimentoFisicoRepository extends Repository<RecebimentoFisico, Long> {
-	List<RecebimentoFisico> obterRecebimentoFisico();
-	void adicionarRecebimentoFisico(RecebimentoFisico recebimentoFisico);
+	
+	List<RecebimentoFisicoDTO> obterRecebimentoFisico(String cnpj, String numeroNota, String serieNota );
+	 void alterarOrSalvarDiferencaRecebimentoFisico(List<RecebimentoFisicoDTO> listaRecebimentoFisicoDTO,
+				ItemRecebimentoFisico itemRecebimentoFisico);
 }
