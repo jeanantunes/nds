acff8f91913a349cf3b542775ccd7ae5dd8b4f2d
luismaschietto <gumaschietto@hotmail.com>
2012-02-24 08:38:21 -0200

    modificacao RecebimentoFisicoService.java

--- nds-client/src/main/java/br/com/abril/nds/service/RecebimentoFisicoService.java
+++ nds-client/src/main/java/br/com/abril/nds/service/RecebimentoFisicoService.java
@@ -2,12 +2,14 @@ package br.com.abril.nds.service;
 
 import java.util.List;
 
+import br.com.abril.nds.dto.RecebimentoFisicoDTO;
+import br.com.abril.nds.model.estoque.ItemRecebimentoFisico;
 import br.com.abril.nds.model.estoque.RecebimentoFisico;
 
 public interface RecebimentoFisicoService {
-
-
 	
-	List<RecebimentoFisico> obterRecebimentoFisico(); 
 	void adicionarRecebimentoFisico(RecebimentoFisico recebimentoFisico);
+	List<RecebimentoFisicoDTO> obterRecebimentoFisico(String cnpj, String numeroNota, String serieNota );
+	 void alterarOrSalvarDiferencaRecebimentoFisico(List<RecebimentoFisicoDTO> listaRecebimentoFisicoDTO,
+				ItemRecebimentoFisico itemRecebimentoFisico);
 }
