d97d9ee7b0ed93d30adda2e32f579752fd636bd3
luismaschietto <gumaschietto@hotmail.com>
2012-02-25 08:29:27 -0200

    PessoaService.java modificado

--- nds-client/src/main/java/br/com/abril/nds/service/PessoaService.java
+++ nds-client/src/main/java/br/com/abril/nds/service/PessoaService.java
@@ -2,10 +2,10 @@ package br.com.abril.nds.service;
 
 import java.util.List;
 
-import br.com.abril.nds.model.cadastro.Pessoa;
+import br.com.abril.nds.model.cadastro.PessoaJuridica;
 
 public interface PessoaService {
 
-	List<Pessoa> buscaPorCnpj(String cnpj);
+	List<PessoaJuridica> buscarPorCnpj(String cnpj);
 	
 }
