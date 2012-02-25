542bd8e275a6963469e1dcda070fe644a22abc9d
luismaschietto <gumaschietto@hotmail.com>
2012-02-25 08:28:03 -0200

    Mudanca em PessoaRepository.java e criacao de PessoaJuridicaRepository.java

---
+++ nds-client/src/main/java/br/com/abril/nds/repository/PessoaJuridicaRepository.java
@@ -0,0 +1,9 @@
+package br.com.abril.nds.repository;
+
+import br.com.abril.nds.model.cadastro.PessoaJuridica;
+
+public interface PessoaJuridicaRepository extends Repository<PessoaJuridica, Long> {	
+	
+	PessoaJuridica buscarPorCnpj(String cnpj);
+
+}
--- nds-client/src/main/java/br/com/abril/nds/repository/PessoaRepository.java
+++ nds-client/src/main/java/br/com/abril/nds/repository/PessoaRepository.java
@@ -3,11 +3,12 @@ package br.com.abril.nds.repository;
 import java.util.List;
 
 import br.com.abril.nds.model.cadastro.Pessoa;
+import br.com.abril.nds.model.cadastro.PessoaJuridica;
 
 public interface PessoaRepository extends Repository<Pessoa, Long> {
 	
 	List<Pessoa> buscarPorNome(String nome);
 	
-	List<Pessoa> buscarPorCnpj(String cnpj);
+	List<PessoaJuridica> buscarPorCnpj(String cnpj);
 
 }
