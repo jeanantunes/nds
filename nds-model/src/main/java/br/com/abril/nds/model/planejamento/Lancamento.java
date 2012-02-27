5f29510397b007e93da2931d6af8fa7114e33366
chicaogarcia <chicaogarcia@gmail.com>
2012-02-27 09:40:53 -0300

    Incrementando modelo

--- nds-model/src/main/java/br/com/abril/nds/model/planejamento/Lancamento.java
+++ nds-model/src/main/java/br/com/abril/nds/model/planejamento/Lancamento.java
@@ -12,12 +12,14 @@ import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.persistence.UniqueConstraint;
 
 import br.com.abril.nds.model.cadastro.ProdutoEdicao;
 import br.com.abril.nds.model.estoque.ItemRecebimentoFisico;
@@ -28,8 +30,9 @@ import br.com.abril.nds.model.estoque.ItemRecebimentoFisico;
  * @created 14-fev-2012 11:35:32
  */
 @Entity
-@Table(name = "LANCAMENTO")
-@SequenceGenerator(name="LANCAMENTO_SEQ", initialValue = 1, allocationSize = 1)
+@Table(name = "LANCAMENTO", uniqueConstraints = { @UniqueConstraint(columnNames = {
+		"DATA_LCTO_PREVISTA", "PRODUTO_EDICAO_ID" })})
+@SequenceGenerator(name = "LANCAMENTO_SEQ", initialValue = 1, allocationSize = 1)
 public class Lancamento {
 
 	@Id
@@ -52,6 +55,7 @@ public class Lancamento {
 	@Column(name = "DATA_REC_DISTRIB", nullable = false)
 	private Date dataRecolhimentoDistribuidor;
 	@ManyToOne(optional = false)
+	@JoinColumn(name = "PRODUTO_EDICAO_ID")
 	private ProdutoEdicao produtoEdicao;
 	@Enumerated(EnumType.STRING)
 	@Column(name = "TIPO_LANCAMENTO", nullable = false)
