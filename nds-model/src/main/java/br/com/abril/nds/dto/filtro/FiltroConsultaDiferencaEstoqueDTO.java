f5718f5087a63d9df0d845ea5ed57f3cd9e47856
foxlol <foxlol@gmail.com>
2012-02-27 10:58:46 -0300

    Criação de DTO's de filtro

---
+++ nds-model/src/main/java/br/com/abril/nds/dto/filtro/FiltroConsultaDiferencaEstoqueDTO.java
@@ -0,0 +1,18 @@
+package br.com.abril.nds.dto.filtro;
+
+import java.io.Serializable;
+
+/**
+ * Data Transfer Object para filtro de pesquisa de diferenças de estoque.
+ * 
+ * @author Discover Technology
+ *
+ */
+public class FiltroConsultaDiferencaEstoqueDTO implements Serializable {
+
+	/**
+	 * Serial Version UID
+	 */
+	private static final long serialVersionUID = -2032477830142408298L;
+
+}
