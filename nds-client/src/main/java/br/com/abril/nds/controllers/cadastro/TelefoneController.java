9ff1f09a27e0ae27f88269fcd73aa78c8c51b63f
francisco-maximo <francisco-maximo@gmail.com>
2012-03-09 14:17:54 -0300

    TelefoneController adicionado.

---
+++ nds-client/src/main/java/br/com/abril/nds/controllers/cadastro/TelefoneController.java
@@ -0,0 +1,20 @@
+package br.com.abril.nds.controllers.cadastro;
+
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Resource;
+
+@Resource
+@Path("/cadastro/telefone")
+public class TelefoneController {
+
+	public static final String LISTA_TELEFONES_SALVAR_SESSAO = "listaTelefonesSalvarSessao";
+	
+	public static final String LISTA_TELEFONES_REMOVER_SESSAO = "listaTelefonesRemoverSessao";
+	
+	@Path("/")
+	public void index(){}
+	
+	public void pesquisarTelefones(){
+		//TODO: chamar service para carregar telefones
+	}
+}
