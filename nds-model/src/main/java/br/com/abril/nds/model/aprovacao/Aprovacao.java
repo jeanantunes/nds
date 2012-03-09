72bf339bb763fc093fb4730f07e54107f355b07c
6uimorais <6uimorais@gmail.com>
2012-03-09 09:47:07 -0300

    Alteração - Aprovacao.motivo=nullable

--- nds-model/src/main/java/br/com/abril/nds/model/aprovacao/Aprovacao.java
+++ nds-model/src/main/java/br/com/abril/nds/model/aprovacao/Aprovacao.java
@@ -28,9 +28,9 @@ public abstract class Aprovacao {
 	@Column(name = "DATA_APROVACAO")
 	private Date dataAprovacao;
 	
-	@Column(name = "APROVADO_AUTOMATICAMENTE", nullable = false)
+	@Column(name = "APROVADO_AUTOMATICAMENTE", nullable = true)
 	private boolean aprovadoAutomaticamente;
-	@Column(name = "MOTIVO", nullable = false)
+	@Column(name = "MOTIVO", nullable = true)
 	private String motivo;
 	
 	public Usuario getAprovador() {
