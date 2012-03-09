41f91b8ba8ad4bdbb47541d9a1bad655846001b1
william-balan <william.balan.machado@gmail.com>
2012-03-09 11:58:29 -0300

    EnderecoController

--- nds-client/src/main/java/br/com/abril/nds/controllers/cadastro/EnderecoController.java
+++ nds-client/src/main/java/br/com/abril/nds/controllers/cadastro/EnderecoController.java
@@ -7,10 +7,14 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import br.com.abril.nds.controllers.exception.ValidacaoException;
+import br.com.abril.nds.dto.EnderecoAssociacaoDTO;
 import br.com.abril.nds.model.cadastro.Endereco;
 import br.com.abril.nds.model.cadastro.TipoEndereco;
+import br.com.abril.nds.service.EnderecoService;
 import br.com.abril.nds.util.CellModel;
 import br.com.abril.nds.util.TableModel;
+import br.com.abril.nds.util.TipoMensagem;
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.Resource;
@@ -33,6 +37,9 @@ public class EnderecoController {
 
 	@Autowired
 	private HttpSession session;
+	
+	@Autowired
+	private EnderecoService enderecoService;
 
 	@Path("/")
 	public void index() { }
@@ -42,52 +49,142 @@ public class EnderecoController {
 	 * armazenado na sessão para serem persistidos na base. 
 	 */
 	private static final String LISTA_ENDERECOS_SALVAR_SESSAO = "listaEnderecosSalvarSessao";
-	
+
 	/**
 	 * Constante que representa o nome do atributo com a lista de endereços 
-	 * armazenado na sessão para serem removidos da base. 
+	 * armazenado na sessão para serem persistidos na base. 
 	 */
 	private static final String LISTA_ENDERECOS_REMOVER_SESSAO = "listaEnderecosRemoverSessao";
-
+	
 	/**
 	 * Método que realiza a pesquisa dos endereços cadastrados para uma determinada pessoa.
 	 */
 	@Post
+	@SuppressWarnings("unchecked")
 	public void pesquisarEnderecos() {
 
-		TableModel<CellModel> tableModelEndereco = getTableModelListaEndereco(getListaEndereco());
+		List<EnderecoAssociacaoDTO> listaEndereco =
+				(List<EnderecoAssociacaoDTO>) this.session.getAttribute(LISTA_ENDERECOS_SALVAR_SESSAO);
 
-		session.setAttribute(LISTA_ENDERECOS_SALVAR_SESSAO, tableModelEndereco);
+		TableModel<CellModel> tableModelEndereco = new TableModel<CellModel>();
+		
+		if (listaEndereco == null) {
 
-		result.use(Results.json()).withoutRoot().from(tableModelEndereco).recursive().serialize();
+			listaEndereco = new ArrayList<EnderecoAssociacaoDTO>();
+
+		} else {
+
+			tableModelEndereco = getTableModelListaEndereco(listaEndereco);
+		}
+
+		this.session.setAttribute(LISTA_ENDERECOS_SALVAR_SESSAO, listaEndereco);
+
+		this.result.use(Results.json()).withoutRoot().from(tableModelEndereco).recursive().serialize();
 	}
 
 	/**
 	 * Método responsável por incluir um novo endereço para a pessoa em questão.
 	 * 
-	 * @param endereco
-	 * @param isEnderecoPrincipal
+	 * @param enderecoAssociacao
+	 */
+	@SuppressWarnings("unchecked")
+	public void incluirNovoEndereco(EnderecoAssociacaoDTO enderecoAssociacao) {
+
+		List<EnderecoAssociacaoDTO> listaEnderecoAssociacao =
+				(List<EnderecoAssociacaoDTO>) this.session.getAttribute(LISTA_ENDERECOS_SALVAR_SESSAO);
+
+		if (listaEnderecoAssociacao == null) {
+
+			listaEnderecoAssociacao = new ArrayList<EnderecoAssociacaoDTO>();		
+		
+		} else if (enderecoAssociacao.isEnderecoPrincipal()) {
+
+			validarExistenciaEnderecoPrincipal(listaEnderecoAssociacao);
+		}
+
+		listaEnderecoAssociacao.add(enderecoAssociacao);
+
+		TableModel<CellModel> tableModelEndereco = getTableModelListaEndereco(listaEnderecoAssociacao);
+
+		this.session.setAttribute(LISTA_ENDERECOS_SALVAR_SESSAO, listaEnderecoAssociacao);
+
+		this.result.use(Results.json()).from(tableModelEndereco, "result").recursive().serialize();
+	}
+
+	/**
+	 * Método que irá remover um endereço a partir de seu ID. 
+	 * 
+	 * @param idEndereco
+	 */
+	@SuppressWarnings("unchecked")
+	public void removerEndereco(Long idEndereco) {
+
+		Endereco endereco = enderecoService.obterEnderecoPorId(idEndereco);
+
+		if (endereco == null) {
+
+			endereco = new Endereco();
+
+			endereco.setId(idEndereco);
+
+		} else {
+
+			List<Endereco> listaEnderecosRemover = 
+				(List<Endereco>) this.session.getAttribute(LISTA_ENDERECOS_REMOVER_SESSAO);
+
+			if (listaEnderecosRemover == null) {
+
+				listaEnderecosRemover = new ArrayList<Endereco>();
+			}
+
+			listaEnderecosRemover.add(endereco);
+
+			this.session.setAttribute(LISTA_ENDERECOS_REMOVER_SESSAO, listaEnderecosRemover);
+		}
+
+		EnderecoAssociacaoDTO enderecoAssociacao = new EnderecoAssociacaoDTO();
+		
+		enderecoAssociacao.setEndereco(endereco);
+		
+		List<EnderecoAssociacaoDTO> listaEnderecoAssociacao =
+				(List<EnderecoAssociacaoDTO>) this.session.getAttribute(LISTA_ENDERECOS_SALVAR_SESSAO);
+
+		listaEnderecoAssociacao.remove(enderecoAssociacao);
+
+		TableModel<CellModel> tableModelEndereco = getTableModelListaEndereco(listaEnderecoAssociacao);
+
+		this.session.setAttribute(LISTA_ENDERECOS_SALVAR_SESSAO, listaEnderecoAssociacao);
+
+		this.result.use(Results.json()).from(tableModelEndereco, "result").recursive().serialize();
+	}
+
+	/**
+	 * Método responsável por preparar um endereço para a edição.
+	 * 
+	 * @param idEndereco
 	 */
 	@Post
 	@SuppressWarnings("unchecked")
-	public void incluirNovoEndereco(Endereco endereco, boolean isEnderecoPrincipal) {
-		
-		TableModel<CellModel> tableModelEndereco = 
-				(TableModel<CellModel>) session.getAttribute(LISTA_ENDERECOS_SALVAR_SESSAO);
+	public void editarEndereco(Long idEndereco) {		
 
-		CellModel cellModel = getCellModelEndereco(endereco);
+		Endereco endereco = new Endereco();
 
-		tableModelEndereco.getRows().add(cellModel);
+		endereco.setId(idEndereco);
 
-		session.setAttribute(LISTA_ENDERECOS_SALVAR_SESSAO, tableModelEndereco);
+		EnderecoAssociacaoDTO enderecoAssociacao = new EnderecoAssociacaoDTO();
 
-		result.use(Results.json()).withoutRoot().from(tableModelEndereco).recursive().serialize();
+		enderecoAssociacao.setEndereco(endereco);
+
+		List<EnderecoAssociacaoDTO> listaEndereco =
+				(List<EnderecoAssociacaoDTO>) this.session.getAttribute(LISTA_ENDERECOS_SALVAR_SESSAO);
+
+		int index = listaEndereco.indexOf(enderecoAssociacao);
+
+		enderecoAssociacao = listaEndereco.get(index);
+
+		this.result.use(Results.json()).from(enderecoAssociacao, "result").recursive().serialize();
 	}
 
-	@Post
-	public void removerEndereco(Long idEndereco) {
-		
-	}
 	
 	/**
 	 * Método que retorna um Table Model de acordo com a lista de Endereços desejada.
@@ -95,14 +192,18 @@ public class EnderecoController {
 	 * @param listaEndereco
 	 * @return TableModel<CellModel>
 	 */
-	private TableModel<CellModel> getTableModelListaEndereco(List<Endereco> listaEndereco) {
+	private TableModel<CellModel> getTableModelListaEndereco(List<EnderecoAssociacaoDTO> listaEnderecoAssociacao) {
 
 		TableModel<CellModel> tableModel = new TableModel<CellModel>();
 
 		List<CellModel> listaCellModel = new ArrayList<CellModel>();
 
-		for (Endereco endereco : listaEndereco) {
-			
+		long idCellModel = 0;
+		
+		for (EnderecoAssociacaoDTO endereco : listaEnderecoAssociacao) {
+
+			endereco.getEndereco().setId(++idCellModel);
+
 			CellModel cellModel = getCellModelEndereco(endereco);
 
 			listaCellModel.add(cellModel);
@@ -121,19 +222,35 @@ public class EnderecoController {
 	 * @param endereco
 	 * @return CellModel
 	 */
-	private CellModel getCellModelEndereco(Endereco endereco) {
+	private CellModel getCellModelEndereco(EnderecoAssociacaoDTO enderecoAssociacao) {
 
 		return new CellModel(
-					  endereco.getId().intValue(),
-					  endereco.getTipoEndereco().toString(),
-					  endereco.getLogradouro(), 
-					  endereco.getBairro(),
-					  endereco.getCep(), 
-					  endereco.getCidade(), 
-					  "true"
-				   );
+				enderecoAssociacao.getEndereco().getId().intValue(),
+				enderecoAssociacao.getTipoEndereco().toString(),
+				enderecoAssociacao.getEndereco().getLogradouro(), 
+				enderecoAssociacao.getEndereco().getBairro(),
+				enderecoAssociacao.getEndereco().getCep(), 
+				enderecoAssociacao.getEndereco().getCidade(), 
+				String.valueOf(enderecoAssociacao.isEnderecoPrincipal())
+			);
 	}
-	
+
+	/**
+	 * Método que realiza a verificação de endereço principal na lista corrente. 
+	 * 
+	 * @param listaEnderecoAssociacao
+	 */
+	private void validarExistenciaEnderecoPrincipal(List<EnderecoAssociacaoDTO> listaEnderecoAssociacao) {
+
+		for (EnderecoAssociacaoDTO enderecoAssociacao : listaEnderecoAssociacao) {
+
+			if (enderecoAssociacao.isEnderecoPrincipal()) {
+
+				throw new ValidacaoException(TipoMensagem.ERROR, "Já existe um endereço principal.");
+			}
+		}
+	}
+
 	/**
 	 * 
 	 * 
