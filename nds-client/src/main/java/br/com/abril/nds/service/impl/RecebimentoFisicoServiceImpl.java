d44be939b7c24d8426f11585685fbb8f79164cac
luismaschietto <gumaschietto@hotmail.com>
2012-03-14 11:37:42 -0300

    Modificacao para funcao de cancelamento de Recebimento Fisico

--- nds-client/src/main/java/br/com/abril/nds/service/impl/RecebimentoFisicoServiceImpl.java
+++ nds-client/src/main/java/br/com/abril/nds/service/impl/RecebimentoFisicoServiceImpl.java
@@ -150,6 +150,8 @@ public class RecebimentoFisicoServiceImpl implements RecebimentoFisicoService {
 						}
 					}				
 					recebimentoFisicoRepository.remover(recebimentoFisico);
+				}else{
+					throw new ValidacaoException(TipoMensagem.WARNING, "O Recebimento ja foi confirmado, não é possível alterar os dados do mesmo.");
 				}
 					
 			}
