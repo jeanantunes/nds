8882f25a4d58e9adf6449a1fb6b80377996e4737
michel.jader <fiateternan@gmail.com>
2012-03-15 11:43:14 -0300

    Contrucao backend EMS 0104.

--- nds-client/src/main/java/br/com/abril/nds/repository/MovimentoEstoqueCotaRepository.java
+++ nds-client/src/main/java/br/com/abril/nds/repository/MovimentoEstoqueCotaRepository.java
@@ -1,8 +1,15 @@
 package br.com.abril.nds.repository;
 
+import java.util.Date;
+import java.util.List;
+
+import br.com.abril.nds.dto.ContagemDevolucaoDTO;
 import br.com.abril.nds.model.estoque.MovimentoEstoqueCota;
 
 
 public interface MovimentoEstoqueCotaRepository extends Repository<MovimentoEstoqueCota, Long>{
 
+	public List<ContagemDevolucaoDTO> obterListaContagemDevolucao(Date dataOperacao, List<Long> listaIdProdutoDosFornecedores);
+
+	
 }
