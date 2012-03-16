95ddc536124994f1db67150779dc2673eb606ba2
francisco-maximo <francisco-maximo@gmail.com>
2012-03-16 10:02:31 -0300

    EMS 160.

--- nds-client/src/main/java/br/com/abril/nds/repository/impl/TelefoneFornecedorRepositoryImpl.java
+++ nds-client/src/main/java/br/com/abril/nds/repository/impl/TelefoneFornecedorRepositoryImpl.java
@@ -33,6 +33,8 @@ public class TelefoneFornecedorRepositoryImpl extends AbstractRepository<Telefon
 			hql.append(" and t.telefone.id not in (:idsIgnorar) ");
 		}
 		
+		hql.append(" order by t.tipoTelefone ");
+		
 		Query query = this.getSession().createQuery(hql.toString());
 		query.setParameter("idFornecedor", idFornecedor);
 		
