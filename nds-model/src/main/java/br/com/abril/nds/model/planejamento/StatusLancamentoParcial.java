c8def62eed4fd7b8630c7a87829ba3bb2dfebbb4
chicaogarcia <chicaogarcia@gmail.com>
2012-04-16 16:39:37 -0300

    Evoluindo modelo, lancamento parcial

---
+++ nds-model/src/main/java/br/com/abril/nds/model/planejamento/StatusLancamentoParcial.java
@@ -0,0 +1,14 @@
+package br.com.abril.nds.model.planejamento;
+
+/**
+ * Estados possíveis de um lancamento parcial
+ * 
+ * @author francisco.garcia
+ *
+ */
+public enum StatusLancamentoParcial {
+		
+	PROJETADO, 
+	RECOLHIDO;
+
+}
