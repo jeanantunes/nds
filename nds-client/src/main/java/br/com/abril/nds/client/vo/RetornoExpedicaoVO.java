97255d73893be29b9806edf9141bdc2a26144ed2
leandrolupianhez <leandro.lupianhez@gmail.com>
2012-04-19 08:41:25 -0300

    Correção de Resumo Expedição

---
+++ nds-client/src/main/java/br/com/abril/nds/client/vo/RetornoExpedicaoVO.java
@@ -0,0 +1,74 @@
+package br.com.abril.nds.client.vo;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class RetornoExpedicaoVO {
+	
+	private List<ResumoExpedicaoVO> resumosExpedicao;
+	
+	private List<ResumoExpedicaoBoxVO> resumosExpedicaoBox;
+	
+	private BigDecimal totalReparte;
+	
+	private BigDecimal totalValorFaturado;
+
+	/**
+	 * @return the resumosExpedicao
+	 */
+	public List<ResumoExpedicaoVO> getResumosExpedicao() {
+		return resumosExpedicao;
+	}
+
+	/**
+	 * @param resumosExpedicao the resumosExpedicao to set
+	 */
+	public void setResumosExpedicao(List<ResumoExpedicaoVO> resumosExpedicao) {
+		this.resumosExpedicao = resumosExpedicao;
+	}
+
+	/**
+	 * @return the totalReparte
+	 */
+	public BigDecimal getTotalReparte() {
+		return totalReparte;
+	}
+
+	/**
+	 * @param totalReparte the totalReparte to set
+	 */
+	public void setTotalReparte(BigDecimal totalReparte) {
+		this.totalReparte = totalReparte;
+	}
+
+	/**
+	 * @return the totalValorFaturado
+	 */
+	public BigDecimal getTotalValorFaturado() {
+		return totalValorFaturado;
+	}
+
+	/**
+	 * @param totalValorFaturado the totalValorFaturado to set
+	 */
+	public void setTotalValorFaturado(BigDecimal totalValorFaturado) {
+		this.totalValorFaturado = totalValorFaturado;
+	}
+
+	/**
+	 * @return the resumosExpedicaoBox
+	 */
+	public List<ResumoExpedicaoBoxVO> getResumosExpedicaoBox() {
+		return resumosExpedicaoBox;
+	}
+
+	/**
+	 * @param resumosExpedicaoBox the resumosExpedicaoBox to set
+	 */
+	public void setResumosExpedicaoBox(
+			List<ResumoExpedicaoBoxVO> resumosExpedicaoBox) {
+		this.resumosExpedicaoBox = resumosExpedicaoBox;
+	}
+	
+	
+}
