d7a90bb9e188e0ff35722d102f7d7c523de7c5f5
chicaogarcia <chicaogarcia@gmail.com>
2012-04-25 11:11:50 -0300

    Evolucao modelo cadastro PDV

---
+++ nds-model/src/main/java/br/com/abril/nds/model/cadastro/TipoPontoPDV.java
@@ -0,0 +1,31 @@
+package br.com.abril.nds.model.cadastro;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TIPO_PONTO_PDV")
+@SequenceGenerator(name="TIPO_PONTO_PDV_SEQ", initialValue = 1, allocationSize = 1)
+public class TipoPontoPDV extends CodigoDescricao {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@Column(name = "ID")
+	@GeneratedValue(generator = "TIPO_PONTO_PDV_SEQ")
+	private Long id; 
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	
+}
