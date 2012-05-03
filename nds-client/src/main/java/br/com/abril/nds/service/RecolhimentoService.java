bf4d7a4ed643b8a220031005021726f69a2e003c
foxlol <foxlol@gmail.com>
2012-05-03 13:27:29 -0300

    EMS 0089

--- nds-client/src/main/java/br/com/abril/nds/service/RecolhimentoService.java
+++ nds-client/src/main/java/br/com/abril/nds/service/RecolhimentoService.java
@@ -1,11 +1,8 @@
 package br.com.abril.nds.service;
 
-import java.util.Date;
 import java.util.List;
 
 import br.com.abril.nds.dto.BalanceamentoRecolhimentoDTO;
-import br.com.abril.nds.dto.RecolhimentoDTO;
-import br.com.abril.nds.dto.ResumoPeriodoBalanceamentoDTO;
 import br.com.abril.nds.util.TipoBalanceamentoRecolhimento;
 
 /**
@@ -17,24 +14,14 @@ import br.com.abril.nds.util.TipoBalanceamentoRecolhimento;
 public interface RecolhimentoService {
 	
 	/**
-	 * Obtém os dados do resumo do período do balanceamento.
+	 * Obtém a matriz de balanceamento de recolhimento.
 	 * 
-	 * @param dataInicial - data inicial do período
-	 * @param listaIdsFornecedores - lista com os id's dos fornecedores
+	 * @param numeroSemana - número da semana para balanceamento
+	 * @param listaIdsFornecedores - lista de id's dos fornecedores
+	 * @param tipoBalanceamentoRecolhimento - tipo de balanceamento de recolhimento
 	 * 
-	 * @return {@link ResumoPeriodoBalanceamentoDTO}
+	 * @return {@link BalanceamentoRecolhimentoDTO}
 	 */
-	List<ResumoPeriodoBalanceamentoDTO> obterResumoPeriodoBalanceamento(Date dataInicial, List<Long> listaIdsFornecedores);
-	
-	/**
-	 * Obtém os dados do balanceamento de recolhimento.
-	 * 
-	 * @param dataInicial - data inicial do período
-	 * 
-	 * @return {@link RecolhimentoDTO}
-	 */
-	List<RecolhimentoDTO> obterDadosBalanceamentoRecolhimento(Date dataInicial);
-	
 	BalanceamentoRecolhimentoDTO obterMatrizBalanceamento(Integer numeroSemana,
 														  List<Long> listaIdsFornecedores,
 														  TipoBalanceamentoRecolhimento tipoBalanceamentoRecolhimento);
