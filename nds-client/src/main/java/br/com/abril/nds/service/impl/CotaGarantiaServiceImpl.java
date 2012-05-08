0da20dd36735f66a466e71eefdace8faf8292a1d
Diego Fernandes <diego.fernandes@discover.com.br>
2012-05-08 16:22:48 -0300

    EMS 0170 - Cadastro de Cota - Garantias - OK

--- nds-client/src/main/java/br/com/abril/nds/service/impl/CotaGarantiaServiceImpl.java
+++ nds-client/src/main/java/br/com/abril/nds/service/impl/CotaGarantiaServiceImpl.java
@@ -1,6 +1,7 @@
 package br.com.abril.nds.service.impl;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,10 +9,12 @@ import org.springframework.transaction.annotation.Transactional;
 
 import br.com.abril.nds.model.cadastro.Cota;
 import br.com.abril.nds.model.cadastro.NotaPromissoria;
+import br.com.abril.nds.model.cadastro.TipoGarantia;
 import br.com.abril.nds.model.cadastro.garantia.CotaGarantia;
 import br.com.abril.nds.model.cadastro.garantia.CotaGarantiaNotaPromissoria;
 import br.com.abril.nds.repository.CotaGarantiaRepository;
 import br.com.abril.nds.repository.CotaRepository;
+import br.com.abril.nds.repository.DistribuidorRepository;
 import br.com.abril.nds.service.CotaGarantiaService;
 import br.com.abril.nds.service.exception.RelationshipRestrictionException;
 
@@ -28,6 +31,10 @@ public class CotaGarantiaServiceImpl implements CotaGarantiaService {
 	
 	@Autowired
 	private CotaRepository cotaRepository;
+	
+	
+	@Autowired
+	private DistribuidorRepository distribuidorRepository;
 
 	/*
 	 * (non-Javadoc)
@@ -84,5 +91,18 @@ public class CotaGarantiaServiceImpl implements CotaGarantiaService {
 		
 		return (CotaGarantiaNotaPromissoria) cotaGarantiaRepository.merge(cotaGarantiaNota);
 	}
+
+	/**
+	 * @return
+	 * @see br.com.abril.nds.repository.DistribuidorRepository#obtemTiposGarantiasAceitas()
+	 */
+	@Override
+	@Transactional(readOnly = true)
+	public List<TipoGarantia> obtemTiposGarantiasAceitas() {
+		return distribuidorRepository.obtemTiposGarantiasAceitas();
+	}
+	
+	
+	
 
 }
