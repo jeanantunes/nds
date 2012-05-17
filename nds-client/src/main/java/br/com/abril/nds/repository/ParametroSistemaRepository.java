f7240962df7be96907ebcc1bf127bf7f24c396ff
Alexandre Takashi <alexandre.tamanaha@discover.com.br>
2012-05-17 18:23:40 -0300

    EMS 173: Inclusao do metodo de busca que tras todos os Parametros do Sistema a serem atualizados.

--- nds-client/src/main/java/br/com/abril/nds/repository/ParametroSistemaRepository.java
+++ nds-client/src/main/java/br/com/abril/nds/repository/ParametroSistemaRepository.java
@@ -1,9 +1,19 @@
 package br.com.abril.nds.repository;
 
+import java.util.List;
+
 import br.com.abril.nds.model.cadastro.ParametroSistema;
 import br.com.abril.nds.model.cadastro.TipoParametroSistema;
 
 public interface ParametroSistemaRepository extends Repository<ParametroSistema, Long>{
 
 	ParametroSistema buscarParametroPorTipoParametro(TipoParametroSistema tipoParametroSistema);
+	
+	/**
+	 * Busca os parâmetros gerais do sistema.
+	 * 
+	 * @return Lista dos parâmetros do sistema que são considerados gerais. 
+	 */
+	public List<ParametroSistema> buscarParametroSistemaGeral();
+	
 }
--- nds-client/src/main/java/br/com/abril/nds/repository/impl/ParametroSistemaRepositoryImpl.java
+++ nds-client/src/main/java/br/com/abril/nds/repository/impl/ParametroSistemaRepositoryImpl.java
@@ -1,5 +1,8 @@
 package br.com.abril.nds.repository.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.hibernate.Query;
 import org.springframework.stereotype.Repository;
 
@@ -25,4 +28,45 @@ public class ParametroSistemaRepositoryImpl extends AbstractRepository<Parametro
 		
 		return (ParametroSistema) query.uniqueResult();
 	}
+	
+	/**
+	 * Busca os parâmetros gerais do sistema.
+	 * 
+	 * @return Lista dos parâmetros do sistema que são considerados gerais. 
+	 */
+	@SuppressWarnings("unchecked")
+	public List<ParametroSistema> buscarParametroSistemaGeral() {
+		
+		String hql = "from ParametroSistema p where p.tipoParametroSistema in (:listaTipoParametroSistema) ";
+		
+		List<TipoParametroSistema> lst = new ArrayList<TipoParametroSistema>();
+		lst.add(TipoParametroSistema.PATH_LOGO_SISTEMA);
+		lst.add(TipoParametroSistema.CNPJ);
+		lst.add(TipoParametroSistema.RAZAO_SOCIAL);
+		lst.add(TipoParametroSistema.EMAIL_USUARIO);
+		lst.add(TipoParametroSistema.UF);
+		lst.add(TipoParametroSistema.CODIGO_DISTRIBUIDOR_DINAP);
+		lst.add(TipoParametroSistema.CODIGO_DISTRIBUIDOR_FC);
+		lst.add(TipoParametroSistema.LOGIN_DISTRIBUIDOR);
+		lst.add(TipoParametroSistema.SENHA_DISTRIBUIDOR);
+		lst.add(TipoParametroSistema.VERSAO_SISTEMA);
+		lst.add(TipoParametroSistema.PATH_INTERFACE_CE_EXPORTACAO);
+		lst.add(TipoParametroSistema.PATH_INTERFACE_PRODIN_IMPORTCAO);
+		lst.add(TipoParametroSistema.PATH_INTERFACE_PRODIN_EXPORTACAO);
+		lst.add(TipoParametroSistema.PATH_INTERFACE_MDC_IMPORTCAO);
+		lst.add(TipoParametroSistema.PATH_INTERFACE_MDC_EXPORTACAO);
+		lst.add(TipoParametroSistema.PATH_INTERFACE_BANCAS_EXPORTACAO);
+		lst.add(TipoParametroSistema.PATH_INTERFACE_GFS_IMPORTCAO);
+		lst.add(TipoParametroSistema.PATH_INTERFACE_GFS_EXPORTACAO);
+		lst.add(TipoParametroSistema.PATH_INTERFACE_NFE_IMPORTCAO);
+		lst.add(TipoParametroSistema.PATH_INTERFACE_NFE_EXPORTACAO);
+		lst.add(TipoParametroSistema.NFE_DPEC);
+		lst.add(TipoParametroSistema.DATA_OPERACAO_CORRENTE);
+		
+		Query query = this.getSession().createQuery(hql);
+		query.setParameter("listaTipoParametroSistema", lst);
+		
+		return query.list();
+	}
+	
 }
