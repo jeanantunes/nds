7b4b2cb8c17ced8368d2f48a4916a842b258a1cd
Roger Maedo <rogermaedo@hotmail.com>
2012-05-18 18:10:37 -0300

    Implementação relatório de vendas 18/05/2012

--- nds-client/src/main/java/br/com/abril/nds/service/CotaService.java
+++ nds-client/src/main/java/br/com/abril/nds/service/CotaService.java
@@ -22,180 +22,180 @@ import br.com.abril.nds.model.seguranca.Usuario;
 
 /**
  * Interface que define serviços referentes a entidade
- * {@link br.com.abril.nds.model.cadastro.Cota}  
- * 
+ * {@link br.com.abril.nds.model.cadastro.Cota}
+ *
  * @author Discover Technology
  *
  */
 public interface CotaService {
 
 	/**
-	 * 
+	 *
 	 * Retorna uma lista de Cotas em função dos filtros informado.
-	 * 
+	 *
 	 * @param filtro - filtro com as opções de consulta
-	 * 
+	 *
 	 * @return List<CotaDTO>
 	 */
 	List<CotaDTO> obterCotas(FiltroCotaDTO filtro);
-	
+
 	/**
-	 * 
+	 *
 	 * Retorna a quantidade de Cotas em função dos filtros informado.
-	 * 
+	 *
 	 * @param filtro - filtro com as opções de consulta
-	 * 
+	 *
 	 * @return Long
 	 */
 	Long obterQuantidadeCotasPesquisadas(FiltroCotaDTO filtro);
-	
+
 	Cota obterPorNumeroDaCota(Integer numeroCota);
 
 	List<Cota> obterCotasPorNomePessoa(String nome);
 
 	Cota obterPorNome(String nome);
-	
+
 	Cota obterPorId(Long idCota);
-	
+
 	/**
 	 * Obtém uma lista dos endereços cadastrados para uma determinada cota.
-	 * 
+	 *
 	 * @param idCota - Id da cota.
-	 * 
-	 * @return List<EnderecoAssociacaoDTO> 
+	 *
+	 * @return List<EnderecoAssociacaoDTO>
 	 */
 	List<EnderecoAssociacaoDTO> obterEnderecosPorIdCota(Long idCota);
 	/**
 	 * Persiste um conjunto de endereços para uma determinada cota.
-	 * 
+	 *
 	 * @param cota
-	 * 
+	 *
 	 * @param listaEnderecoAssociacaoSalvar
-	 * 
+	 *
 	 * @param listaEnderecoAssociacaoRemover
 	 */
-	void processarEnderecos(Long idCota, 
-							List<EnderecoAssociacaoDTO> listaEnderecoAssociacaoSalvar, 
+	void processarEnderecos(Long idCota,
+							List<EnderecoAssociacaoDTO> listaEnderecoAssociacaoSalvar,
 							List<EnderecoAssociacaoDTO> listaEnderecoAssociacaoRemover);
-	
-	void processarTelefones(Long idCota, 
-			List<TelefoneAssociacaoDTO> listaTelefonesAdicionar, 
+
+	void processarTelefones(Long idCota,
+			List<TelefoneAssociacaoDTO> listaTelefonesAdicionar,
 			Collection<Long> listaTelefonesRemover);
-	
+
 	/**
 	 * Obtém cobranças não pagas em nome da cota
-	 * 
+	 *
 	 * @param idCota -  Código da cota
 	 * @return Lista de Cobranças
 	 */
 	List<Cobranca> obterCobrancasDaCotaEmAberto(Long idCota);
-	
+
 	/**
 	 * Suspende lista de cotas
-	 * 
+	 *
 	 * @param idCotas - código das cotas a serem suspensas
 	 * @param idUsuario - identificador do usuário
 	 * @param motivoAlteracaoSituacao - motivo de alteração da situação
-	 * 
+	 *
 	 * @return Lista de Cotas suspensas
 	 */
 	List<Cota>suspenderCotas(List<Long> idCotas, Long idUsuario, MotivoAlteracaoSituacao motivoAlteracaoSituacao);
-	
+
 	/**
 	 * Suspende cota
-	 * 
-	 * 
+	 *
+	 *
 	 * @param idCota - Código da cota a ser suspensa
 	 * @param usuario - usuário
 	 * @param motivoAlteracaoSituacao - motivo de alteração da situação
-	 * 
+	 *
 	 * @return Cota - cota suspensa
 	 */
 	Cota suspenderCota(Long idCota, Usuario usuario, MotivoAlteracaoSituacao motivoAlteracaoSituacao);
 
 	/**
-	 * Suspende lista de cotas e retorna dados básicos das cota suspensas com 
+	 * Suspende lista de cotas e retorna dados básicos das cota suspensas com
 	 * necessidade de devolução de contrato
-	 * 
+	 *
 	 * @param idCotas - Códigos das cotas
 	 * @param usuario - usuário
 	 * @return
 	 */
 	List<CotaSuspensaoDTO> suspenderCotasGetDTO(List<Long> idCotas, Long idUsuario);
-	
+
 	/**
-	 * Obtém sugestão de cotas a serem suspensas com base 
-	 * @param rp 
-	 * @param page 
-	 * 
+	 * Obtém sugestão de cotas a serem suspensas com base
+	 * @param rp
+	 * @param page
+	 *
 	 * @param limiteInadimplencia - Quantidade de inadimplencias que define a cota como sujeita a suspensão
-	 * 
+	 *
 	 * @param paginacaoVO - Dados referentes a paginação e ordenação
 	 * @return lista de CotaSuspensaoDTO
 	 */
 	List<CotaSuspensaoDTO> obterDTOCotasSujeitasSuspensao(String sortOrder, String sortColumn, Integer inicio, Integer rp);
 
 	Long obterTotalCotasSujeitasSuspensao();
-	
+
 	String obterNomeResponsavelPorNumeroDaCota(Integer numeroCota);
-	
+
 	List<TelefoneAssociacaoDTO> buscarTelefonesCota(Long idCota, Set<Long> idsIgnorar);
-	
+
 	Cota obterCotaPDVPorNumeroDaCota(Integer numeroCota);
-	
+
 	//void cadastrarTelefonesCota(List<TelefoneCota> listaTelefonesAdicionar, Collection<Long> listaTelefonesRemover);
 
 	List<Cota> obterCotaAssociadaFiador(Long idFiador);
 
 	/**
 	 * Obtém dados de Distribuição da cota
-	 * 
+	 *
 	 * @param idCota - Código da cota
 	 * @return DTO com dados de distribuiçãos
 	 */
 	DistribuicaoDTO obterDadosDistribuicaoCota(Long idCota);
-	
-	
+
+
 	List<Fornecedor> obterFornecedoresCota(Long idCota);
 
 	/**
 	 * Salvar dados de Distribuição da cota
-	 * 
+	 *
 	 * @param distribuicao - DTO que representa os dados de Distribuição da cota
 	 */
 	void salvarDistribuicaoCota(DistribuicaoDTO distribuicao);
-	
+
 
 	/**
-	 * Salva os dados básicos da cota 
-	 * 
+	 * Salva os dados básicos da cota
+	 *
 	 * @param cotaDto
 	 */
 	Long salvarCota(CotaDTO cotaDto);
-	
+
 	void excluirCota(Long idCota);
-	
+
 	CotaDTO obterDadosCadastraisCota(Long idCota);
-	
+
 	Integer gerarNumeroSugestaoCota();
-	
+
 	List<TipoDesconto> obterDescontos(Long idCota);
-	
+
 	List<TipoDesconto> obterDescontosCota(Long idCota);
-	
+
 	void salvarDescontosCota(List<Long> descontos, Long idCota);
-	
+
 	void salvarSociosCota(List<SocioCota> sociosCota, Long idCota );
-	
+
 	List<SocioCota> obterSociosCota(Long idCota);
-	
+
 	Cota obterPorNumeroDaCotaAtiva(Integer numeroCota);
-	
+
 	/**
 	 * Método responsável por obter tipos de cota para preencher combo da camada view
 	 * @return comboTiposCota: Tipos de cota padrão.
 	 */
 	public List<ItemDTO<TipoCota, String>> getComboTiposCota();
 
-	
+
 }
--- nds-model/src/main/java/br/com/abril/nds/dto/filtro/FiltroCurvaABCDistribuidorDTO.java
+++ nds-model/src/main/java/br/com/abril/nds/dto/filtro/FiltroCurvaABCDistribuidorDTO.java
@@ -2,143 +2,73 @@ package br.com.abril.nds.dto.filtro;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 
-import br.com.abril.nds.util.export.Exportable;
-import br.com.abril.nds.vo.PaginacaoVO;
-
-@Exportable
-public class FiltroCurvaABCDistribuidorDTO implements Serializable {
-
+public class FiltroCurvaABCDistribuidorDTO extends FiltroCurvaABCDTO implements Serializable {
+	
 	/**
-	 * Serial Version UID
+	 * 
 	 */
-	private static final long serialVersionUID = -8369999015893950135L;
-
-	private Date dataDe;
-	
-	private Date dataAte;
-	
-	private String codigoFornecedor;
-	
-	private String codigoProduto;
-	
-	private String nomeProduto;
-	
-	private String edicaoProduto;
-	
-	private String codigoEditor;
-	
-	private String codigoCota;
-	
-	private String nomeCota;
-	
-	private String municipio;
-
-	private PaginacaoVO paginacao;
-	
-	public Date getDataDe() {
-		return dataDe;
-	}
-
-	public void setDataDe(Date dataDe) {
-		this.dataDe = dataDe;
-	}
-
-	public Date getDataAte() {
-		return dataAte;
-	}
-
-	public void setDataAte(Date dataAte) {
-		this.dataAte = dataAte;
-	}
-
-	public String getCodigoFornecedor() {
-		return codigoFornecedor;
-	}
-
-	public void setCodigoFornecedor(String codigoFornecedor) {
-		this.codigoFornecedor = codigoFornecedor;
-	}
-
-	public String getCodigoProduto() {
-		return codigoProduto;
-	}
-
-	public void setCodigoProduto(String codigoProduto) {
-		this.codigoProduto = codigoProduto;
-	}
-
-	public String getNomeProduto() {
-		return nomeProduto;
-	}
-
-	public void setNomeProduto(String nomeProduto) {
-		this.nomeProduto = nomeProduto;
-	}
-
-	public String getEdicaoProduto() {
-		return edicaoProduto;
-	}
-
-	public void setEdicaoProduto(String edicaoProduto) {
-		this.edicaoProduto = edicaoProduto;
-	}
-
-	public String getCodigoEditor() {
-		return codigoEditor;
-	}
-
-	public void setCodigoEditor(String codigoEditor) {
-		this.codigoEditor = codigoEditor;
-	}
-
-	public String getCodigoCota() {
-		return codigoCota;
-	}
-
-	public void setCodigoCota(String codigoCota) {
-		this.codigoCota = codigoCota;
-	}
-
-	public String getNomeCota() {
-		return nomeCota;
-	}
-
-	public void setNomeCota(String nomeCota) {
-		this.nomeCota = nomeCota;
-	}
-
-	public String getMunicipio() {
-		return municipio;
-	}
-
-	public void setMunicipio(String municipio) {
-		this.municipio = municipio;
-	}
-
-	public PaginacaoVO getPaginacao() {
-		return paginacao;
-	}
-
-	public void setPaginacao(PaginacaoVO paginacao) {
-		this.paginacao = paginacao;
-	}
+	private static final long serialVersionUID = -1308372670097421023L;
 
 	public FiltroCurvaABCDistribuidorDTO(Date dataDe, Date dataAte,
 			String codigoFornecedor, String codigoProduto, String nomeProduto,
 			String edicaoProduto, String codigoEditor, String codigoCota,
 			String nomeCota, String municipio) {
-		super();
-		this.dataDe = dataDe;
-		this.dataAte = dataAte;
-		this.codigoFornecedor = codigoFornecedor;
-		this.codigoProduto = codigoProduto;
-		this.nomeProduto = nomeProduto;
-		this.edicaoProduto = edicaoProduto;
-		this.codigoEditor = codigoEditor;
-		this.codigoCota = codigoCota;
-		this.nomeCota = nomeCota;
-		this.municipio = municipio;
-	}	
+		this.setDataDe(dataDe);
+		this.setDataAte(dataAte);
+		this.setCodigoFornecedor(codigoFornecedor);
+		this.setCodigoProduto(codigoProduto);
+		this.setNomeProduto(nomeProduto);
+		this.setEdicaoProduto(edicaoProduto);
+		this.setCodigoEditor(codigoEditor);
+		this.setCodigoCota(codigoCota);
+		this.setNomeCota(nomeCota);
+		this.setMunicipio(municipio);
+	}
+	
+	private List<ColunaOrdenacaoCurvaABCDistribuidor> listaColunaOrdenacao;
+
+	private ColunaOrdenacaoCurvaABCDistribuidor ordenacaoColuna;
+	
+	public enum ColunaOrdenacaoCurvaABCDistribuidor {
+
+		COTA("numeroCota"),
+		NOME("nomeCota"),
+		MUNICIPIO("municipio"),
+		QTDEPDV("quantidadePdvs"),
+		VENDA_EXEMPLARES("vendaExemplares"),
+		FATURAMENTO("faturamentoCapa"),
+		PARTICIPACAO("participacao"),
+		PARTICIPACAO_ACUMULADA("participacaoAcumulada");
+
+		private String nomeColuna;
+		
+		private ColunaOrdenacaoCurvaABCDistribuidor(String nomeColuna) {
+			this.nomeColuna = nomeColuna;
+		}
+		
+		@Override
+		public String toString() {
+			return this.nomeColuna;
+		}
+	}
+
+	public List<ColunaOrdenacaoCurvaABCDistribuidor> getListaColunaOrdenacao() {
+		return listaColunaOrdenacao;
+	}
+
+	public void setListaColunaOrdenacao(List<ColunaOrdenacaoCurvaABCDistribuidor> listaColunaOrdenacao) {
+		this.listaColunaOrdenacao = listaColunaOrdenacao;
+	}
+
+	public ColunaOrdenacaoCurvaABCDistribuidor getOrdenacaoColuna() {
+		return ordenacaoColuna;
+	}
+
+	public void setOrdenacaoColuna(
+			ColunaOrdenacaoCurvaABCDistribuidor ordenacaoColuna) {
+		this.ordenacaoColuna = ordenacaoColuna;
+	}
 
 }
