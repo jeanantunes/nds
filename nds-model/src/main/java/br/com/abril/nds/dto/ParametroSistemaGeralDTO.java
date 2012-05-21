4d8a9ecb02267187471d3af2412e8eba98076363
Alexandre Takashi <alexandre.tamanaha@discover.com.br>
2012-05-21 20:00:06 -0300

    EMS 173: Alteracao na forma de armazenar os atributos no DTO.

--- nds-model/src/main/java/br/com/abril/nds/dto/ParametroSistemaGeralDTO.java
+++ nds-model/src/main/java/br/com/abril/nds/dto/ParametroSistemaGeralDTO.java
@@ -1,10 +1,8 @@
 package br.com.abril.nds.dto;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import br.com.abril.nds.model.cadastro.ParametroSistema;
@@ -19,11 +17,10 @@ import br.com.abril.nds.model.cadastro.TipoParametroSistema;
 public class ParametroSistemaGeralDTO implements Serializable {
 	
 	/** */
-	private static final long serialVersionUID = 4527330351986164865L;
+	private static final long serialVersionUID = -3743082353422640710L;
 	
-	
-	private Map<TipoParametroSistema, ParametroSistema> params = 
-			new HashMap<TipoParametroSistema, ParametroSistema>();
+	private Map<TipoParametroSistema, String> params = 
+			new HashMap<TipoParametroSistema, String>();
 	
 	
 	/** Método Construtor padrão. */
@@ -32,12 +29,14 @@ public class ParametroSistemaGeralDTO implements Serializable {
 	
 	
 	/**
-	 * Obtém todos os parâmetros de sistema contidos nesta DTO.
-	 *  
+	 * Trata o valor do parâmetro para que não retorne 'null':
+	 * @param tps
 	 * @return
 	 */
-	public List<ParametroSistema> getParametrosSistema() {
-		return new ArrayList<ParametroSistema>(params.values());
+	private String getParametroSistemaString(TipoParametroSistema tps) {
+		
+		String str = this.params.get(tps);
+		return str == null ? "" : str;
 	}
 	
 	/**
@@ -47,338 +46,338 @@ public class ParametroSistemaGeralDTO implements Serializable {
 	 */
 	public void setParametrosSistema(Collection<ParametroSistema> parametrosSistema) {
 		for (ParametroSistema ps : parametrosSistema) {
-			params.put(ps.getTipoParametroSistema(), ps);
+			params.put(ps.getTipoParametroSistema(), ps.getValor());
 		}
 	}
 	
 	
 	/**
-	 * @return the pathLogo
+	 * @return the logoSistema
 	 */
-	public ParametroSistema getPathLogo() {
-		return this.params.get(TipoParametroSistema.PATH_LOGO_SISTEMA);
+	public String getLogoSistema() {
+		return this.params.get(TipoParametroSistema.LOGO_SISTEMA);
 	}
 
 	/**
-	 * @param pathLogo
-	 *            the pathLogo to set
+	 * @param logoSistema
+	 *            the logoSistema to set
 	 */
-	public void setPathLogo(ParametroSistema pathLogo) {
-		this.params.put(TipoParametroSistema.PATH_LOGO_SISTEMA, pathLogo);
+	public void setLogoSistema(String logoSistema) {
+		this.params.put(TipoParametroSistema.LOGO_SISTEMA, logoSistema);
 	}
 
 	/**
 	 * @return the cnpj
 	 */
-	public ParametroSistema getCnpj() {
-		return this.params.get(TipoParametroSistema.CNPJ);
+	public String getCnpj() {
+		return this.getParametroSistemaString(TipoParametroSistema.CNPJ);
 	}
 
 	/**
 	 * @param cnpj
 	 *            the cnpj to set
 	 */
-	public void setCnpj(ParametroSistema cnpj) {
+	public void setCnpj(String cnpj) {
 		this.params.put(TipoParametroSistema.CNPJ, cnpj);
 	}
 
 	/**
 	 * @return the razaoSocial
 	 */
-	public ParametroSistema getRazaoSocial() {
-		return this.params.get(TipoParametroSistema.RAZAO_SOCIAL);
+	public String getRazaoSocial() {
+		return this.getParametroSistemaString(TipoParametroSistema.RAZAO_SOCIAL);
 	}
 
 	/**
 	 * @param razaoSocial
 	 *            the razaoSocial to set
 	 */
-	public void setRazaoSocial(ParametroSistema razaoSocial) {
+	public void setRazaoSocial(String razaoSocial) {
 		this.params.put(TipoParametroSistema.RAZAO_SOCIAL, razaoSocial);
 	}
 
 	/**
 	 * @return the email
 	 */
-	public ParametroSistema getEmail() {
-		return this.params.get(TipoParametroSistema.EMAIL_USUARIO);
+	public String getEmail() {
+		return this.getParametroSistemaString(TipoParametroSistema.EMAIL_USUARIO);
 	}
 
 	/**
 	 * @param email
 	 *            the email to set
 	 */
-	public void setEmail(ParametroSistema email) {
+	public void setEmail(String email) {
 		this.params.put(TipoParametroSistema.EMAIL_USUARIO, email);
 	}
 
 	/**
 	 * @return the uf
 	 */
-	public ParametroSistema getUf() {
-		return this.params.get(TipoParametroSistema.UF);
+	public String getUf() {
+		return this.getParametroSistemaString(TipoParametroSistema.UF);
 	}
 
 	/**
 	 * @param uf
 	 *            the uf to set
 	 */
-	public void setUf(ParametroSistema uf) {
+	public void setUf(String uf) {
 		this.params.put(TipoParametroSistema.UF, uf);
 	}
 
 	/**
 	 * @return the codDistribuidorDinap
 	 */
-	public ParametroSistema getCodDistribuidorDinap() {
-		return this.params.get(TipoParametroSistema.CODIGO_DISTRIBUIDOR_DINAP);
+	public String getCodDistribuidorDinap() {
+		return this.getParametroSistemaString(TipoParametroSistema.CODIGO_DISTRIBUIDOR_DINAP);
 	}
 
 	/**
 	 * @param codDistribuidorDinap
 	 *            the codDistribuidorDinap to set
 	 */
-	public void setCodDistribuidorDinap(ParametroSistema codDistribuidorDinap) {
+	public void setCodDistribuidorDinap(String codDistribuidorDinap) {
 		this.params.put(TipoParametroSistema.CODIGO_DISTRIBUIDOR_DINAP, codDistribuidorDinap);
 	}
 
 	/**
 	 * @return the codDistribuidorFc
 	 */
-	public ParametroSistema getCodDistribuidorFc() {
-		return this.params.get(TipoParametroSistema.CODIGO_DISTRIBUIDOR_FC);
+	public String getCodDistribuidorFc() {
+		return this.getParametroSistemaString(TipoParametroSistema.CODIGO_DISTRIBUIDOR_FC);
 	}
 
 	/**
 	 * @param codDistribuidorFc
 	 *            the codDistribuidorFc to set
 	 */
-	public void setCodDistribuidorFc(ParametroSistema codDistribuidorFc) {
+	public void setCodDistribuidorFc(String codDistribuidorFc) {
 		this.params.put(TipoParametroSistema.CODIGO_DISTRIBUIDOR_FC, codDistribuidorFc);
 	}
 
 	/**
 	 * @return the login
 	 */
-	public ParametroSistema getLogin() {
-		return this.params.get(TipoParametroSistema.LOGIN_DISTRIBUIDOR);
+	public String getLogin() {
+		return this.getParametroSistemaString(TipoParametroSistema.LOGIN_DISTRIBUIDOR);
 	}
 
 	/**
 	 * @param login
 	 *            the login to set
 	 */
-	public void setLogin(ParametroSistema login) {
+	public void setLogin(String login) {
 		this.params.put(TipoParametroSistema.LOGIN_DISTRIBUIDOR, login);
 	}
 
 	/**
 	 * @return the senha
 	 */
-	public ParametroSistema getSenha() {
-		return this.params.get(TipoParametroSistema.SENHA_DISTRIBUIDOR);
+	public String getSenha() {
+		return this.getParametroSistemaString(TipoParametroSistema.SENHA_DISTRIBUIDOR);
 	}
 
 	/**
 	 * @param senha
 	 *            the senha to set
 	 */
-	public void setSenha(ParametroSistema senha) {
+	public void setSenha(String senha) {
 		this.params.put(TipoParametroSistema.SENHA_DISTRIBUIDOR, senha);
 	}
 
 	/**
 	 * @return the versaoSistema
 	 */
-	public ParametroSistema getVersaoSistema() {
-		return this.params.get(TipoParametroSistema.VERSAO_SISTEMA);
+	public String getVersaoSistema() {
+		return this.getParametroSistemaString(TipoParametroSistema.VERSAO_SISTEMA);
 	}
 
 	/**
 	 * @param versaoSistema
 	 *            the versaoSistema to set
 	 */
-	public void setVersaoSistema(ParametroSistema versaoSistema) {
+	public void setVersaoSistema(String versaoSistema) {
 		this.params.put(TipoParametroSistema.VERSAO_SISTEMA, versaoSistema);
 	}
 
 	/**
 	 * @return the pathCeExportacao
 	 */
-	public ParametroSistema getPathCeExportacao() {
-		return this.params.get(TipoParametroSistema.PATH_INTERFACE_CE_EXPORTACAO);
+	public String getPathCeExportacao() {
+		return this.getParametroSistemaString(TipoParametroSistema.PATH_INTERFACE_CE_EXPORTACAO);
 	}
 
 	/**
 	 * @param pathCeExportacao
 	 *            the pathCeExportacao to set
 	 */
-	public void setPathCeExportacao(ParametroSistema pathCeExportacao) {
+	public void setPathCeExportacao(String pathCeExportacao) {
 		this.params.put(TipoParametroSistema.PATH_INTERFACE_CE_EXPORTACAO, pathCeExportacao);
 	}
 
 	/**
-	 * @return the pathProdinImportcao
+	 * @return the pathProdinImportacao
 	 */
-	public ParametroSistema getPathProdinImportcao() {
-		return this.params.get(TipoParametroSistema.PATH_INTERFACE_PRODIN_IMPORTCAO);
+	public String getPathProdinImportacao() {
+		return this.getParametroSistemaString(TipoParametroSistema.PATH_INTERFACE_PRODIN_IMPORTACAO);
 	}
 
 	/**
-	 * @param pathProdinImportcao
-	 *            the pathProdinImportcao to set
+	 * @param pathProdinImportacao
+	 *            the pathProdinImportacao to set
 	 */
-	public void setPathProdinImportcao(ParametroSistema pathProdinImportcao) {
-		this.params.put(TipoParametroSistema.PATH_INTERFACE_PRODIN_IMPORTCAO, pathProdinImportcao);
+	public void setPathProdinImportacao(String pathProdinImportacao) {
+		this.params.put(TipoParametroSistema.PATH_INTERFACE_PRODIN_IMPORTACAO, pathProdinImportacao);
 	}
 
 	/**
 	 * @return the pathProdinExportacao
 	 */
-	public ParametroSistema getPathProdinExportacao() {
-		return this.params.get(TipoParametroSistema.PATH_INTERFACE_PRODIN_EXPORTACAO);
+	public String getPathProdinExportacao() {
+		return this.getParametroSistemaString(TipoParametroSistema.PATH_INTERFACE_PRODIN_EXPORTACAO);
 	}
 
 	/**
 	 * @param pathProdinExportacao
 	 *            the pathProdinExportacao to set
 	 */
-	public void setPathProdinExportacao(ParametroSistema pathProdinExportacao) {
+	public void setPathProdinExportacao(String pathProdinExportacao) {
 		this.params.put(TipoParametroSistema.PATH_INTERFACE_PRODIN_EXPORTACAO, pathProdinExportacao);
 	}
 
 	/**
-	 * @return the pathMdcImportcao
+	 * @return the pathMdcImportacao
 	 */
-	public ParametroSistema getPathMdcImportcao() {
-		return this.params.get(TipoParametroSistema.PATH_INTERFACE_MDC_IMPORTCAO);
+	public String getPathMdcImportacao() {
+		return this.getParametroSistemaString(TipoParametroSistema.PATH_INTERFACE_MDC_IMPORTACAO);
 	}
 
 	/**
-	 * @param pathMdcImportcao
-	 *            the pathMdcImportcao to set
+	 * @param pathMdcImportacao
+	 *            the pathMdcImportacao to set
 	 */
-	public void setPathMdcImportcao(ParametroSistema pathMdcImportcao) {
-		this.params.put(TipoParametroSistema.PATH_INTERFACE_MDC_IMPORTCAO, pathMdcImportcao);
+	public void setPathMdcImportacao(String pathMdcImportacao) {
+		this.params.put(TipoParametroSistema.PATH_INTERFACE_MDC_IMPORTACAO, pathMdcImportacao);
 	}
 
 	/**
 	 * @return the pathMdcExportacao
 	 */
-	public ParametroSistema getPathMdcExportacao() {
-		return this.params.get(TipoParametroSistema.PATH_INTERFACE_MDC_EXPORTACAO);
+	public String getPathMdcExportacao() {
+		return this.getParametroSistemaString(TipoParametroSistema.PATH_INTERFACE_MDC_EXPORTACAO);
 	}
 
 	/**
 	 * @param pathMdcExportacao
 	 *            the pathMdcExportacao to set
 	 */
-	public void setPathMdcExportacao(ParametroSistema pathMdcExportacao) {
+	public void setPathMdcExportacao(String pathMdcExportacao) {
 		this.params.put(TipoParametroSistema.PATH_INTERFACE_MDC_EXPORTACAO, pathMdcExportacao);
 	}
 
 	/**
 	 * @return the pathBancasExportacao
 	 */
-	public ParametroSistema getPathBancasExportacao() {
-		return this.params.get(TipoParametroSistema.PATH_INTERFACE_BANCAS_EXPORTACAO);
+	public String getPathBancasExportacao() {
+		return this.getParametroSistemaString(TipoParametroSistema.PATH_INTERFACE_BANCAS_EXPORTACAO);
 	}
 
 	/**
 	 * @param pathBancasExportacao
 	 *            the pathBancasExportacao to set
 	 */
-	public void setPathBancasExportacao(ParametroSistema pathBancasExportacao) {
+	public void setPathBancasExportacao(String pathBancasExportacao) {
 		this.params.put(TipoParametroSistema.PATH_INTERFACE_BANCAS_EXPORTACAO, pathBancasExportacao);
 	}
 
 	/**
-	 * @return the pathGfsImportcao
+	 * @return the pathGfsImportacao
 	 */
-	public ParametroSistema getPathGfsImportcao() {
-		return this.params.get(TipoParametroSistema.PATH_INTERFACE_GFS_IMPORTCAO);
+	public String getPathGfsImportacao() {
+		return this.getParametroSistemaString(TipoParametroSistema.PATH_INTERFACE_GFS_IMPORTACAO);
 	}
 
 	/**
-	 * @param pathGfsImportcao
-	 *            the pathGfsImportcao to set
+	 * @param pathGfsImportacao
+	 *            the pathGfsImportacao to set
 	 */
-	public void setPathGfsImportcao(ParametroSistema pathGfsImportcao) {
-		this.params.put(TipoParametroSistema.PATH_INTERFACE_GFS_IMPORTCAO, pathGfsImportcao);
+	public void setPathGfsImportacao(String pathGfsImportacao) {
+		this.params.put(TipoParametroSistema.PATH_INTERFACE_GFS_IMPORTACAO, pathGfsImportacao);
 	}
 
 	/**
 	 * @return the pathGfsExportacao
 	 */
-	public ParametroSistema getPathGfsExportacao() {
-		return this.params.get(TipoParametroSistema.PATH_INTERFACE_GFS_EXPORTACAO);
+	public String getPathGfsExportacao() {
+		return this.getParametroSistemaString(TipoParametroSistema.PATH_INTERFACE_GFS_EXPORTACAO);
 	}
 
 	/**
 	 * @param pathGfsExportacao
 	 *            the pathGfsExportacao to set
 	 */
-	public void setPathGfsExportacao(ParametroSistema pathGfsExportacao) {
+	public void setPathGfsExportacao(String pathGfsExportacao) {
 		this.params.put(TipoParametroSistema.PATH_INTERFACE_GFS_EXPORTACAO, pathGfsExportacao);
 	}
 
 	/**
-	 * @return the pathNfeImportcao
+	 * @return the pathNfeImportacao
 	 */
-	public ParametroSistema getPathNfeImportcao() {
-		return this.params.get(TipoParametroSistema.PATH_INTERFACE_NFE_IMPORTCAO);
+	public String getPathNfeImportacao() {
+		return this.getParametroSistemaString(TipoParametroSistema.PATH_INTERFACE_NFE_IMPORTACAO);
 	}
 
 	/**
-	 * @param pathNfeImportcao
-	 *            the pathNfeImportcao to set
+	 * @param pathNfeImportacao
+	 *            the pathNfeImportacao to set
 	 */
-	public void setPathNfeImportcao(ParametroSistema pathNfeImportcao) {
-		this.params.put(TipoParametroSistema.PATH_INTERFACE_NFE_IMPORTCAO, pathNfeImportcao);
+	public void setPathNfeImportacao(String pathNfeImportacao) {
+		this.params.put(TipoParametroSistema.PATH_INTERFACE_NFE_IMPORTACAO, pathNfeImportacao);
 	}
 
 	/**
 	 * @return the pathNfeExportacao
 	 */
-	public ParametroSistema getPathNfeExportacao() {
-		return this.params.get(TipoParametroSistema.PATH_INTERFACE_NFE_EXPORTACAO);
+	public String getPathNfeExportacao() {
+		return this.getParametroSistemaString(TipoParametroSistema.PATH_INTERFACE_NFE_EXPORTACAO);
 	}
 
 	/**
 	 * @param pathNfeExportacao
 	 *            the pathNfeExportacao to set
 	 */
-	public void setPathNfeExportacao(ParametroSistema pathNfeExportacao) {
+	public void setPathNfeExportacao(String pathNfeExportacao) {
 		this.params.put(TipoParametroSistema.PATH_INTERFACE_NFE_EXPORTACAO, pathNfeExportacao);
 	}
 
 	/**
 	 * @return the nfeDpec
 	 */
-	public ParametroSistema getNfeDpec() {
-		return this.params.get(TipoParametroSistema.NFE_DPEC);
+	public String getNfeDpec() {
+		return this.getParametroSistemaString(TipoParametroSistema.NFE_DPEC);
 	}
 
 	/**
 	 * @param nfeDpec
 	 *            the nfeDpec to set
 	 */
-	public void setNfeDpec(ParametroSistema nfeDpec) {
+	public void setNfeDpec(String nfeDpec) {
 		this.params.put(TipoParametroSistema.NFE_DPEC, nfeDpec);
 	}
 
 	/**
 	 * @return the dtOperacaoCorrente
 	 */
-	public ParametroSistema getDtOperacaoCorrente() {
-		return this.params.get(TipoParametroSistema.DATA_OPERACAO_CORRENTE);
+	public String getDtOperacaoCorrente() {
+		return this.getParametroSistemaString(TipoParametroSistema.DATA_OPERACAO_CORRENTE);
 	}
 
 	/**
 	 * @param dtOperacaoCorrente
 	 *            the dtOperacaoCorrente to set
 	 */
-	public void setDtOperacaoCorrente(ParametroSistema dtOperacaoCorrente) {
+	public void setDtOperacaoCorrente(String dtOperacaoCorrente) {
 		this.params.put(TipoParametroSistema.DATA_OPERACAO_CORRENTE, dtOperacaoCorrente);
 	}
 
