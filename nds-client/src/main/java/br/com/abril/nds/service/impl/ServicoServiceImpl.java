4a1ca89bab6088a985a6d67ed428cce6ac61c466
mateus.medice <mateus.medice@DSCN007.discover.com.br>
2012-05-21 14:42:22 -0300

    Criação dos seriços do cadastro de serviços

---
+++ nds-client/src/main/java/br/com/abril/nds/service/impl/ServicoServiceImpl.java
@@ -0,0 +1,13 @@
+package br.com.abril.nds.service.impl;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Classe de implementação dos serviços de jornaleiros.
+ * 
+ * @author Discover Technology
+ */
+@Service
+public class ServicoServiceImpl {
+
+}
