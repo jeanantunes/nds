048be2c957269a13b7deaf37d8179fd60692d391
paulokinho <paulo.nascimento@discover.com.br>
2012-05-23 14:04:35 -0300

    Entrega Onda 2

---
+++ ndsi-engine/src/main/java/br/com/abril/nds/integracao/service/PeriodicidadeProdutoService.java
@@ -0,0 +1,8 @@
+package br.com.abril.nds.integracao.service;
+
+import br.com.abril.nds.model.cadastro.PeriodicidadeProduto;
+
+public interface PeriodicidadeProdutoService {
+	
+	public PeriodicidadeProduto getPeriodicidadeProdutoAsArchive(Integer periodicidade);
+}
