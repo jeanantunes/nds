5ac724560d1ae33f41b79a2742c3d572bc7e13be
paulokinho <paulo.nascimento@discover.com.br>
2012-05-23 15:49:14 -0300

    Inclusao Relacionamento Bi-direcional do ProdutoEdicao (MovimentoEstoque e ChamadaEncalhe)

--- nds-model/src/main/java/br/com/abril/nds/model/cadastro/ProdutoEdicao.java
+++ nds-model/src/main/java/br/com/abril/nds/model/cadastro/ProdutoEdicao.java
@@ -12,6 +12,7 @@ import javax.persistence.DiscriminatorType;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
@@ -25,6 +26,8 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.persistence.UniqueConstraint;
 
+import br.com.abril.nds.model.estoque.MovimentoEstoque;
+import br.com.abril.nds.model.planejamento.ChamadaEncalhe;
 import br.com.abril.nds.model.planejamento.Lancamento;
 
 /**
@@ -85,6 +88,12 @@ public class ProdutoEdicao implements Serializable {
 	@OneToMany(mappedBy = "produtoEdicao")
 	protected Set<Lancamento> lancamentos = new HashSet<Lancamento>();
 	
+	@OneToMany(mappedBy = "produtoEdicao", fetch=FetchType.LAZY)
+	private Set<MovimentoEstoque> movimentoEstoques = new HashSet<MovimentoEstoque>();
+
+	@OneToMany(mappedBy = "produtoEdicao", fetch=FetchType.LAZY)
+	private Set<ChamadaEncalhe> chamadaEncalhes = new HashSet<ChamadaEncalhe>(); 
+
 	@Column(name = "POSSUI_BRINDE", nullable = true)
 	protected boolean possuiBrinde;
 	
@@ -371,6 +380,22 @@ public class ProdutoEdicao implements Serializable {
 	 */
 	public void setSlogan(String slogan) {
 		this.slogan = slogan;
+	}
+
+	public Set<MovimentoEstoque> getMovimentoEstoques() {
+		return movimentoEstoques;
+	}
+
+	public void setMovimentoEstoques(Set<MovimentoEstoque> movimentoEstoques) {
+		this.movimentoEstoques = movimentoEstoques;
+	}
+
+	public Set<ChamadaEncalhe> getChamadaEncalhes() {
+		return chamadaEncalhes;
+	}
+
+	public void setChamadaEncalhes(Set<ChamadaEncalhe> chamadaEncalhes) {
+		this.chamadaEncalhes = chamadaEncalhes;
 	}
 
 }
