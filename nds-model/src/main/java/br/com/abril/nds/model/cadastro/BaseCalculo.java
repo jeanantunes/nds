440d9ed12c973f13e9f3f4ab453dbbc214d29366
Mateus <mateus.medice@gmail.com>
2012-05-25 13:12:41 -0300

    Criação do menu para serviço de entrega e javadoc.

--- nds-model/src/main/java/br/com/abril/nds/model/cadastro/BaseCalculo.java
+++ nds-model/src/main/java/br/com/abril/nds/model/cadastro/BaseCalculo.java
@@ -2,16 +2,23 @@ package br.com.abril.nds.model.cadastro;
 
 public enum BaseCalculo {
 
-	FATURAMENTO_BRUTO("B"),
-	FATURAMENTO_LIQUIDO("L");
-	
+	FATURAMENTO_BRUTO("B", "Faturamento Bruto"),
+	FATURAMENTO_LIQUIDO("L", "Faturamento Liquido");
+
+	private String key;
+
 	private String value;
 	
-	private BaseCalculo(String value) {
+	private BaseCalculo(String key, String value) {
+		this.key = key;
 		this.value = value;
 	}
-	
+
 	public String getValue() {
 		return this.value;
+	}
+	
+	public String getKey() {
+		return this.key;
 	}
 }
