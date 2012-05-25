41b2ef9b3feee728c36b318a112e95e6edd3c4a3
Alexandre Takashi <alexandre.tamanaha@discover.com.br>
2012-05-25 18:33:39 -0300

    EMS 173: Inclusao de js para AJAX.

---
+++ nds-client/src/main/webapp/scripts/parametrosSistema.js
@@ -0,0 +1,62 @@
+/* JS para a tela de Parâmetros do Sistema. */
+
+function ParametroSistema() {
+	
+	this.bindEvents();
+	this.initUploadForm();
+
+}
+
+ParametroSistema.prototype.path = contextPath + "/administracao/parametrosSistema/";
+
+ParametroSistema.prototype.initUploadForm = function() {
+	var _this = this;
+	
+	$('#formUpload').bind('submit', function(e) {
+		
+		e.preventDefault(); // <-- important
+		$(this).ajaxSubmit({
+			beforeSubmit: function(arr, formData, options) {
+				
+				// Validar seleção de imagem:
+				if (!$('#imgLogoSistema').val()) { 
+					exibirMensagem('WARNING', ['Escolha um arquivo para ser enviado.'], "");
+					return false; 
+				}
+			},
+			success: function(responseText, statusText, xhr, $form)  { 
+				var mensagens = (responseText.mensagens) ? responseText.mensagens : responseText.result;   
+				var tipoMensagem = mensagens.tipoMensagem;
+				var listaMensagens = mensagens.listaMensagens;
+				if (tipoMensagem && listaMensagens) {
+					exibirMensagem(tipoMensagem, listaMensagens, "");
+				}    	    	    
+				
+				// TODO: Implementar posteriormente:
+				// _this.loadImage();
+				
+			} ,
+			url: _this.path + 'salvar',
+			type: 'POST',
+			dataType: 'json'
+		});
+	});
+};
+
+
+// Traz a imagem do logotipo para a tela:
+ParametroSistema.prototype.loadImage = function(){
+	var imgPath = this.path + 'getImageLogotipo';
+	var img = null;
+	img = $("<img />").attr('src', imgPath).attr('width', '110').attr('height', '70').attr('alt', 'Logo Sistema')
+   .load(function() {
+      if (!(!this.complete || typeof this.naturalWidth == "undefined" || this.naturalWidth == 0)) {
+          $("#div_imagem_logotipo").append(img);
+      }
+   });
+};
+
+
+ParametroSistema.prototype.bindEvents = function() {
+	$("#cnpj").mask("99.999.999/9999-99");
+};
