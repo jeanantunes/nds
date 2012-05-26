74be54b60d276ffe7e5bf459d63da5fa871729db
Alexandre Takashi <alexandre.tamanaha@discover.com.br>
2012-05-26 14:39:35 -0300

    EMS-173: Inclusao de validacao diversas.

--- nds-client/src/main/java/br/com/abril/nds/controllers/administracao/ParametrosSistemaController.java
+++ nds-client/src/main/java/br/com/abril/nds/controllers/administracao/ParametrosSistemaController.java
@@ -4,6 +4,7 @@ import java.io.InputStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import br.com.abril.nds.client.vo.ValidacaoVO;
 import br.com.abril.nds.dto.ParametroSistemaGeralDTO;
 import br.com.abril.nds.exception.ValidacaoException;
 import br.com.abril.nds.service.ParametroSistemaService;
@@ -11,16 +12,23 @@ import br.com.abril.nds.util.TipoMensagem;
 import br.com.abril.nds.util.UfEnum;
 import br.com.abril.nds.util.Util;
 
+import br.com.caelum.stella.ValidationMessage;
+import br.com.caelum.stella.validation.CNPJValidator;
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.interceptor.download.ByteArrayDownload;
+import br.com.caelum.vraptor.interceptor.download.Download;
 import br.com.caelum.vraptor.interceptor.multipart.UploadedFile;
+import br.com.caelum.vraptor.view.Results;
 
 @Resource
 @Path("/administracao/parametrosSistema")
 public class ParametrosSistemaController {
+	
+	
 	
 	@Autowired
 	private Result result;
@@ -51,8 +59,6 @@ public class ParametrosSistemaController {
 	 * @param dto
 	 * @param imgLogoSistema
 	 */
-	@Post
-	@Path("/salvar")
 	public void salvar(ParametroSistemaGeralDTO dto, UploadedFile imgLogoSistema) {
 		
 		// WORKAROUND: para tratar o checkbox (no cenário que ele foi desmarcado):
@@ -66,42 +72,68 @@ public class ParametrosSistemaController {
 			nomeImgLogotipo = imgLogoSistema.getFileName();
 		}
 		
-		dto.setLogoSistema(nomeImgLogotipo);
+		//dto.setLogoSistema(nomeImgLogotipo);
 		
-		
-		// TODO: validar imagem
-		// TODO: validar CNPJ
-		
-		// TODO: Validar email:
-		/*
-		if (!Util.validarEmail(dto.getEmail())) {
-			throw new ValidacaoException(TipoMensagem.ERROR, "Formato de email é inválido!");
-		}
-		*/
-		
-		// TODO: Validar UF:
-		/*
-		boolean invalidUf = true;
-		for (UfEnum uf : Util.getUfs(null)) {
-			if (uf.getSigla().equals(dto.getUf())) {
-				invalidUf = false;
-				break;
-			}
-		}
-		if (invalidUf) {
-			throw new ValidacaoException(TipoMensagem.ERROR, "Unidade Federativa é inválida!");
-		}
-		*/
+		//
+		this.validar(dto, imgLogoSistema);
 		
 		
 		// Salvar:
-		psService.salvar(dto, imgLogotipo);
+		//psService.salvar(dto, imgLogotipo);
+		result.use(Results.json()).from(new ValidacaoVO(TipoMensagem.SUCCESS, "Parâmetros salvos com sucesso."), "result").recursive().serialize();
+	}
+	
+	
+	/**
+	 * Realiza as validações dos campos da tela.
+	 *  
+	 * @param dto
+	 * @param imgLogoSistema
+	 * 
+	 * @exception
+	 */
+	private void validar(ParametroSistemaGeralDTO dto, UploadedFile imgLogoSistema) 
+				throws ValidacaoException {
 		
-		// deu erro:
-		//result.forwardTo(this).index();
-				
-		// gravou certinho:
-		result.redirectTo(this).index();
+		// TODO: validar imagem
+		if (imgLogoSistema == null) {
+			throw new ValidacaoException(TipoMensagem.ERROR, "Escolha um arquivo para ser enviado!");
+		}
+		String fileName = imgLogoSistema.getFileName().toLowerCase();
+		if (!fileName.matches("([A-Za-z0-9]|_|.|-)+\\.(png|gif)")) {
+			throw new ValidacaoException(TipoMensagem.ERROR, "Por favor, selecione um formato de arquivo 'gif' ou 'png'!");
+		}
+		
+		// TODO: validar CNPJ
+		CNPJValidator vld = new CNPJValidator(true);
+		for (ValidationMessage error : vld.invalidMessagesFor(dto.getCnpj())) {
+			throw new ValidacaoException(TipoMensagem.ERROR, "Por favor, preencha com um CNPJ válido!");
+		}
+		
+		// TODO: Validar email:
+		if (!Util.validarEmail(dto.getEmail())) {
+			throw new ValidacaoException(TipoMensagem.ERROR, "Por favor, preencha com um email válido!");
+		}
+		
+		// TODO: Validar UF:
+		try {
+			UfEnum.valueOf(dto.getUf().toUpperCase());
+		} catch (Exception e) {
+			throw new ValidacaoException(TipoMensagem.ERROR, "Por favor, selecione uma Unidade Federativa válida!");
+		}
+	}
+	
+	public Download getImageLogotipo() {		
+		/*
+		byte[] buff = cotaGarantiaService.getImageCheque(idCheque);
+		
+		if (buff == null) {
+			buff = new byte[0];
+		}
+		
+		return new ByteArrayDownload(buff, "image/jpeg", "cheque.jpg");
+		*/
+		return null;
 	}
 	
 }
