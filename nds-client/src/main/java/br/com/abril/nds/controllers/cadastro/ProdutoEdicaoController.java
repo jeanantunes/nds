b6772bae2d5d5a174e2cce824adca996a77eb92d
Alexandre Takashi <alexandre.tamanaha@discover.com.br>
2012-05-27 23:18:17 -0300

    EMS-217: implementacao do botao de calendario.

--- nds-client/src/main/java/br/com/abril/nds/controllers/cadastro/ProdutoEdicaoController.java
+++ nds-client/src/main/java/br/com/abril/nds/controllers/cadastro/ProdutoEdicaoController.java
@@ -2,6 +2,7 @@ package br.com.abril.nds.controllers.cadastro;
 
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.Resource;
 
 @Resource
@@ -12,5 +13,21 @@ public class ProdutoEdicaoController {
 	@Get
 	@Path("/")
 	public void index() { }
+	
+	@Get
+	@Path("/pesquisarEdicoes")
+	public void pesquisarEdicoes(String codigoProduto, String nomeProduto,
+			String dataLancamento, String situacaoLancamento,
+			String codigoDeBarras, String brinde) {
+		
+		
+		System.out.println("codigoProduto: " + codigoProduto);
+		System.out.println("nomeProduto: " + nomeProduto);
+		System.out.println("dataLancamento: " + dataLancamento);
+		System.out.println("situacaoLancamento: " + situacaoLancamento);
+		System.out.println("codigoDeBarras: " + codigoDeBarras);
+		System.out.println("brinde: " + brinde);
+		
+	}
 	
 }
