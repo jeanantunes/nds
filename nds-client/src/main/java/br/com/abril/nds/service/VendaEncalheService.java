1694152be7a27bbcca7332c814234c06a69d37b0
leandrolupianhez <leandro.lupianhez@gmail.com>
2012-05-30 14:32:07 -0300

    Inclusão de artefatos para desenvolvimento da EMS 0020 - Venda de Encalhe

---
+++ nds-client/src/main/java/br/com/abril/nds/service/VendaEncalheService.java
@@ -0,0 +1,13 @@
+package br.com.abril.nds.service;
+
+/**
+ * 
+ * Interface de serviços referentes a entidade
+ * {@link br.com.abril.nds.model.estoque.VendaEncalhe}  
+ * 
+ * @author Discover Technology
+ *
+ */
+public interface VendaEncalheService {
+
+}
