4ea784a43e623a5c209b5fdd5420fc5d2bec980e
6uimorais <6uimorais@gmail.com>
2012-06-01 08:12:15 -0300

    Correções - Tipo de Movimento Mapa de Abastecimento.

---
+++ nds-model/src/main/java/br/com/abril/nds/dto/AbastecimentoDTO.java
@@ -0,0 +1,106 @@
+package br.com.abril.nds.dto;
+
+import java.io.Serializable;
+
+public class AbastecimentoDTO implements Serializable {
+
+	private static final long serialVersionUID = -1022730482920692146L;
+	
+	private String data;
+	private Long idBox;
+	private String box;
+	private Integer totalProduto;
+	private Integer totalReparte;
+	private String totalBox;
+	
+	public AbastecimentoDTO() {
+		
+	}	
+	
+	public AbastecimentoDTO(String data, Long idBox, String box,
+			Integer totalProduto, Integer totalReparte, String totalBox) {
+		super();
+		this.data = data;
+		this.idBox = idBox;
+		this.box = box;
+		this.totalProduto = totalProduto;
+		this.totalReparte = totalReparte;
+		this.totalBox = totalBox;
+	}
+
+	/**
+	 * @return the data
+	 */
+	public String getData() {
+		return data;
+	}
+	/**
+	 * @param data the data to set
+	 */
+	public void setData(String data) {
+		this.data = data;
+	}
+	/**
+	 * @return the idBox
+	 */
+	public Long getIdBox() {
+		return idBox;
+	}
+	/**
+	 * @param idBox the idBox to set
+	 */
+	public void setIdBox(Long idBox) {
+		this.idBox = idBox;
+	}
+	/**
+	 * @return the box
+	 */
+	public String getBox() {
+		return box;
+	}
+	/**
+	 * @param box the box to set
+	 */
+	public void setBox(String box) {
+		this.box = box;
+	}
+	/**
+	 * @return the totalProduto
+	 */
+	public Integer getTotalProduto() {
+		return totalProduto;
+	}
+	/**
+	 * @param totalProduto the totalProduto to set
+	 */
+	public void setTotalProduto(Integer totalProduto) {
+		this.totalProduto = totalProduto;
+	}
+	/**
+	 * @return the totalReparte
+	 */
+	public Integer getTotalReparte() {
+		return totalReparte;
+	}
+	/**
+	 * @param totalReparte the totalReparte to set
+	 */
+	public void setTotalReparte(Integer totalReparte) {
+		this.totalReparte = totalReparte;
+	}
+	/**
+	 * @return the totalBox
+	 */
+	public String getTotalBox() {
+		return totalBox;
+	}
+	/**
+	 * @param totalBox the totalBox to set
+	 */
+	public void setTotalBox(String totalBox) {
+		this.totalBox = totalBox;
+	}
+	
+	
+	
+}
