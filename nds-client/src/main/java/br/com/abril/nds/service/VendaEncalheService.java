738d73f97a61160e922c871143b54505f8b42952
leandrolupianhez <leandro.lupianhez@gmail.com>
2012-06-11 08:26:33 -0300

    Atualização dos arquivos refrente a venda de encalhe

--- nds-client/src/main/java/br/com/abril/nds/service/VendaEncalheService.java
+++ nds-client/src/main/java/br/com/abril/nds/service/VendaEncalheService.java
@@ -4,11 +4,14 @@ import java.util.Date;
 import java.util.List;
 
 import br.com.abril.nds.dto.SlipVendaEncalheDTO;
+import br.com.abril.nds.dto.VendaEncalheDTO;
+import br.com.abril.nds.dto.filtro.FiltroVendaEncalheDTO;
+import br.com.abril.nds.model.estoque.TipoVendaEncalhe;
+
 
 /**
  * 
- * Interface de serviços referentes a entidade
- * {@link br.com.abril.nds.model.estoque.VendaEncalhe}  
+ * Interface de serviços referentes a venda de produtos de encalhe
  * 
  * @author Discover Technology
  *
@@ -34,6 +37,7 @@ public interface VendaEncalheService {
 	  * @return byte[]
 	 */
 	 byte[] geraImpressaoVendaEncalhe(long idCota, Date dataInicio, Date dataFim);
+
 	 
 	 
 	 /**
@@ -44,5 +48,61 @@ public interface VendaEncalheService {
 	  * @return byte[]
 	 */
 	 byte[] geraImpressaoVendaSuplementar(long idCota, Date dataInicio, Date dataFim);
+
+
+	 /**
+	 * Retorna os dados de uma determinada venda de encalhe
+	 * 
+	 * @param idVendaEncalhe - identificador da venda de encalhe 
+	 * 
+	 * @return VendaEncalheDTO
+	 */
+	VendaEncalheDTO buscarVendaEncalhe(Long idVendaEncalhe);
+	
+	/**
+	 * Efetiva uma venda de encalhe
+	 * 
+	 * @param vendaEncalheDTO - dados referente a vernda de encalhe 
+	 */
+	void efetivarVendaEncalhe(VendaEncalheDTO vendaEncalheDTO);
+	
+	/**
+	 * Exclui uma venda de encalhe efetivada
+	 * 
+	 * @param idVendaEncalhe - identificador da venda de encalhe
+	 */
+	void excluirVendaEncalhe(Long idVendaEncalhe);
+	
+	/**
+	 * Altera uma venda de encalhe efetivada.
+	 * 
+	 * @param vendaEncalheDTO - dados referente a vernda de encalhe 
+	 */
+	void alterarVendaEncalhe(VendaEncalheDTO vendaEncalheDTO);
+	
+	/**
+	 * Retorna o slip no formato PDF referente a venda de encalhe.
+	 *  
+	 * @param idVendaEncalhe - identificador da venda de encalhe
+	 * 
+	 * @return  byte[]
+	 * 
+	 */
+	byte[] gerarSlipVendaEncalhe(Long idVendaEncalhe);
+	
+	/**
+	 * 
+	 * Retorna as informações de um produto edição, candidato a venda de encalhe.
+	 * 
+	 * @param codigoProduto - código do produto
+	 * @param numeroEdicao - número da edição do produto
+	 * @param tipoVendaEncalhe - tipo de venda referente ao produto em estoque
+	 * @return VendaEncalheDTO
+	 */
+	VendaEncalheDTO buscarProdutoComEstoque(String codigoProduto,Long numeroEdicao, TipoVendaEncalhe tipoVendaEncalhe);
+	
+	List<VendaEncalheDTO> buscarVendasProduto(FiltroVendaEncalheDTO filtro);
+	
+	Long buscarQntVendasProduto(FiltroVendaEncalheDTO filtro);
 	
 }
