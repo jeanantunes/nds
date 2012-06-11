4a723b6a178a0f9f68f24ea56cca8766108fa4e3
murilolocatelli <murilolocatelli@gmail.com>
2012-06-11 15:32:14 -0300

    Desenvolvimento EMS 0165

--- nds-client/src/main/java/br/com/abril/nds/controllers/lancamento/MatrizLancamentoController.java
+++ nds-client/src/main/java/br/com/abril/nds/controllers/lancamento/MatrizLancamentoController.java
@@ -6,8 +6,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import br.com.abril.nds.client.vo.ResultadoResumoBalanceamentoVO;
 import br.com.abril.nds.client.vo.ValidacaoVO;
+import br.com.abril.nds.dto.BalanceamentoLancamentoDTO;
 import br.com.abril.nds.dto.LancamentoDTO;
+import br.com.abril.nds.dto.ProdutoLancamentoDTO;
 import br.com.abril.nds.dto.ResumoPeriodoBalanceamentoDTO;
 import br.com.abril.nds.dto.SumarioLancamentosDTO;
 import br.com.abril.nds.dto.filtro.FiltroLancamentoDTO;
@@ -62,6 +65,135 @@ public class MatrizLancamentoController {
 		result.include("data", data);
 		result.include("fornecedores", fornecedores);
 	}
+	
+	@Post
+	public void obterMatrizLancamento(Date dataLancamento, List<Long> idsFornecedores) {
+		
+		BalanceamentoLancamentoDTO balanceamentoLancamento = null;
+		
+		// TODO: criar filtro e setar na sessão
+		
+		// TODO: chamar o service para retornar a matriz de balanceamento
+		
+		// TODO: setar a matriz na sessão
+		
+		ResultadoResumoBalanceamentoVO resumoPeriodoBalanceamento =
+			obterResumoBalanceamentoLancamento(balanceamentoLancamento);
+		
+		this.result.use(Results.json()).from(resumoPeriodoBalanceamento, "result").recursive().serialize();
+	}
+	
+	@Post
+	public void obterGridMatrizLancamento(String sortorder, String sortname, int page, int rp) {
+		
+		BalanceamentoLancamentoDTO balanceamentoLancamento = null;
+		
+		// TODO: obter a matriz da sessão
+		
+		if (balanceamentoLancamento == null
+				|| balanceamentoLancamento.getMatrizLancamento() == null
+				|| balanceamentoLancamento.getMatrizLancamento().isEmpty()) {
+			
+			throw new ValidacaoException(
+				TipoMensagem.WARNING, "Não houve carga de informações para o período escolhido!");
+		}
+		
+		List<ProdutoLancamentoDTO> listaProdutoLancamento = null;
+		
+		// TODO: montar a lista de produtoLancamentoDTO através do map
+		
+		if (listaProdutoLancamento != null && !listaProdutoLancamento.isEmpty()) {
+			
+			PaginacaoVO paginacao = new PaginacaoVO(page, rp, sortorder);
+			
+			processarBalanceamento(listaProdutoLancamento, paginacao);
+			
+		} else {
+			
+			this.result.use(Results.json()).from(Results.nothing()).serialize();
+		}
+	}
+	
+	@Post
+	public void confirmarMatrizLancamento() {
+		
+		// TODO: obter a matriz da sessão
+		
+		// TODO: chamar a service de confirmação
+	}
+	
+	@Post
+	public void voltarConfiguracaoOriginal() {
+		
+		// TODO: montar a matriz inicial e setar na sessão
+	}
+	
+	@Post
+	public void reprogramarLancamentosSelecionados(List<ProdutoLancamentoDTO> listaProdutoLancamento,
+												   String novaDataFormatada, String dataAntigaFormatada) {
+		
+		// TODO: reprogramar os lançamentos selecionados
+		
+		// TODO: atualizar a matriz q estava na sessão
+		
+		// TODO: setar a matriz na sessão
+	}
+	
+	@Post
+	public void reprogramarLancamentoUnico(ProdutoLancamentoDTO produtoLancamento,
+										   String dataAntigaFormatada) {
+		
+		// TODO: reprogramar o lançamento informado
+		
+		// TODO: atualizar a matriz q estava na sessão
+		
+		// TODO: setar a matriz na sessão
+	}	
+	
+	/**
+	 * Obtém o resumo do período de balanceamento de lançamento.
+	 */
+	private ResultadoResumoBalanceamentoVO obterResumoBalanceamentoLancamento(
+											BalanceamentoLancamentoDTO balanceamentoLancamento) {
+		
+		// TODO: montar o resumo de balanceamento de lançamento
+		
+		return null;
+	}
+	
+	private void processarBalanceamento(List<ProdutoLancamentoDTO> listaProdutoLancamento,
+										PaginacaoVO paginacao) {
+		
+		TableModel<CellModelKeyValue<ProdutoLancamentoDTO>> tableModel =
+			new TableModel<CellModelKeyValue<ProdutoLancamentoDTO>>();
+		
+		tableModel.setPage(paginacao.getPaginaAtual());
+		//tableModel.setTotal(totalRegistros);
+		
+		List<CellModelKeyValue<ProdutoLancamentoDTO>> listaCellModel =
+			new ArrayList<CellModelKeyValue<ProdutoLancamentoDTO>>();
+		
+		CellModelKeyValue<ProdutoLancamentoDTO> cellModel = null;
+		
+		for (ProdutoLancamentoDTO dto : listaProdutoLancamento) {
+			
+			cellModel =
+				new CellModelKeyValue<ProdutoLancamentoDTO>(dto.getIdLancamento().intValue(),
+															dto);
+			
+			listaCellModel.add(cellModel);
+		}
+		
+		tableModel.setRows(listaCellModel);
+		
+		// TODO: montar o DTO ou VO (se necessário) para exibir no grid
+		
+		// TODO: paginação e ordenação em memória
+		
+		result.use(Results.json()).withoutRoot().from(tableModel).recursive().serialize();
+	}
+	
+	
 	
 	
 	@Post
