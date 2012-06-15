4b2fc9caf83af7bef2298ff5c4a63a4eccbf04ba
direx <diogo.vecchiati@gmail.com>
2012-06-15 16:36:11 -0300

    criacao da rotina de exportar NFes

--- nds-client/src/main/java/br/com/abril/nds/service/impl/NotaFiscalServiceImpl.java
+++ nds-client/src/main/java/br/com/abril/nds/service/impl/NotaFiscalServiceImpl.java
@@ -1,5 +1,10 @@
 package br.com.abril.nds.service.impl;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -10,6 +15,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import br.com.abril.nds.dto.ConsultaLoteNotaFiscalDTO;
 import br.com.abril.nds.dto.RetornoNFEDTO;
+import br.com.abril.nds.model.cadastro.ParametroSistema;
+import br.com.abril.nds.model.cadastro.TipoParametroSistema;
 import br.com.abril.nds.model.fiscal.nota.IdentificacaoEmitente;
 import br.com.abril.nds.model.fiscal.nota.InformacaoEletronica;
 import br.com.abril.nds.model.fiscal.nota.NotaFiscal;
@@ -18,6 +25,7 @@ import br.com.abril.nds.model.fiscal.nota.Status;
 import br.com.abril.nds.model.fiscal.nota.StatusProcessamentoInterno;
 import br.com.abril.nds.repository.NotaFiscalRepository;
 import br.com.abril.nds.service.NotaFiscalService;
+import br.com.abril.nds.service.ParametroSistemaService;
 
 
 /**
@@ -32,6 +40,9 @@ public class NotaFiscalServiceImpl implements NotaFiscalService {
 	
 	@Autowired
 	private NotaFiscalRepository notaFiscalDAO;
+	
+	@Autowired
+	private ParametroSistemaService parametroSistemaService;
 	
 	@Override
 	public Map<Long, Integer> obterTotalItensNotaFiscalPorCotaEmLote(
@@ -132,4 +143,57 @@ public class NotaFiscalServiceImpl implements NotaFiscalService {
 		this.notaFiscalDAO.merge(notaFiscal);
 		
 	}
+
+	/* (non-Javadoc)
+	 * @see br.com.abril.nds.service.NotaFiscalService#enviarNotaFiscal(java.lang.Long)
+	 */
+	@Override
+	@Transactional
+	public void enviarNotaFiscal(Long id) {
+		
+		NotaFiscal notaFiscal = this.notaFiscalDAO.buscarPorId(id);
+		
+		notaFiscal.setStatusProcessamentoInterno(StatusProcessamentoInterno.ENVIADA);
+		
+		this.notaFiscalDAO.merge(notaFiscal);
+	}
+	
+	/* (non-Javadoc)
+	 * @see br.com.abril.nds.service.NotaFiscalService#exportarNotasFiscais()
+	 */
+	@Override
+	@Transactional
+	public synchronized void exportarNotasFiscais() throws IOException {
+		
+		List<NotaFiscal> notasFiscaisParaExportacao =
+				this.notaFiscalDAO.obterListaNotasFiscaisPor(StatusProcessamentoInterno.GERADA);
+		
+		String dados = ""; //TODO: chamar rotina de gerar arquivo;		
+		
+		ParametroSistema pathNFEExportacao =
+				this.parametroSistemaService.buscarParametroPorTipoParametro(
+							TipoParametroSistema.PATH_INTERFACE_NFE_EXPORTACAO);
+		
+		File diretorioExportacaoNFE = new File(pathNFEExportacao.getValor());
+		
+		if (!diretorioExportacaoNFE.isDirectory()) {
+			throw new FileNotFoundException("O diretório de exportação parametrizado não é válido!");
+		}
+		
+		File notaExportacao = 
+				new File(diretorioExportacaoNFE + File.pathSeparator + new File("NFeExportacao"));
+		
+		FileWriter fileWriter;
+			
+		fileWriter = new FileWriter(notaExportacao);
+		
+		BufferedWriter buffer = new BufferedWriter(fileWriter);
+			
+		buffer.write(dados);
+		
+		for (NotaFiscal notaFiscal : notasFiscaisParaExportacao) {
+			this.enviarNotaFiscal(notaFiscal.getId());
+		}
+	}
+	
 }
