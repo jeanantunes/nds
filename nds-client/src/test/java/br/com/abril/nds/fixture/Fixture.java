ea85368f4a787bac9f927a7dc24b03a558de9182
Alexandre Takashi <alexandre.tamanaha@discover.com.br>
2012-06-17 17:58:58 -0300

    Inclusao de atributo.

--- nds-client/src/test/java/br/com/abril/nds/fixture/Fixture.java
+++ nds-client/src/test/java/br/com/abril/nds/fixture/Fixture.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Set;
 
 import br.com.abril.nds.model.DiaSemana;
+import br.com.abril.nds.model.LeiautePicking;
 import br.com.abril.nds.model.Origem;
 import br.com.abril.nds.model.StatusCobranca;
 import br.com.abril.nds.model.StatusConfirmacao;
@@ -588,6 +589,7 @@ public class Fixture {
 		distribuidor.setCapacidadeDistribuicao(new BigDecimal("10000"));
 		distribuidor.setCapacidadeRecolhimento(new BigDecimal("1000"));
 		distribuidor.setPreenchimentoAutomaticoPDV(true);
+		distribuidor.setLeiautePicking(LeiautePicking.DOIS);
 		
 		return distribuidor;
 	}
