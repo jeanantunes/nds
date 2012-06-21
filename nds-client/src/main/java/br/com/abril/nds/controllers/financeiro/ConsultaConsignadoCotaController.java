819fcdb70881cc06313cd2a75f68c7a6c8c07b11
Lazaro Junior <lazaro.prates@infoa2.com.br>
2012-06-20 22:26:25 -0300

    Comeando EMS 180

---
+++ nds-client/src/main/java/br/com/abril/nds/controllers/financeiro/ConsultaConsignadoCotaController.java
@@ -0,0 +1,15 @@
+package br.com.abril.nds.controllers.financeiro;
+
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Resource;
+
+@Resource
+@Path("/financeiro/consultaConsignadoCota")
+public class ConsultaConsignadoCotaController {
+	
+	@Path("/")
+	public void index(){
+		
+	}
+
+}
