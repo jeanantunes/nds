274041055490f253e60e2ac8f3cca4e484ad33ce
francisco-maximo <francisco-maximo@gmail.com>
2012-06-26 16:11:31 -0300

    Layout das mensagens modificado.

--- nds-client/src/main/java/br/com/abril/nds/service/impl/GerarCobrancaServiceImpl.java
+++ nds-client/src/main/java/br/com/abril/nds/service/impl/GerarCobrancaServiceImpl.java
@@ -179,6 +179,7 @@ public class GerarCobrancaServiceImpl implements GerarCobrancaService {
 			Fornecedor ultimoFornecedor = null;
 			
 			if (listaMovimentoFinanceiroCota.get(0).getMovimentos() != null && 
+					!listaMovimentoFinanceiroCota.get(0).getMovimentos().isEmpty() &&
 					listaMovimentoFinanceiroCota.get(0).getMovimentos().get(0) != null){
 				
 				ultimoFornecedor = listaMovimentoFinanceiroCota.get(0).getMovimentos().get(0).getProdutoEdicao().getProduto().getFornecedor();
@@ -193,7 +194,9 @@ public class GerarCobrancaServiceImpl implements GerarCobrancaService {
 			for (MovimentoFinanceiroCota movimentoFinanceiroCota : listaMovimentoFinanceiroCota){
 				
 				if (movimentoFinanceiroCota.getCota().equals(ultimaCota) &&
-						movimentoFinanceiroCota.getMovimentos() != null && movimentoFinanceiroCota.getMovimentos().get(0) != null &&
+						movimentoFinanceiroCota.getMovimentos() != null && 
+						!movimentoFinanceiroCota.getMovimentos().isEmpty() && 
+						movimentoFinanceiroCota.getMovimentos().get(0) != null &&
 						movimentoFinanceiroCota.getMovimentos().get(0).getProdutoEdicao().getProduto().getFornecedor().equals(ultimoFornecedor)){
 					
 					movimentos.add(movimentoFinanceiroCota);
@@ -219,6 +222,7 @@ public class GerarCobrancaServiceImpl implements GerarCobrancaService {
 					ultimaCota = movimentoFinanceiroCota.getCota();
 					
 					if (movimentoFinanceiroCota.getMovimentos() != null && 
+							!movimentoFinanceiroCota.getMovimentos().isEmpty() &&
 							movimentoFinanceiroCota.getMovimentos().get(0) != null){
 						
 						ultimoFornecedor = movimentoFinanceiroCota.getMovimentos().get(0).getProdutoEdicao().getProduto().getFornecedor();
