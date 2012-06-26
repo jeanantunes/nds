4ec8755a6a198ebdc1a42c10eadb1711242ec29f
Diego Fernandes <diego.fernandes@discover.com.br>
2012-06-26 11:08:03 -0300

    Alteração NCM para Long

--- nds-client/src/test/java/br/com/abril/nds/repository/impl/EstudoRepositoryImplTest.java
+++ nds-client/src/test/java/br/com/abril/nds/repository/impl/EstudoRepositoryImplTest.java
@@ -55,7 +55,7 @@ public class EstudoRepositoryImplTest extends AbstractRepositoryImplTest {
 		Fornecedor dinap = Fixture.fornecedorDinap(tipoFornecedorPublicacao);
 		save(dinap);
 		
-		TipoProduto tipoProduto = Fixture.tipoProduto("grupo teste", GrupoProduto.REVISTA, "ncm", null, 10L);
+		TipoProduto tipoProduto = Fixture.tipoProduto("grupo teste", GrupoProduto.REVISTA, 123L, null, 10L);
 		
 		save(tipoProduto);
 		
