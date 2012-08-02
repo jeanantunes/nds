6ed5da855c27b26ba5d3c3d0e05cc1138a2cc872
leandrolupianhez <leandro.lupianhez@gmail.com>
2012-08-02 09:43:03 -0300

    Ajuste Tipo Notas Fiscais

--- nds-client/src/main/java/br/com/abril/nds/client/vo/RegistroTipoNotaFiscalVO.java
+++ nds-client/src/main/java/br/com/abril/nds/client/vo/RegistroTipoNotaFiscalVO.java
@@ -9,14 +9,20 @@ import br.com.abril.nds.util.export.Exportable;
 public class RegistroTipoNotaFiscalVO implements Serializable {
 
 	private static final long serialVersionUID = -2039605389828028947L;
-
-	@Export(label = "Tipo de Nota", exhibitionOrder = 1)
+	
+	@Export(label="Operação", exhibitionOrder= 1)
+	private String tipoAtividade;
+	
+	@Export(label="Processo", exhibitionOrder=2)
+	private String processo;
+	
+	@Export(label = "Tipo de Nota", exhibitionOrder = 3)
 	private String nopDescricao;
 	
-	@Export(label = "CFOP Dentro UF", exhibitionOrder = 2)
+	@Export(label = "CFOP Dentro UF", exhibitionOrder = 4)
 	private String cfopEstado;
 	
-	@Export(label = "CFOP Fora UF", exhibitionOrder = 3)
+	@Export(label = "CFOP Fora UF", exhibitionOrder = 5)
 	private String cfopOutrosEstados;
 
 	public String getNopDescricao() {
@@ -42,5 +48,35 @@ public class RegistroTipoNotaFiscalVO implements Serializable {
 	public void setCfopOutrosEstados(String cfopOutrosEstados) {
 		this.cfopOutrosEstados = cfopOutrosEstados;
 	}
+
+	/**
+	 * @return the tipoAtividade
+	 */
+	public String getTipoAtividade() {
+		return tipoAtividade;
+	}
+
+	/**
+	 * @param tipoAtividade the tipoAtividade to set
+	 */
+	public void setTipoAtividade(String tipoAtividade) {
+		this.tipoAtividade = tipoAtividade;
+	}
+
+	/**
+	 * @return the processo
+	 */
+	public String getProcesso() {
+		return processo;
+	}
+
+	/**
+	 * @param processo the processo to set
+	 */
+	public void setProcesso(String processo) {
+		this.processo = processo;
+	}
+	
+	
 
 }
