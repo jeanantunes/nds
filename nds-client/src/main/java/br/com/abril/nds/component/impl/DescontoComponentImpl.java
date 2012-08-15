0ee7358fde2018c855a5be8ba810934d3578dbbe
leandrolupianhez <leandro.lupianhez@gmail.com>
2012-08-15 15:33:25 -0300

    Ajuste componente de desconto

--- nds-client/src/main/java/br/com/abril/nds/component/impl/DescontoComponentImpl.java
+++ nds-client/src/main/java/br/com/abril/nds/component/impl/DescontoComponentImpl.java
@@ -1,6 +1,7 @@
 package br.com.abril.nds.component.impl;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -49,8 +50,80 @@ public class DescontoComponentImpl implements DescontoComponent {
 	@Transactional(readOnly=true)
 	public Set<ProdutoEdicao> filtrarProdutosPassiveisDeDesconto(TipoDesconto tipoDesconto,Fornecedor fornecedor,Set<ProdutoEdicao> produtos) {
 		
-		//TODO criar logica de filtragem de produtos passiveis de desconto
+		Cota cota = null;
 		
-		return null;
+		switch (tipoDesconto) {
+			
+			case PRODUTO:
+				return produtos;
+			
+			case ESPECIFICO:
+				return filtrarProdutosCota(tipoDesconto,fornecedor,produtos,cota);
+			
+			case GERAL:
+				return filtrarProdutoDistribuidor(tipoDesconto,fornecedor,produtos);
+			
+			default:
+				throw new RuntimeException("Tipo de Desconto inválido!");
+		}
+	}
+
+	private Set<ProdutoEdicao> filtrarProdutoDistribuidor(TipoDesconto tipoDesconto, Fornecedor fornecedor,Set<ProdutoEdicao> produtos) {
+		
+		List<DescontoProdutoEdicao> descontosProduto = descontoProdutoEdicaoRepository.buscarDescontoProdutoEdicaoNotInTipoDesconto(tipoDesconto, fornecedor);
+		
+		if(descontosProduto.isEmpty()){
+			return produtos;
+		}
+		
+		for(ProdutoEdicao prd : produtos){
+			
+			if(this.contains(prd, descontosProduto,null)){
+				produtos.remove(prd);
+			}
+		}
+		
+		return produtos;
+	}
+
+	private Set<ProdutoEdicao> filtrarProdutosCota(TipoDesconto tipoDesconto, Fornecedor fornecedor,Set<ProdutoEdicao> produtos, Cota cota) {
+		
+		List<DescontoProdutoEdicao> descontosProduto = descontoProdutoEdicaoRepository.buscarDescontoProdutoEdicaoNotInTipoDesconto(tipoDesconto, fornecedor,cota);
+		
+		if(descontosProduto.isEmpty()){
+			return produtos;
+		}
+		
+		for(ProdutoEdicao prd : produtos){
+			
+			if(!this.contains(prd, descontosProduto,TipoDesconto.GERAL)){
+				produtos.remove(prd);
+			}
+		}
+		
+		return produtos;
+	}
+	
+	private boolean contains(ProdutoEdicao produtoEdicao, List<DescontoProdutoEdicao> descontos,TipoDesconto tipoDesconto){
+		
+		for(DescontoProdutoEdicao desc : descontos){
+			
+			if(desc.getProdutoEdicao().equals(produtoEdicao)){	
+				
+				if(tipoDesconto!= null){
+					
+					if(desc.getTipoDesconto().equals(tipoDesconto)){	
+						return true;
+					}
+					else{
+						return false;
+					}
+				}
+				else{
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 }
