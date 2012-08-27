545eddf1c9f2964387a4a7b742f87ec4061873a3
Gabriel Mancini <gabriel.mancini@gmail.com>
2012-08-27 19:45:25 -0300

    add todas as informacoes de correios no couchdb

--- ndsi-couchdbinterface/src/main/java/br/com/abril/nds/integracao/fileimporter/InterfaceExecutor.java
+++ ndsi-couchdbinterface/src/main/java/br/com/abril/nds/integracao/fileimporter/InterfaceExecutor.java
@@ -240,65 +240,51 @@ public class InterfaceExecutor {
 		try {
 			
 			Database db = Database.open(new File(diretorio + "dnecom.mdb"));
-			/*
+
 			Table tblBairro = db.getTable("LOG_BAIRRO");
+			Table tblLogradouro = db.getTable("LOG_LOGRADOURO");
+			Table tblLocalidade = db.getTable("LOG_LOCALIDADE");
+			Table tblUf = db.getTable("LOG_FAIXA_UF");
+			
+			
 			for(Map<String, Object> row : tblBairro) {
-				
-				System.out.println(
-						  row.get("BAI_NU").toString() 
-						  + row.get("UFE_SG").toString() 
-						  + row.get("LOC_NU").toString() 
-						  + row.get("BAI_NO").toString()  
-						  + row.get("BAI_NO_ABREV")
-				);
-				  
-				Bairro doc = new Bairro();
-				doc.setTipoDocumento("CEP");					
-				doc.setSubTipoDocumento("BAIRRO");
-				doc.set_id(row.get("BAI_NU").toString());
-				doc.setNome(row.get("BAI_NO").toString());
-				doc.setUf(row.get("UFE_SG").toString());
-				Localidade l = new Localidade();
-				l.set_id(row.get("LOC_NU").toString());
-				doc.setLocalidade(l);
-				try {
-					couchDbClient.save(doc);
-				} catch (CouchDbException ce) {
+					
+				if(row != null && !row.isEmpty()) {
+					
+					Bairro doc = new Bairro();
+					doc.setTipoDocumento("CEP");					
+					doc.setSubTipoDocumento("BAIRRO");
+					doc.set_id((row.get("BAI_NU") != null ? row.get("BAI_NU").toString() : "" ));
+					doc.setNome((row.get("BAI_NO") != null ? row.get("BAI_NO").toString() : "" ));
+					doc.setUf((row.get("UFE_SG") != null ? row.get("UFE_SG").toString() : "" ));
+					Localidade l = new Localidade();
+					l.set_id((row.get("LOC_NU") != null ? row.get("LOC_NU").toString() : "" ));
+					doc.setLocalidade(l);
+					try {
+						couchDbClient.save(doc);
+					} catch (CouchDbException ce) {
+						
+					}
 					
 				}
-				
 			}
 
-			Table tblLogradouro = db.getTable("LOG_LOGRADOURO");
 			for(Map<String, Object> row : tblLogradouro) {
 				
-				System.out.println(
-					  row.get("LOG_NU").toString() 
-					  + row.get("UFE_SG").toString() 
-					  + row.get("LOC_NU").toString() 
-					  + row.get("BAI_NU_INI").toString()  
-					  + row.get("BAI_NU_FIM").toString()
-					  + row.get("LOG_NO").toString()
-					  + row.get("LOG_COMPLEMENTO").toString()
-					  + row.get("CEP").toString()
-					  + row.get("TLO_TX").toString()
-					  + row.get("LOG_STA_TLO").toString()
-					  + row.get("LOG_NO_ABREV").toString()
-					  );
-				
+							
 				Logradouro doc = new Logradouro();
 				
 				doc.setTipoDocumento("CEP");					
 				doc.setSubTipoDocumento("LOGRADOURO");
-				doc.set_id(row.get("LOG_NU").toString());
-				doc.setNome(row.get("LOG_NO").toString());
-				doc.setComplemento(row.get("LOG_COMPLEMENTO").toString());
-				doc.setCep(row.get("CEP").toString());
-				doc.setUf(row.get("UFE_SG").toString());				
-				doc.setAbreviatura(row.get("LOG_NO_ABREV").toString());
+				doc.set_id((row.get("LOG_NU") != null ? row.get("LOG_NU").toString() : "" ));
+				doc.setNome((row.get("LOG_NO") != null ? row.get("LOG_NO").toString() : "" ));
+				doc.setComplemento((row.get("LOG_COMPLEMENTO") != null ? row.get("LOG_COMPLEMENTO").toString() : "" ));
+				doc.setCep((row.get("CEP") != null ? row.get("CEP").toString() : "" ));
+				doc.setUf((row.get("UFE_SG") != null ? row.get("UFE_SG").toString() : "" ));				
+				doc.setAbreviatura((row.get("LOG_NO_ABREV") != null ? row.get("LOG_NO_ABREV").toString() : "" ));
 				
 				Localidade l = new Localidade();
-				l.set_id(row.get("LOC_NU").toString());
+				l.set_id((row.get("LOC_NU") != null ? row.get("LOC_NU").toString() : "" ));
 				doc.setLocalidade(l);
 				try {
 					couchDbClient.save(doc);
@@ -307,32 +293,21 @@ public class InterfaceExecutor {
 				}
 			}
 			
-			Table tblLocalidade = db.getTable("LOG_LOCALIDADE");
+			
 			for(Map<String, Object> row : tblLocalidade) {
 				 
-				System.out.println(
-					  row.get("LOC_NU").toString() 
-					  + row.get("UFE_SG").toString() 
-					  + row.get("LOC_NO").toString() 
-					  + row.get("CEP").toString()  
-					  + row.get("LOC_IN_SIT").toString()
-					  + row.get("LOC_IN_TIPO_LOC").toString()
-					  + row.get("LOC_NU_SUB").toString()
-					  + row.get("LOC_NO_ABREV").toString()
-					  + row.get("MUN_NU").toString()
-					  );
-				
 				Localidade doc = new Localidade();
 				
 				doc.setTipoDocumento("CEP");					
 				doc.setSubTipoDocumento("LOCALIDADE");
-				doc.set_id(row.get("LOC_NU").toString());
-				doc.setNome(row.get("LOG_NO").toString());
-				doc.setCep(row.get("CEP").toString());
-				doc.setAbreviatura(row.get("LOG_NO_ABREV").toString());
+				doc.set_id((row.get("LOC_NU") != null ? row.get("LOC_NU").toString() : "" ));
+				doc.setNome((row.get("LOG_NO") != null ? row.get("LOG_NO").toString() : "" ));
+				doc.setCep((row.get("CEP") != null ? row.get("CEP").toString() : "" ));
+				doc.setAbreviatura((row.get("LOG_NO_ABREV") != null ? row.get("LOG_NO_ABREV").toString() : "" ));
+				doc.setCodigoMunicipioIBGE((row.get("MUN_NU") != null ? Long.valueOf( row.get("MUN_NU").toString() ) : null ));
 				
 				UnidadeFederacao u = new UnidadeFederacao();
-				u.set_id(row.get("UFE_SG").toString());
+				u.set_id((row.get("UFE_SG") != null ? row.get("UFE_SG").toString() : "" ));
 				doc.setUnidadeFederacao(u);
 				try {
 					couchDbClient.save(doc);
@@ -341,36 +316,25 @@ public class InterfaceExecutor {
 				}
 			}
 
-			Table tblUf = db.getTable("LOG_FAIXA_UF");
+			
 			for(Map<String, Object> row : tblUf) {
-				
-				System.out.println(						
-					  row.get("UFE_SG").toString() 
-					  + row.get("UFE_CEP_INI").toString() 
-					  + row.get("UFE_CEP_FIM").toString() 						  
-					  );
+								
 				
 				UnidadeFederacao doc = new UnidadeFederacao();
 				
 				doc.setTipoDocumento("CEP");					
 				doc.setSubTipoDocumento("LOCALIDADE");
-				doc.set_id(row.get("UFE_SG").toString());
-				doc.setFaixaCepInicial(row.get("UFE_CEP_INI").toString() );
-				doc.setFaixaCepFinal(row.get("UFE_CEP_FIM").toString() );
+				doc.set_id((row.get("UFE_SG") != null ? row.get("UFE_SG").toString() : "" ));
+				doc.setFaixaCepInicial((row.get("UFE_CEP_INI") != null ? row.get("UFE_CEP_INI").toString() : "" ));
+				doc.setFaixaCepFinal((row.get("UFE_CEP_FIM") != null ? row.get("UFE_CEP_FIM").toString() : "" ));
 				try {
 					couchDbClient.save(doc);
 				} catch (CouchDbException ce) {
 					
 				}
 			}
-			*/
 			
-			Table tblBairro = db.getTable("LOG_BAIRRO");
-			Table tblLogradouro = db.getTable("LOG_LOGRADOURO");
-			Table tblLocalidade = db.getTable("LOG_LOCALIDADE");
-			Table tblUf = db.getTable("LOG_FAIXA_UF");
-			
-			
+						
 			for(Map<String, Object> rowLog : tblLogradouro) {
 				
 				if(rowLog != null && !rowLog.isEmpty()) {
