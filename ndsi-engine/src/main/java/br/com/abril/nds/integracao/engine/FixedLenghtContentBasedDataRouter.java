808abb1dc09604a1e72e311a7e5286a2d31aaecc
Alexandre Takashi [VF-1A] <alexandre.takashi@gmail.com>
2012-08-31 18:45:41 -0300

    Limpeza de import

--- ndsi-engine/src/main/java/br/com/abril/nds/integracao/engine/FixedLenghtContentBasedDataRouter.java
+++ ndsi-engine/src/main/java/br/com/abril/nds/integracao/engine/FixedLenghtContentBasedDataRouter.java
@@ -11,10 +11,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.TransactionCallback;
 import org.springframework.transaction.support.TransactionTemplate;
 
