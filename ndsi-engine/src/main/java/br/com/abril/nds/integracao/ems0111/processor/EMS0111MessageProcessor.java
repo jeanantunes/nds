66d191345af3321b0eeb7382ebbf31b1c1d7bc05
Alexandre Takashi - SDF-1 <alexandre.takashi@me.com>
2012-09-02 02:13:22 -0300

    Implementação da regra de negócio para novo lançamento

--- ndsi-engine/src/main/java/br/com/abril/nds/integracao/ems0111/processor/EMS0111MessageProcessor.java
+++ ndsi-engine/src/main/java/br/com/abril/nds/integracao/ems0111/processor/EMS0111MessageProcessor.java
@@ -1,5 +1,6 @@
 package br.com.abril.nds.integracao.ems0111.processor;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.Calendar;
 import java.util.Date;
@@ -15,6 +16,7 @@ import br.com.abril.nds.integracao.engine.MessageProcessor;
 import br.com.abril.nds.integracao.engine.data.Message;
 import br.com.abril.nds.integracao.engine.log.NdsiLoggerFactory;
 import br.com.abril.nds.integracao.model.canonic.EMS0111Input;
+import br.com.abril.nds.integracao.service.DistribuidorService;
 import br.com.abril.nds.model.cadastro.ProdutoEdicao;
 import br.com.abril.nds.model.integracao.EventoExecucaoEnum;
 import br.com.abril.nds.model.planejamento.Lancamento;
@@ -47,6 +49,9 @@ public class EMS0111MessageProcessor extends AbstractRepository implements
 	@Autowired
 	private NdsiLoggerFactory ndsiLoggerFactory;
 
+	@Autowired
+	private DistribuidorService distribuidorService;
+	
 	@Override
 	public void preProcess() {
 		// TODO Auto-generated method stub
@@ -84,6 +89,70 @@ public class EMS0111MessageProcessor extends AbstractRepository implements
 		}
 		
 		
+		// Verificação de alteração do Preço Previsto para o ProdutoEdiçao:
+		BigDecimal precoPrevisto = input.getPrecoPrevisto();
+		if (!produtoEdicao.getPrecoPrevisto().equals(precoPrevisto)) {
+			this.ndsiLoggerFactory.getLogger().logError(message,
+					EventoExecucaoEnum.RELACIONAMENTO,
+					"Alteracao do Preco Previsto do Produto: "
+							+ codigoProduto
+							+ " e Edicao: " + edicao
+							+ " , de: " + produtoEdicao.getPrecoPrevisto() 
+							+ "para: " + precoPrevisto);
+			produtoEdicao.setPrecoPrevisto(precoPrevisto);
+			this.getSession().merge(produtoEdicao);
+		}
+		
+		
+		Date dataLancamento = input.getDataLancamento();
+		Date dataGeracaoArquivo = input.getDataGeracaoArquivo();
+		Lancamento lancamento = this.getLancamentoPrevistoMaisProximo(
+				produtoEdicao, dataLancamento, dataGeracaoArquivo);
+		if (lancamento == null ) {
+			
+			// Cadastrar novo lançamento
+			lancamento = new Lancamento();
+			
+			// Cálcular data de recolhimento
+			Calendar calRecolhimento = Calendar.getInstance();
+			calRecolhimento.setTime(input.getDataLancamento());
+			calRecolhimento.add(Calendar.DAY_OF_MONTH, produtoEdicao.getPeb());
+			Date dataRecolhimento = calRecolhimento.getTime();
+
+			// Data da Operação do sistema:
+			Date dataOperacao = distribuidorService.obter().getDataOperacao();
+			lancamento.setDataStatus(dataOperacao);
+			
+			lancamento.setId(null);
+			lancamento.setProdutoEdicao(produtoEdicao);
+			lancamento.setDataLancamentoPrevista(input.getDataLancamento());
+			lancamento.setTipoLancamento(parseTipo(input.getTipoLancamento()));
+			lancamento.setReparte(BigInteger.valueOf(input.getRepartePrevisto()));
+			lancamento.setStatus(StatusLancamento.TRANSMITIDO);
+			lancamento.setRepartePromocional(BigInteger.valueOf(input.getRepartePromocional()));// confirmado
+			lancamento.setDataCriacao(new Date());// confirmado
+			lancamento.setDataLancamentoDistribuidor(input.getDataLancamento());// confirmado
+			lancamento.setDataRecolhimentoDistribuidor(dataRecolhimento);// confirmado
+			lancamento.setDataRecolhimentoPrevista(dataRecolhimento);// confirmado
+			lancamento.setExpedicao(null);// default
+			lancamento.setHistoricos(null);// default
+			lancamento.setRecebimentos(null);// default
+			lancamento.setNumeroReprogramacoes(null);// confirmado
+			lancamento.setSequenciaMatriz(null);// confirmado				
+
+			// EFETIVAR INSERCAO NA BASE
+			getSession().persist(lancamento);			
+		} else {
+			
+			// Atualizar lançamento
+			
+			StatusLancamento status = lancamento.getStatus();
+			
+			
+		}
+		
+		
+		
 
 		if (null != produtoEdicao) {
 			// SE EXISTIR PRODUTO/EDICAO NA TABELA
@@ -103,7 +172,7 @@ public class EMS0111MessageProcessor extends AbstractRepository implements
 			query.setParameter("produtoEdicao", produtoEdicao);
 			query.setParameter("dataLancamento", input.getDataLancamento());
 
-			Lancamento lancamento = (Lancamento) query.uniqueResult();
+//			Lancamento lancamento = (Lancamento) query.uniqueResult();
 			if (null != lancamento) {
 
 				// VERIFICAR SE OS CAMPOS ESTAO DESATUALIZADOS
@@ -162,36 +231,6 @@ public class EMS0111MessageProcessor extends AbstractRepository implements
 
 				}
 
-			} else {
-				// NAO EXISTE LANCAMENTO PARA O PRODUTO/EDICAO INFORMADO
-				// INSERIR LANCAMENTO
-
-				lancamento = new Lancamento();
-				Calendar data = Calendar.getInstance();
-
-				data.add(Calendar.DAY_OF_MONTH, produtoEdicao.getPeb());
-
-				lancamento.setId(null);
-				lancamento.setProdutoEdicao(produtoEdicao);
-				lancamento.setDataLancamentoPrevista(input.getDataLancamento());
-				lancamento.setTipoLancamento(parseTipo(input.getTipoLancamento()));
-				lancamento.setReparte(new BigInteger( input.getRepartePrevisto().toString()));
-				lancamento.setStatus(StatusLancamento.PLANEJADO);// confirmado
-				lancamento.setRepartePromocional(new BigInteger( input.getRepartePromocional().toString()));// confirmado
-				lancamento.setDataCriacao(new Date());// confirmado
-				lancamento.setDataLancamentoDistribuidor(input.getDataLancamento());// confirmado
-				lancamento.setDataRecolhimentoDistribuidor(data.getTime());// confirmado
-				lancamento.setDataRecolhimentoPrevista(data.getTime());// confirmado
-				lancamento.setDataStatus(new Date());// confirmado
-				lancamento.setExpedicao(null);// default
-				lancamento.setHistoricos(null);// default
-				lancamento.setRecebimentos(null);// default
-				lancamento.setNumeroReprogramacoes(null);// confirmado
-				lancamento.setSequenciaMatriz(null);// confirmado				
-
-				// EFETIVAR INSERCAO NA BASE
-				getSession().persist(lancamento);
-
 			}
 
 		}
@@ -227,6 +266,40 @@ public class EMS0111MessageProcessor extends AbstractRepository implements
 		}
 	}	
 	
+	
+	/**
+	 * Obtém o Lançamento com data de lançamento mais próximo da data de 
+	 * lançamento previsto.
+	 *  
+	 * @param produtoEdicao
+	 * @param dataLancamento Data de Lançamento da Edição.
+	 * @param dataGeracaoArquivo Data de Geração do Arquivo.
+	 * 
+	 * @return
+	 */
+	private Lancamento getLancamentoPrevistoMaisProximo(
+			ProdutoEdicao produtoEdicao, Date dataLancamento, 
+			Date dataGeracaoArquivo) {
+		
+		StringBuilder sql = new StringBuilder();
+		
+		sql.append("SELECT lcto FROM Lancamento lcto ");
+		sql.append("      JOIN FETCH lcto.produtoEdicao pe ");
+		sql.append("    WHERE pe = :produtoEdicao ");
+		sql.append("      AND lcto.dataLancamentoPrevista > :dataGeracaoArquivo ");
+		sql.append("      AND lcto.dataLancamentoPrevista = :dataOperacao ");
+		sql.append(" ORDER BY lcto.dataLancamentoPrevista ASC");
+		
+		Query query = getSession().createQuery(sql.toString());
+		query.setParameter("produtoEdicao", produtoEdicao);
+		query.setDate("dataGeracaoArquivo", dataGeracaoArquivo);
+		query.setDate("dataLancamento", dataLancamento);
+		
+		query.setMaxResults(1);
+		query.setFetchSize(1);
+		
+		return (Lancamento) query.uniqueResult();
+	}
 	
 	@Override
 	public void posProcess() {
