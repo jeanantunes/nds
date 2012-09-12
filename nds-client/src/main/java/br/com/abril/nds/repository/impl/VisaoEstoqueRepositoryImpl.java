3a715f51d9f2f6a60604baadeb477e58fb6b251d
Victor Montanher <vhmontanher2@gmail.com>
2012-09-12 16:04:23 -0300

    Classes VisaoEstoque adicionadas

---
+++ nds-client/src/main/java/br/com/abril/nds/repository/impl/VisaoEstoqueRepositoryImpl.java
@@ -0,0 +1,10 @@
+package br.com.abril.nds.repository.impl;
+
+import org.springframework.stereotype.Repository;
+
+import br.com.abril.nds.repository.VisaoEstoqueRepository;
+
+@Repository
+public class VisaoEstoqueRepositoryImpl implements VisaoEstoqueRepository{
+	
+}
