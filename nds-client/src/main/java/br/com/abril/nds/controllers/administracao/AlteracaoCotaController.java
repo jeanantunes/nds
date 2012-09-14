1721e83fba297126552e31dc84c8f489162bf1ec
Jonatas de Moraes Junior <jonatas.junior@discover.com.br>
2012-09-14 11:35:56 -0300

    Preparação tela Alteração / Cota

--- nds-client/src/main/java/br/com/abril/nds/controllers/administracao/AlteracaoCotaController.java
+++ nds-client/src/main/java/br/com/abril/nds/controllers/administracao/AlteracaoCotaController.java
@@ -1,12 +1,18 @@
 package br.com.abril.nds.controllers.administracao;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 import br.com.abril.nds.client.annotation.Rules;
+import br.com.abril.nds.model.cadastro.Fornecedor;
+import br.com.abril.nds.model.cadastro.TipoEntrega;
 import br.com.abril.nds.model.seguranca.Permissao;
+import br.com.abril.nds.service.FornecedorService;
+import br.com.abril.nds.service.TipoEntregaService;
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
@@ -15,6 +21,12 @@ import br.com.caelum.vraptor.Result;
 @Path("administracao/alteracaoCota")
 public class AlteracaoCotaController {
 
+	@Autowired
+	private FornecedorService fornecedorService;
+	
+	@Autowired
+	private TipoEntregaService tipoEntregaService;
+	
 	@Autowired
 	private HttpServletResponse httpServletResponse;
 	
@@ -32,6 +44,12 @@ public class AlteracaoCotaController {
 	@Rules(Permissao.ROLE_ADMINISTRACAO_ALTERACAO_COTA)
 	public void index()
 	{
+		List<Fornecedor> listFornecedores = fornecedorService.obterFornecedores();
+		List<TipoEntrega> listTipoEntrega = tipoEntregaService.obterTodos();
 		
+		
+		
+		result.include("listFornecedores", listFornecedores);
+		result.include("listTipoEntrega", listTipoEntrega);
 	}
 }
