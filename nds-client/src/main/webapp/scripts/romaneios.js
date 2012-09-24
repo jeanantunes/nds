8590f0108c6820ac0efde214d6d2b3fd0dc9077b
francisco-maximo <francisco-maximo@gmail.com>
2012-09-24 17:18:42 -0300

    .

--- nds-client/src/main/webapp/scripts/romaneios.js
+++ nds-client/src/main/webapp/scripts/romaneios.js
@@ -2,7 +2,7 @@ var romaneiosController = $.extend(true, {
 	
 	init : function () {
 		$(".romaneiosGrid", romaneiosController.workspace).flexigrid({
-			preProcess: executarPreProcessamento,
+			preProcess: romaneiosController.executarPreProcessamento,
 			dataType : 'json',
 			colModel : [ {
 				display : 'Cota',
@@ -55,8 +55,17 @@ var romaneiosController = $.extend(true, {
 			showTableToggleBtn : true,
 			width : 960,
 			height : 150
-		});		
-
+		});
+		
+		$( "#dataLancamento", romaneiosController.workspace ).datepicker({
+			showOn: "button",
+			buttonImage: contextPath +"/scripts/jquery-ui-1.8.16.custom/development-bundle/demos/datepicker/images/calendar.gif",
+			buttonImageOnly: true
+		});
+		
+		$("#selectProdutos", romaneiosController.workspace).multiselect({
+			selectedList : 6
+		});
 	},
 	
 	mostrar : function(){
@@ -71,16 +80,28 @@ var romaneiosController = $.extend(true, {
 			$(".dadosFiltro", romaneiosController.workspace).show();		
 	},
 
-	pesquisar : function(){	
-		$(".romaneiosGrid").flexOptions({
+	pesquisar : function(){
+		
+		var params = [];
+		
+		params.push({name: 'filtro.data',      value: $("#dataLancamento", romaneiosController.workspace).val()});
+		params.push({name: 'filtro.idBox',     value:$('#idBox', romaneiosController.workspace).val()});
+		params.push({name: 'filtro.idRoteiro', value:$('#idRoteiro', romaneiosController.workspace).val()});
+		params.push({name: 'filtro.idRota',    value:$('#idRota', romaneiosController.workspace).val()});
+		params.push({name: 'filtro.nomeRota',  value: $('#idRota option:selected', romaneiosController.workspace).text()});
+		
+		var produtos = $("#selectProdutos", romaneiosController.workspace).val();
+		
+		if (produtos){
+			$.each(produtos, function(index, value){
+				params.push({name: 'filtro.produtos['+index+']', value: value});
+			});
+		}
+		
+		$(".romaneiosGrid", romaneiosController.workspace).flexOptions({
 			url: contextPath + "/romaneio/pesquisarRomaneio",
 			dataType : 'json',
-			params: [
-						{name:'filtro.idBox', value:$('#idBox', romaneiosController.workspace).val()},
-						{name:'filtro.idRoteiro', value:$('#idRoteiro', romaneiosController.workspace).val()},
-						{name:'filtro.idRota', value:$('#idRota', romaneiosController.workspace).val()},
-						{name:'filtro.nomeRota',	value: $('#idRota option:selected', romaneiosController.workspace).text()}
-						]
+			params: params
 		});
 		
 		$(".romaneiosGrid", romaneiosController.workspace).flexReload();
@@ -104,6 +125,47 @@ var romaneiosController = $.extend(true, {
 		$(".grids", romaneiosController.workspace).show();
 		
 		return resultado;
+	},
+	
+	pesquisarProdutos : function(){
+		
+		var data = $("#dataLancamento", romaneiosController.workspace).val();
+		
+		if (data){
+		
+			$.postJSON(contextPath + "/romaneio/carregarProdutosDataLancamento",
+				"data=" + data,
+				function(result) {
+					
+					if (result.mensagens) {
+		
+						exibirMensagem(
+							result.mensagens.tipoMensagem, 
+							result.mensagens.listaMensagens
+						);
+						
+						$(".grids", romaneiosController.workspace).hide();
+					}
+					
+					var itens = '';
+					
+					$.each(result, function(index, result) {
+						itens += '<option value="'+ result.key.$ +'">'+ result.value.$ +'</option>';
+					});
+					
+					$("#selectProdutos", romaneiosController.workspace).multiselect("destroy");
+					$("#selectProdutos", romaneiosController.workspace).html("");
+					$("#selectProdutos", romaneiosController.workspace).append(itens).multiselect();
+				}
+			);
+		} else {
+			
+			$("#selectProdutos", romaneiosController.workspace).multiselect("destroy");
+			$("#selectProdutos", romaneiosController.workspace).html("");
+			$("#selectProdutos", romaneiosController.workspace).multiselect();
+		}
 	}
 		
 }, BaseController);
+
+//@ sourceURL=romaneios.js
