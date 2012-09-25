6a5c795985239e2a8ee8f1596790a32cd3dee280
Jonatas de Moraes Junior <jonatas.junior@discover.com.br>
2012-09-25 15:38:48 -0300

    Relatorio Tipos Produtos - desenvolvimento

--- nds-client/src/main/java/br/com/abril/nds/controllers/lancamento/RelatorioTiposProdutosController.java
+++ nds-client/src/main/java/br/com/abril/nds/controllers/lancamento/RelatorioTiposProdutosController.java
@@ -1,20 +1,50 @@
 package br.com.abril.nds.controllers.lancamento;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 import br.com.abril.nds.client.annotation.Rules;
+import br.com.abril.nds.client.vo.RelatorioTiposProdutosVO;
+import br.com.abril.nds.dto.RelatorioTiposProdutosDTO;
+import br.com.abril.nds.dto.filtro.FiltroRelatorioTiposProdutos;
+import br.com.abril.nds.integracao.service.DistribuidorService;
+import br.com.abril.nds.model.cadastro.Distribuidor;
+import br.com.abril.nds.model.cadastro.TipoProduto;
 import br.com.abril.nds.model.seguranca.Permissao;
+import br.com.abril.nds.model.seguranca.Usuario;
+import br.com.abril.nds.serialization.custom.FlexiGridJson;
+import br.com.abril.nds.service.RelatorioTiposProdutosService;
+import br.com.abril.nds.service.TipoProdutoService;
+import br.com.abril.nds.util.export.FileExporter;
+import br.com.abril.nds.util.export.FileExporter.FileType;
+import br.com.abril.nds.util.export.NDSFileHeader;
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.view.Results;
 
 @Resource
 @Path("lancamento/relatorioTiposProdutos")
 public class RelatorioTiposProdutosController {
 
+	private static final String FILTRO_RELATORIO_TIPOS_PRODUTOS = "FILTRO_RELATORIO_TIPOS_PRODUTOS";
+	
+	@Autowired
+	private TipoProdutoService tipoProdutoService; 
+	
+	@Autowired
+	private DistribuidorService distribuidorService;
+	
+	@Autowired
+	private RelatorioTiposProdutosService service;
+	
 	@Autowired
 	private HttpServletResponse httpServletResponse;
 	
@@ -33,7 +63,74 @@ public class RelatorioTiposProdutosController {
 	@Path("/")
 	@Rules(Permissao.ROLE_LANCAMENTO_RELATORIO_TIPOS_PRODUTOS)
 	public void index()
-	{}
+	{
+		List<TipoProduto> listTipoProduto = tipoProdutoService.obterTodosTiposProduto();
+		result.include("listTipoProduto", listTipoProduto);
+	}
 	
 	
+	@Path("/pesquisar.json")
+	public void pesquisar(FiltroRelatorioTiposProdutos filtro, String sortname, String sortorder, int rp, int page) {
+		
+		this.session.setAttribute(FILTRO_RELATORIO_TIPOS_PRODUTOS, filtro);
+		
+		List<RelatorioTiposProdutosVO> list = this.convertList(service.gerarRelatorio(filtro));
+		result.use(FlexiGridJson.class).from(list).total(list.size()).page(page).serialize();
+	}
+	
+	
+	@Path("/exportar")
+	public void exportar(FileType fileType) throws IOException {
+		
+		FiltroRelatorioTiposProdutos filtro = (FiltroRelatorioTiposProdutos) this.session.getAttribute(FILTRO_RELATORIO_TIPOS_PRODUTOS);
+		List<RelatorioTiposProdutosVO> list = this.convertList(service.gerarRelatorio(filtro));
+		
+		FileExporter.to("relatorio-tipos-produtos", fileType).inHTTPResponse(
+				this.getNDSFileHeader(), null, null,
+				list, RelatorioTiposProdutosVO.class,
+				this.httpServletResponse);
+		
+		result.use(Results.nothing());
+	}
+	
+	
+	private List<RelatorioTiposProdutosVO> convertList(List<RelatorioTiposProdutosDTO> listDTO) {
+		
+		List<RelatorioTiposProdutosVO> listVO = new ArrayList<RelatorioTiposProdutosVO>();
+		
+		if (listDTO != null) {
+			for (RelatorioTiposProdutosDTO dto : listDTO) {
+				listVO.add(new RelatorioTiposProdutosVO(dto));
+			}
+		}
+		
+		return listVO;
+	}
+	
+	
+	private NDSFileHeader getNDSFileHeader() {
+
+		NDSFileHeader ndsFileHeader = new NDSFileHeader();
+		Distribuidor distribuidor = distribuidorService.obter();
+
+		if (distribuidor != null) {
+			ndsFileHeader.setNomeDistribuidor(distribuidor.getJuridica().getRazaoSocial());
+			ndsFileHeader.setCnpjDistribuidor(distribuidor.getJuridica().getCnpj());
+		}
+
+		ndsFileHeader.setData(new Date());
+		ndsFileHeader.setNomeUsuario(getUsuario().getNome());
+		return ndsFileHeader;
+	}
+	
+	
+	// TODO: não há como reconhecer usuario, ainda
+	private Usuario getUsuario() {
+
+		Usuario usuario = new Usuario();
+		usuario.setId(1L);
+		usuario.setNome("Jornaleiro da Silva");
+
+		return usuario;
+	}
 }
--- nds-client/src/main/webapp/WEB-INF/jsp/relatorioTiposProdutos/index.jsp
+++ nds-client/src/main/webapp/WEB-INF/jsp/relatorioTiposProdutos/index.jsp
@@ -4,8 +4,48 @@
 
 <body>
 
+	<fieldset class="classFieldset">
+   	    <legend> Pesquisar Tipos de Produtos</legend>
+   	    
+        <table width="950" border="0" cellpadding="2" cellspacing="1" class="filtro">
+            <tr>
+              	<td width="77">Tipo Produto:</td>
+              	<td colspan="3">
+              		<select name="filtro.tipoProduto" style="width:120px;">
+                		<option selected="selected" value="-1"></option>
+						<c:forEach items="${listTipoProduto}" var="tipoProduto">
+							<option value="${tipoProduto.id}">${tipoProduto.descricao}</option>
+						</c:forEach>
+              		</select>
+              	</td>
+              	<td width="71">Lan&ccedil;amento:</td>
+              	<td width="87"><input type="text" name="filtro.dataLancamentoDe" id="dateLanctoDe" style="width:60px;"/></td>
+              	<td width="22">At&eacute;:</td>
+              	<td width="89"><input type="text" name="filtro.dataLancamentoAte" id="dateLanctoAte" style="width:60px;"/></td>
+              	<td width="79">Recolhimento:</td>
+              	<td width="94"><input type="text" name="filtro.dataRecolhimentoDe" id="dateRecoltoDe" style="width:70px;"/></td>
+              	<td width="25">At&eacute;:</td>
+              	<td width="113"><input type="text" name="filtro.dataRecolhimentoAte" id="dateRecoltoAte" style="width:70px;"/></td>
+              	<td width="107"><span class="bt_pesquisar"><a href="javascript:;" onclick="relatorioTiposProdutosController.pesquisar();"></a></span></td>
+            </tr>
+        </table>
+    </fieldset>
 
+    <div class="linha_separa_fields">&nbsp;</div>
+
+    <div class="grids" style="display:none;">
+	    <fieldset class="classFieldset">
+    		<legend>Tipos de Produtos Cadastrados</legend>
+        	<table class="tiposProdutosGrid"></table>
+            <span class="bt_arquivo"><a href="${pageContext.request.contextPath}/lancamento/relatorioTiposProdutos/exportar?fileType=XLS">Arquivo</a></span>
+			<span class="bt_imprimir"><a href="${pageContext.request.contextPath}/lancamento/relatorioTiposProdutos/exportar?fileType=PDF">Imprimir</a></span>
+        </fieldset>
+    </div>
+    
+    <div class="linha_separa_fields">&nbsp;</div>
 	
+	
+	 
 	<script type="text/javascript">
 		$(function(){
 			relatorioTiposProdutosController.init();
