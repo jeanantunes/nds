8f1fb7d33c42b6f57da8d50d18099f0f920679bc
Diego Osse Fernandes <diego.fernandes@discover.com.br>
2012-10-02 11:54:02 -0300

    EMS 0001 – Conta Corrente Cota

--- nds-client/src/main/java/br/com/abril/nds/controllers/financeiro/ContaCorrenteCotaController.java
+++ nds-client/src/main/java/br/com/abril/nds/controllers/financeiro/ContaCorrenteCotaController.java
@@ -468,9 +468,7 @@ public class ContaCorrenteCotaController {
 		paginacao.setPaginaAtual(page);
 
 		filtroViewContaCorrenteCotaDTO
-				.setColunaOrdenacao(Util.getEnumByStringValue(
-						FiltroViewContaCorrenteCotaDTO.ColunaOrdenacao.values(),
-						sortname));
+				.setColunaOrdenacao(sortname);
 	}
 
 	/**
