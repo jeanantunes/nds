bb5902eafbed1d54f253b1696cd70fcb7eaca8a4
FernandoTAA <fernandoftaa@yahoo.com.br>
2012-10-03 15:21:57 -0300

    Ajuste EMS 0091

---
+++ nds-client/src/main/java/br/com/abril/nds/repository/RoteiroRepository.java.LOCAL.java
@@ -0,0 +1,34 @@
+package br.com.abril.nds.repository;
+
+import java.util.List;
+
+import org.hibernate.criterion.MatchMode;
+
+import br.com.abril.nds.model.cadastro.Roteiro;
+import br.com.abril.nds.vo.PaginacaoVO.Ordenacao;
+
+public interface RoteiroRepository extends Repository<Roteiro, Long> {
+	
+	List<Roteiro> buscarRoteiro(String sortname, Ordenacao ordenacao);
+	
+	List<Roteiro> buscarRoteiroPorDescricao(String descricao,  MatchMode matchMode);
+	
+	void atualizaOrdenacao(Roteiro roteiro);
+	
+	List<Roteiro> buscarRoteiroDeBox(Long idBox);
+	
+	Integer buscarMaiorOrdemRoteiro();
+	
+    List<Roteiro> buscarRoteiroEspecial();
+
+    /**
+     * Busca os roteiros associados a um box com a descrição recebida
+     * @param idBox identificador do Box
+     * @param descricaoRoteiro descrição do roteiro para consulta
+     * @return lista de roteiros que satisfaçam os critérios da consulta
+     */
+    List<Roteiro> buscarRoteiroDeBox(Long idBox, String descricaoRoteiro);
+
+	List<Roteiro> obterRoteirosPorCota(Integer numeroCota);
+	
+}
