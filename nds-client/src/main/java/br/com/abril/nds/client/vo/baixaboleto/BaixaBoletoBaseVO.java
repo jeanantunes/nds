e63bc669c6307f817a4382770fc32bfd23af76b9
william-balan <william.balan.machado@gmail.com>
2012-10-06 10:40:10 -0300

    VO's para exportação dos grids de baixa de boleto

---
+++ nds-client/src/main/java/br/com/abril/nds/client/vo/baixaboleto/BaixaBoletoBaseVO.java
@@ -0,0 +1,70 @@
+package br.com.abril.nds.client.vo.baixaboleto;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import br.com.abril.nds.util.export.Export;
+import br.com.abril.nds.util.export.Exportable;
+
+/**
+ * VO que representa informações sobre Baixas de Boletos.
+ * 
+ * @author Discover Technology
+ *
+ */
+@Exportable
+public class BaixaBoletoBaseVO implements Serializable {
+
+	private static final long serialVersionUID = 2743160908272450578L;
+	
+	@Export(label = "Banco", exhibitionOrder=0)
+	private String nomeBanco;
+	
+	@Export(label = "Conta-corrente", exhibitionOrder=0)
+	private String numeroConta;
+	
+	@Export(label = "Valor R$", exhibitionOrder=0)
+	private BigDecimal valorBoleto;
+
+	/**
+	 * @return the nomeBanco
+	 */
+	public String getNomeBanco() {
+		return nomeBanco;
+	}
+
+	/**
+	 * @param nomeBanco the nomeBanco to set
+	 */
+	public void setNomeBanco(String nomeBanco) {
+		this.nomeBanco = nomeBanco;
+	}
+
+	/**
+	 * @return the numeroConta
+	 */
+	public String getNumeroConta() {
+		return numeroConta;
+	}
+
+	/**
+	 * @param numeroConta the numeroConta to set
+	 */
+	public void setNumeroConta(String numeroConta) {
+		this.numeroConta = numeroConta;
+	}
+
+	/**
+	 * @return the valorBoleto
+	 */
+	public BigDecimal getValorBoleto() {
+		return valorBoleto;
+	}
+
+	/**
+	 * @param valorBoleto the valorBoleto to set
+	 */
+	public void setValorBoleto(BigDecimal valorBoleto) {
+		this.valorBoleto = valorBoleto;
+	}
+}
