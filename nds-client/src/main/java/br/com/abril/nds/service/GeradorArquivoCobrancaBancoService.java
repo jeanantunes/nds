0c04cfd358275ee818cfa964b158a1ed528e30e9
murilolocatelli <murilolocatelli@gmail.com>
2012-10-09 20:16:03 -0300

    Desenvolvimento [Ajuste] EMS 0028

--- nds-client/src/main/java/br/com/abril/nds/service/GeradorArquivoCobrancaBancoService.java
+++ nds-client/src/main/java/br/com/abril/nds/service/GeradorArquivoCobrancaBancoService.java
@@ -2,7 +2,20 @@ package br.com.abril.nds.service;
 
 import java.io.IOException;
 
+/**
+ * Interface que define serviços referentes 
+ * a geração de arquivo de cobrança para o banco.
+ * 
+ * @author Discover Technology
+ */
 public interface GeradorArquivoCobrancaBancoService {
 	
-	void gerarArquivoCobranca() throws IOException;
+
+	/**
+	 * Prepara os dados e gera o arquivo de cobrança para o banco. 
+	 * 
+	 * @throws IOException
+	 */
+	void prepararGerarArquivoCobrancaCnab() throws IOException;
+	
 }
