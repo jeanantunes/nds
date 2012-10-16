4b96fe8d61d3b3beab2ea5ba9a56e2810847ff0f
william-balan <william.balan.machado@gmail.com>
2012-10-16 14:04:05 -0300

    Backend consulta principal tela de relatório de garantias.

--- nds-client/src/main/java/br/com/abril/nds/repository/impl/CotaGarantiaRepositoryImpl.java
+++ nds-client/src/main/java/br/com/abril/nds/repository/impl/CotaGarantiaRepositoryImpl.java
@@ -1,13 +1,22 @@
 package br.com.abril.nds.repository.impl;
 
+import java.util.List;
+
 import org.hibernate.Criteria;
 import org.hibernate.Query;
 import org.hibernate.criterion.Restrictions;
+import org.hibernate.transform.AliasToBeanResultTransformer;
 import org.springframework.stereotype.Repository;
 
+import br.com.abril.nds.dto.GarantiaCadastradaDTO;
 import br.com.abril.nds.model.cadastro.Cheque;
 import br.com.abril.nds.model.cadastro.garantia.CotaGarantia;
+import br.com.abril.nds.model.cadastro.garantia.CotaGarantiaCaucaoLiquida;
+import br.com.abril.nds.model.cadastro.garantia.CotaGarantiaChequeCaucao;
 import br.com.abril.nds.model.cadastro.garantia.CotaGarantiaFiador;
+import br.com.abril.nds.model.cadastro.garantia.CotaGarantiaImovel;
+import br.com.abril.nds.model.cadastro.garantia.CotaGarantiaNotaPromissoria;
+import br.com.abril.nds.model.cadastro.garantia.CotaGarantiaOutros;
 import br.com.abril.nds.repository.CotaGarantiaRepository;
 
 /**
@@ -83,5 +92,46 @@ public class CotaGarantiaRepositoryImpl extends AbstractRepositoryModel<CotaGara
 		criteria.add(Restrictions.eq("fiador.id", idFiador));
 		
 		return (CotaGarantiaFiador) criteria.uniqueResult();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	@SuppressWarnings("unchecked")
+	public List<GarantiaCadastradaDTO> obterGarantiasCadastradas() {
+
+		StringBuilder hql = new StringBuilder();
+
+		hql.append(" select TYPE(garantia) as tipoGarantia, ")
+		   .append(" 		count(distinct garantia.cota.id) as quantidadeCotas, ")
+		   .append("  		sum( ")
+		   .append(" 			 case when garantia.class = " + CotaGarantiaFiador.class.getSimpleName())
+		   .append("			 	then garantiaFiador.valor ")
+		   .append(" 			 when garantia.class = " + CotaGarantiaCaucaoLiquida.class.getSimpleName())
+		   .append("			 	then garantiaCaucaoLiquida.valor ")
+		   .append(" 			 when garantia.class = " + CotaGarantiaChequeCaucao.class.getSimpleName())
+		   .append("			 	then garantiaChequeCaucao.valor ")
+		   .append(" 			 when garantia.class = " + CotaGarantiaImovel.class.getSimpleName())
+		   .append("			 	then garantiaImovel.valor ")
+		   .append(" 			 when garantia.class = " + CotaGarantiaNotaPromissoria.class.getSimpleName())
+		   .append("			 	then garantiaNotaPromissoria.valor ")
+		   .append(" 			 when garantia.class = " + CotaGarantiaOutros.class.getSimpleName())
+		   .append("			 	then garantiaOutros.valor ")
+		   .append(" 		end ) as valorTotal ")
+		   .append(" from CotaGarantia garantia ")
+		   .append(" left join garantia.caucaoLiquidas as garantiaCaucaoLiquida ")
+		   .append(" left join garantia.cheque as garantiaChequeCaucao ")
+		   .append(" left join garantia.fiador.garantias as garantiaFiador ")
+		   .append(" left join garantia.imoveis as garantiaImovel ")
+		   .append(" left join garantia.notaPromissoria as garantiaNotaPromissoria ")
+		   .append(" left join garantia.outros as garantiaOutros ")
+		   .append(" group by garantia.class ");
+		
+		Query query = this.getSession().createQuery(hql.toString());
+		
+		query.setResultTransformer(new AliasToBeanResultTransformer(GarantiaCadastradaDTO.class));
+		
+		return query.list();
 	}
 }
