ec75bb27ea869115735ea330b5bf10865b87eb6a
Jones Nogueira da Costa <jones.ncosta@gmail.com>
2012-10-17 14:13:54 -0300

    local

--- nds-client/src/main/java/br/com/abril/nds/controllers/financeiro/ContasAPagarController.java
+++ nds-client/src/main/java/br/com/abril/nds/controllers/financeiro/ContasAPagarController.java
@@ -2,6 +2,7 @@ package br.com.abril.nds.controllers.financeiro;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
@@ -10,140 +11,252 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import br.com.abril.nds.client.annotation.Rules;
-import br.com.abril.nds.client.vo.ContasAPagarConsultaPorProdutoVO;
+import br.com.abril.nds.client.vo.ContasAPagarConsignadoVO;
 import br.com.abril.nds.client.vo.ContasAPagarConsultaProdutoVO;
+import br.com.abril.nds.client.vo.ContasAPagarEncalheVO;
+import br.com.abril.nds.client.vo.ContasAPagarFaltasSobrasVO;
+import br.com.abril.nds.client.vo.ContasAPagarGridPrincipalFornecedorVO;
+import br.com.abril.nds.client.vo.ContasAPagarGridPrincipalProdutoVO;
+import br.com.abril.nds.client.vo.ContasAPagarParcialVO;
+import br.com.abril.nds.client.vo.VisaoEstoqueConferenciaCegaVO;
+import br.com.abril.nds.dto.ContasAPagarConsignadoDTO;
 import br.com.abril.nds.dto.ContasAPagarConsultaProdutoDTO;
+import br.com.abril.nds.dto.ContasAPagarEncalheDTO;
+import br.com.abril.nds.dto.ContasAPagarFaltasSobrasDTO;
+import br.com.abril.nds.dto.ContasAPagarGridPrincipalFornecedorDTO;
+import br.com.abril.nds.dto.ContasAPagarGridPrincipalProdutoDTO;
+import br.com.abril.nds.dto.ContasAPagarParcialDTO;
 import br.com.abril.nds.dto.ContasApagarConsultaPorProdutoDTO;
+import br.com.abril.nds.dto.FlexiGridDTO;
 import br.com.abril.nds.dto.VisaoEstoqueDTO;
 import br.com.abril.nds.dto.filtro.FiltroConsultaVisaoEstoque;
 import br.com.abril.nds.dto.filtro.FiltroContasAPagarDTO;
+import br.com.abril.nds.integracao.service.DistribuidorService;
+import br.com.abril.nds.model.cadastro.Distribuidor;
 import br.com.abril.nds.model.cadastro.Fornecedor;
 import br.com.abril.nds.model.cadastro.SituacaoCadastro;
 import br.com.abril.nds.model.seguranca.Permissao;
+import br.com.abril.nds.model.seguranca.Usuario;
 import br.com.abril.nds.serialization.custom.FlexiGridJson;
 import br.com.abril.nds.service.ContasAPagarService;
 import br.com.abril.nds.service.FornecedorService;
 import br.com.abril.nds.util.export.FileExporter;
+import br.com.abril.nds.util.export.NDSFileHeader;
 import br.com.abril.nds.util.export.FileExporter.FileType;
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
 import br.com.caelum.vraptor.view.Results;
 
-/**
- * Classe responsável pelo controle das ações referentes à tela de Follow Up do
- * Sistema.
- * 
- * @author InfoA2 - Alex
- */
-
 @Resource
 @Path("financeiro/contasAPagar")
 public class ContasAPagarController {
-	
 	private static final String FILTRO_CONTAS_A_PAGAR = "FILTRO_CONTAS_A_PAGAR";
-	
+
 	@Autowired
 	private Result result;
-	
+
 	@Autowired
 	private FornecedorService fornecedorService;
-	
+
 	@Autowired
 	private ContasAPagarService contasAPagarService;
 	
 	@Autowired
 	private HttpSession session;
 	
+	@Autowired
+	private HttpServletResponse httpServletResponse;
+	
+	@Autowired
+	private DistribuidorService distribuidorService;
+
+
 	public ContasAPagarController(Result result) {
 		super();
 		this.result = result;
 	}
-	
+
+
 	@Path("/")
 	@Rules(Permissao.ROLE_FINANCEIRO_CONTAS_A_PAGAR)
 	public void index() {
-		
-		List<Fornecedor> fornecedores = fornecedorService.obterFornecedores(
-				true, SituacaoCadastro.ATIVO);
-		result.include("fornecedores", fornecedores);
-		
-	}
-	
-	@Path("/pesquisar.json")
-	public void pesquisar(FiltroContasAPagarDTO filtro){
 
-		this.session.setAttribute(FILTRO_CONTAS_A_PAGAR, filtro);
-		
-		List<ContasAPagarConsultaPorProdutoVO> listVO = new ArrayList<ContasAPagarConsultaPorProdutoVO>();
-		
-		// Mock
-		ContasAPagarConsultaPorProdutoVO vo = new ContasAPagarConsultaPorProdutoVO();
-		vo.setProdutoEdicaoId("1");
-		vo.setRctl("15/12/2011");
-		vo.setCodigo("9090");
-		vo.setProduto("Veja");
-		vo.setEdicao("4343");
-		vo.setTipo("P");
-		vo.setReparte("20");
-		vo.setSuplementacao("5");
-		vo.setEncalhe("10");
-		vo.setVenda("10");
-		vo.setFaltasSobras("848");
-		vo.setDebitosCreditos("848");
-		vo.setSaldoAPagar("2.000,00");
-		vo.setFornecedor("FC");
-		vo.setDataLcto("12/02/2012");
-		vo.setDataFinal("10/04/2012");
-		listVO.add(vo);
-		
-		
-		
-		result.use(FlexiGridJson.class).from(listVO).total(listVO.size()).serialize();
+		List<Fornecedor> fornecedores = fornecedorService.obterFornecedores(true, SituacaoCadastro.ATIVO);
+		result.include("fornecedores", fornecedores);
 	}
-	
+
+
 	@Path("/pesquisarProduto.json")
-	public void pesquisarProduto(FiltroContasAPagarDTO filtro){
-		
-		this.session.setAttribute(FILTRO_CONTAS_A_PAGAR, filtro);
-		
-		List<ContasAPagarConsultaProdutoDTO> produtos = contasAPagarService.pesquisaProdutoContasAPagar(filtro.getProduto(), filtro.getEdicao());
+	public void pesquisarProduto(FiltroContasAPagarDTO filtro) {
+
+		List<ContasAPagarConsultaProdutoDTO> produtos = contasAPagarService.pesquisarProdutos(filtro);
 		List<ContasAPagarConsultaProdutoVO> produtosVO = new ArrayList<ContasAPagarConsultaProdutoVO>();
-		
+
 		for(ContasAPagarConsultaProdutoDTO dto : produtos){
 			produtosVO.add(new ContasAPagarConsultaProdutoVO(dto));
 		}
-		
+
 		result.use(FlexiGridJson.class).from(produtosVO).total(produtosVO.size()).serialize();
+	}
+
+
+	@Path("/pesquisarPorProduto.json")
+	public void pesquisarPorProduto(FiltroContasAPagarDTO filtro, String sortname, String sortorder, int rp, int page) {
 		
+		this.session.setAttribute(FILTRO_CONTAS_A_PAGAR, filtro);
+
+		ContasAPagarGridPrincipalProdutoDTO dto = contasAPagarService.pesquisarPorProduto(filtro, sortname, sortorder, rp, page);
+
+		if (dto == null) {
+			// TODO: msg erro busca sem resultados
+		}
+		
+		result.use(Results.json()).from(new ContasAPagarGridPrincipalProdutoVO(dto), "result").recursive().serialize();
+	}
+
+
+	@Path("/pesquisarParcial.json")
+	public void pesquisarParcial(FiltroContasAPagarDTO filtro, String sortname, String sortorder, int rp, int page) {
+
+		List<ContasAPagarParcialVO> listVO = new ArrayList<ContasAPagarParcialVO>();
+		FlexiGridDTO<ContasAPagarParcialDTO> flexiDTO = contasAPagarService.pesquisarParcial(filtro, sortname, sortorder, rp, page);
+
+		for (ContasAPagarParcialDTO dto : flexiDTO.getGrid()) {
+			listVO.add(new ContasAPagarParcialVO(dto));
+		}
+
+		result.use(FlexiGridJson.class).from(listVO).total(flexiDTO.getTotalGrid()).serialize();
+	}
+
+
+	@Path("/pesquisarPorFornecedor.json")
+	public void pesquisarPorFornecedor(FiltroContasAPagarDTO filtro, String sortname, String sortorder, int rp, int page) {
+
+		ContasAPagarGridPrincipalFornecedorDTO dto = contasAPagarService.pesquisarPorDistribuidor(filtro, sortname, sortorder, rp, page);
+
+		if (dto == null) {
+			// TODO: msg erro busca sem resultados
+		}
+
+		result.use(Results.json()).from(new ContasAPagarGridPrincipalFornecedorVO(dto), "result").recursive().serialize();
+	}
+
+
+	@Path("/pesquisarConsignado.json")
+	public void pesquisarConsignado(FiltroContasAPagarDTO filtro, String sortname, String sortorder, int rp, int page) {
+
+		List<ContasAPagarConsignadoVO> listVO = new ArrayList<ContasAPagarConsignadoVO>();
+		List<ContasAPagarConsignadoDTO> listDTO = contasAPagarService.pesquisarDetalheConsignado(filtro, sortname, sortorder, rp, page);
+
+		for (ContasAPagarConsignadoDTO dto : listDTO) {
+			listVO.add(new ContasAPagarConsignadoVO(dto));
+		}
+
+		result.use(FlexiGridJson.class).from(listVO).total(listVO.size()).serialize();
+	}
+
+
+	@Path("/pesquisarEncalhe.json")
+	public void pesquisarEncalhe(FiltroContasAPagarDTO filtro, String sortname, String sortorder, int rp, int page) {
+
+		List<ContasAPagarEncalheVO> listVO = new ArrayList<ContasAPagarEncalheVO>();
+		List<ContasAPagarEncalheDTO> listDTO = contasAPagarService.pesquisarDetalheEncalhe(filtro, sortname, sortorder, rp, page);
+
+		for (ContasAPagarEncalheDTO dto : listDTO) {
+			listVO.add(new ContasAPagarEncalheVO(dto));
+		}
+
+		result.use(FlexiGridJson.class).from(listVO).total(listVO.size()).serialize();
+	}
+
+
+	@Path("/pesquisarFaltasSobras.json")
+	public void pesquisarFaltasSobras(FiltroContasAPagarDTO filtro, String sortname, String sortorder, int rp, int page) {
+
+		List<ContasAPagarFaltasSobrasVO> listVO = new ArrayList<ContasAPagarFaltasSobrasVO>();
+		List<ContasAPagarFaltasSobrasDTO> listDTO = contasAPagarService.pesquisarDetalheFaltasSobras(filtro, sortname, sortorder, rp, page);
+
+		for (ContasAPagarFaltasSobrasDTO dto : listDTO) {
+			listVO.add(new ContasAPagarFaltasSobrasVO(dto));
+		}
+
+		result.use(FlexiGridJson.class).from(listVO).total(listVO.size()).serialize();
 	}
 	
-	@Path("/exportar")
-	public void exportar(FileType fileType) throws IOException {
+	@Path("/exportPesquisarPorProduto")
+	public void exportPesquisarPorProduto(FileType fileType) throws IOException {
 		
-		FiltroContasAPagarDTO filtro = (FiltroContasAPagarDTO) this.session.getAttribute(FILTRO_CONTAS_A_PAGAR);
 		
-		List<ContasApagarConsultaPorProdutoDTO> listConsultaPorProduto = contasAPagarService.pesquisaContasAPagarPorProduto(filtro.getProdutoEdicaoIDs());
+		/*FiltroContasAPagarDTO filtro = (FiltroContasAPagarDTO) this.session.getAttribute(FILTRO_CONTAS_A_PAGAR);
+		String sortname;
+		String sortorder;
+		int rp;
+		int page;
 		
-		/*FileExporter.to("visao-estoque", fileType).inHTTPResponse(
-				this.getNDSFileHeader(filtro.getDataMovimentacao()), null, null,
-				listVisaoEstoque, VisaoEstoqueDTO.class,
-				this.httpServletResponse);
+		ContasAPagarGridPrincipalProdutoDTO listContasAPagar =  contasAPagarService.pesquisarPorProduto(filtro, sortname, sortorder, rp, page);
+		
+		FileExporter.to("contas-a-pagar", fileType).inHTTPResponse(new Date(), null, null,
+		new ContasAPagarGridPrincipalProdutoVO(listContasAPagar), ContasAPagarGridPrincipalProdutoVO.class,
+		this.httpServletResponse);
 		
 		result.use(Results.nothing());*/
+	}
+	
+	@Path("/exportPesquisarParcial")
+	public void exportPesquisarParcial(FileType fileType) throws IOException {
+		
+		// TODO
+	}
+	
+	@Path("/exportPesquisarPorDistribuidor")
+	public void exportPesquisarPorDistribuidor(FileType fileType) throws IOException {
+		
+		// TODO
+	}
+	
+	@Path("/exportPesquisarDetalheConsignado")
+	public void exportPesquisarDetalheConsignado(FileType fileType) throws IOException {
+		
+		// TODO
+	}
+	
+	@Path("/exportPesquisarDetalheEncalhe")
+	public void exportPesquisarDetalheEncalhe(FileType fileType) throws IOException {
+		
+		// TODO
+	}
+	
+	@Path("/exportPesquisarDetalheFaltasSobras")
+	public void exportPesquisarDetalheFaltasSobras(FileType fileType) throws IOException {
+		
+		// TODO
+	}
+	
+	private NDSFileHeader getNDSFileHeader(Date data) {
 
+		NDSFileHeader ndsFileHeader = new NDSFileHeader();
+		Distribuidor distribuidor = distribuidorService.obter();
 
-		HttpServletResponse response = null;  
+		if (distribuidor != null) {
+			ndsFileHeader.setNomeDistribuidor(distribuidor.getJuridica().getRazaoSocial());
+			ndsFileHeader.setCnpjDistribuidor(distribuidor.getJuridica().getCnpj());
+		}
 
-                response.getWriter().write("<table><tr><td>linha 1</td></tr><tr><td>linha 2</td></tr></table>");  
-                response.getWriter().flush();  
-                response.getWriter().close();  
-  
-        
+		ndsFileHeader.setData(data);
+		ndsFileHeader.setNomeUsuario(getUsuario().getNome());
+		return ndsFileHeader;
 	}
 	
 	
-	
+	// TODO: não há como reconhecer usuario, ainda
+	private Usuario getUsuario() {
+
+		Usuario usuario = new Usuario();
+		usuario.setId(1L);
+		usuario.setNome("Jornaleiro da Silva");
+
+		return usuario;
+	}
 	
 }
-
--- nds-client/src/main/java/br/com/abril/nds/service/impl/ContasAPagarServiceImpl.java
+++ nds-client/src/main/java/br/com/abril/nds/service/impl/ContasAPagarServiceImpl.java
@@ -1,5 +1,9 @@
 package br.com.abril.nds.service.impl;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +17,7 @@ import br.com.abril.nds.dto.ContasAPagarFaltasSobrasDTO;
 import br.com.abril.nds.dto.ContasAPagarGridPrincipalFornecedorDTO;
 import br.com.abril.nds.dto.ContasAPagarGridPrincipalProdutoDTO;
 import br.com.abril.nds.dto.ContasAPagarParcialDTO;
+import br.com.abril.nds.dto.ContasApagarConsultaPorProdutoDTO;
 import br.com.abril.nds.dto.FlexiGridDTO;
 import br.com.abril.nds.dto.filtro.FiltroContasAPagarDTO;
 import br.com.abril.nds.repository.ContasAPagarRepository;
@@ -34,7 +39,43 @@ public class ContasAPagarServiceImpl implements ContasAPagarService {
 	@Override
 	public ContasAPagarGridPrincipalProdutoDTO pesquisarPorProduto(FiltroContasAPagarDTO filtro, String sortname, String sortorder, int rp, int page) {
 		// TODO Auto-generated method stub
-		return null;
+		
+		//MOCK
+		ContasAPagarGridPrincipalProdutoDTO dto = new ContasAPagarGridPrincipalProdutoDTO();
+        dto.setTotalDesconto(BigDecimal.TEN);
+        dto.setTotalPagto(BigDecimal.TEN);
+        dto.setValorLiquido(BigDecimal.TEN);
+        
+        ContasApagarConsultaPorProdutoDTO obj = new ContasApagarConsultaPorProdutoDTO();
+        obj.setCodigo("1");
+        obj.setDataFinal(new Date());
+        obj.setDataLcto(new Date());
+        obj.setDebitosCreditos(BigInteger.TEN);
+        obj.setEdicao(1L);
+        obj.setEncalhe(BigInteger.TEN);
+        obj.setFaltasSobras(BigInteger.TEN);
+        obj.setFornecedor("FC");
+        obj.setProduto("Veja");
+        obj.setProdutoEdicaoId(1000L);
+        obj.setRctl(new Date());
+        obj.setReparte(BigInteger.TEN);
+        obj.setSaldoAPagar(BigDecimal.TEN);
+        obj.setSuplementacao(BigInteger.TEN);
+        obj.setTipo(true);
+        obj.setVenda(BigInteger.TEN);
+        
+        dto.setGrid(new ArrayList<ContasApagarConsultaPorProdutoDTO>());
+        dto.getGrid().add(obj);
+        dto.getGrid().add(obj);
+        dto.getGrid().add(obj);
+        dto.getGrid().add(obj);
+        dto.getGrid().add(obj);
+        dto.getGrid().add(obj);
+        
+        dto.setTotalGrid(dto.getGrid().size());
+        
+        return dto;
+
 	}
 
 	@Transactional
