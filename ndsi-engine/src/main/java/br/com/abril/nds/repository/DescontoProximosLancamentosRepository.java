d2b06eb99a8cfec107daf9ce30449d3639178d2d
direx <diogo.vecchiati@gmail.com>
2012-10-22 11:31:18 -0200

    modelagem desconto proximos lancamentos

---
+++ ndsi-engine/src/main/java/br/com/abril/nds/repository/DescontoProximosLancamentosRepository.java
@@ -0,0 +1,7 @@
+package br.com.abril.nds.repository;
+
+import br.com.abril.nds.model.financeiro.DescontoProximosLancamentos;
+
+public interface DescontoProximosLancamentosRepository extends Repository<DescontoProximosLancamentos, Long>{
+
+}
