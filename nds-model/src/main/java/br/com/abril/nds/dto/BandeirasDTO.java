0ae639d85cc188f332c0f729e122a42e63fca511
BrunoMonteiro <brunomonteoliveira@gmail.com>
2012-10-24 09:05:03 -0200

    ems194

--- nds-model/src/main/java/br/com/abril/nds/dto/BandeirasDTO.java
+++ nds-model/src/main/java/br/com/abril/nds/dto/BandeirasDTO.java
@@ -1,6 +1,8 @@
 package br.com.abril.nds.dto;
 
 import java.io.Serializable;
+import java.math.BigInteger;
+import java.util.Date;
 
 public class BandeirasDTO  implements Serializable{
 
@@ -10,6 +12,16 @@ public class BandeirasDTO  implements Serializable{
 	private String nomeProduto;
 	private Long edProduto;
 	private Integer pctPadrao;
+	
+	//TODO De onde vem?
+	private String destino;
+	//TODO De onde vem?
+	private Integer prioridade;
+
+	private BigInteger qtde;
+	
+	private Date data;
+
 	
 	public BandeirasDTO() {
 		
@@ -36,16 +48,51 @@ public class BandeirasDTO  implements Serializable{
 	public void setNomeProduto(String nomeProduto) {
 		this.nomeProduto = nomeProduto;
 	}
-	public Long getedProduto() {
-		return edProduto;
-	}
-	public void setedProduto(Long edProduto) {
-		this.edProduto = edProduto;
-	}
+	
 	public Integer getPctPadrao() {
 		return pctPadrao;
 	}
 	public void setPctPadrao(Integer pctPadrao) {
 		this.pctPadrao = pctPadrao;
+	}
+
+	public Long getEdProduto() {
+		return edProduto;
+	}
+
+	public void setEdProduto(Long edProduto) {
+		this.edProduto = edProduto;
+	}
+
+	public String getDestino() {
+		return destino;
+	}
+
+	public void setDestino(String destino) {
+		this.destino = destino;
+	}
+
+	public Integer getPrioridade() {
+		return prioridade;
+	}
+
+	public void setPrioridade(Integer prioridade) {
+		this.prioridade = prioridade;
+	}
+
+	public BigInteger getQtde() {
+		return qtde;
+	}
+
+	public void setQtde(BigInteger qtde) {
+		this.qtde = qtde;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
 	}	
 }
