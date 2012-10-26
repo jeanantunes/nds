f498a47402c97d20c588507526a56c99225e67b8
chicaogarcia <chicaogarcia@gmail.com>
2012-10-26 15:49:30 -0200

    Ajustes impressao Chamada Encalhe Fornecedor

--- nds-client/src/main/java/br/com/abril/nds/client/assembler/ChamadaEncalheFornecedorDTOAssembler.java
+++ nds-client/src/main/java/br/com/abril/nds/client/assembler/ChamadaEncalheFornecedorDTOAssembler.java
@@ -28,6 +28,14 @@ import br.com.abril.nds.model.planejamento.fornecedor.ItemChamadaEncalheForneced
 import br.com.abril.nds.util.MathUtil;
 import br.com.abril.nds.util.Util;
 
+
+/**
+ * Classe utilitária para auxílio na criação dos DTO's com informações das
+ * chamadas de encalhe do fornecedor
+ * 
+ * @author francisco.garcia
+ * 
+ */
 public class ChamadaEncalheFornecedorDTOAssembler {
     
     private ChamadaEncalheFornecedorDTOAssembler() {
@@ -53,33 +61,7 @@ public class ChamadaEncalheFornecedorDTOAssembler {
                 ChamadaEncalheFornecedorDTO cefDTO = chamadasDTO.newDocumento();
                 cefDTO.setNumeroDocumento(documento.getKey());
                 for (ItemChamadaEncalheFornecedor icef : documento.getValue()) {
-                    ItemChamadaEncalheFornecedorDTO icefDTO = new ItemChamadaEncalheFornecedorDTO();
-
-                    ProdutoEdicao produtoEdicao = icef.getProdutoEdicao();
-                    Produto produto = produtoEdicao.getProduto();
-                    Editor editor = produto.getEditor();
-                    String formaDevolucao = icef.getFormaDevolucao() == null ? null : icef.getFormaDevolucao().getDescricao();
-                    String tipoRecolhimento = icef.getRegimeRecolhimento() == null ? null : icef.getRegimeRecolhimento().getCodigo();
-
-                    Long codigoEditor = editor == null ? Long.valueOf(0) : editor.getCodigo();                     
-                    icefDTO.setEditor(codigoEditor);
-                    icefDTO.setDescricao(produto.getNomeComercial());
-                    icefDTO.setNome(produto.getNome());
-                    icefDTO.setCodigo(produto.getCodigo());
-                    icefDTO.setEdicao(produtoEdicao.getNumeroEdicao());
-                    icefDTO.setFormaDevolucao(formaDevolucao);
-                    icefDTO.setItem(icef.getNumeroItem());
-                    icefDTO.setDataRecolhimento(icef.getDataRecolhimento());
-                    icefDTO.setNotaEnvio(icef.getNumeroNotaEnvio());
-                    icefDTO.setPacotePadrao(produtoEdicao.getPacotePadrao());
-                    icefDTO.setPrecoCapa(icef.getPrecoUnitario());
-                    icefDTO.setQtdeDevolvida(icef.getQtdeDevolucaoApurada());
-                    icefDTO.setQtdeEnviada(icef.getQtdeEnviada());
-                    icefDTO.setQtdeVenda(icef.getQtdeVendaApurada());
-                    icefDTO.setTipoRecolhimento(tipoRecolhimento);
-                    icefDTO.setValorVenda(icef.getValorVendaApurado());
-                    icefDTO.setCodigoNotaEnvioMultipla(icef.getCodigoNotaEnvioMultipla());
-                    cefDTO.addItem(icefDTO);
+                    processarItem(cefDTO, icef);
 
                     totalBrutoDocumento = totalBrutoDocumento.add(Util.nvl(icef.getValorVendaApurado(), BigDecimal.ZERO));
                     totalDescontoDocumento = totalDescontoDocumento.add(Util.nvl(icef.getValorMargemApurado(), BigDecimal.ZERO));
@@ -101,20 +83,77 @@ public class ChamadaEncalheFornecedorDTOAssembler {
                 BigDecimal totalMargemDistribuidor = MathUtil.round(MathUtil.calculatePercentageValue(totalBrutoDocumento, margemDistribuidor), 2);
                 cefDTO.setTotalMargemDistribuidor(totalMargemDistribuidor);
   
-                ResumoChamadaEncalheFornecedorDTO resumoDTO = chamadasDTO.getResumo();
-                ItemResumoChamadaEncalheFornecedorDTO itemResumoDTO = resumoDTO.newItem();
-                itemResumoDTO.setNumeroDocumento(cefDTO.getNumeroDocumento());
-                itemResumoDTO.setValorDocumento(cefDTO.getTotalLiquido());
-                itemResumoDTO.setValorMargem(totalMargemDistribuidor);
-                
-                resumoDTO.setSubTotalVendas(resumoDTO.getSubTotalVendas().add(cefDTO.getTotalLiquido()));
-                resumoDTO.setValorPagar(resumoDTO.getSubTotalVendas().subtract(resumoDTO.getValorProjetosEspeciais()));
-                resumoDTO.setTotalMargemDistribuidor(resumoDTO.getTotalMargemDistribuidor().add(totalMargemDistribuidor));
+                processarResumo(chamadasDTO, cefDTO);
             }
         }
         return mapaChamadasDTO.values();
     }
+
+    /**
+     * Processa o item da chamada de encalhe do fornecedor
+     * 
+     * @param cefDTO
+     *            DTO com informações da chamada de encalhe do fornecedor
+     * @param icef
+     *            item da chamada de encalhe do fornecedor
+     */
+    private static void processarItem(ChamadaEncalheFornecedorDTO cefDTO, ItemChamadaEncalheFornecedor icef) {
+        ItemChamadaEncalheFornecedorDTO icefDTO = new ItemChamadaEncalheFornecedorDTO();
+
+        ProdutoEdicao produtoEdicao = icef.getProdutoEdicao();
+        Produto produto = produtoEdicao.getProduto();
+        Editor editor = produto.getEditor();
+        String formaDevolucao = icef.getFormaDevolucao() == null ? null : icef.getFormaDevolucao().getDescricao();
+        String tipoRecolhimento = icef.getRegimeRecolhimento() == null ? null : icef.getRegimeRecolhimento().getCodigo();
+
+        Long codigoEditor = editor == null ? Long.valueOf(0) : editor.getCodigo();                     
+        icefDTO.setEditor(codigoEditor);
+        icefDTO.setDescricao(produto.getNomeComercial());
+        icefDTO.setNome(produto.getNome());
+        icefDTO.setCodigo(produto.getCodigo());
+        icefDTO.setEdicao(produtoEdicao.getNumeroEdicao());
+        icefDTO.setFormaDevolucao(formaDevolucao);
+        icefDTO.setItem(icef.getNumeroItem());
+        icefDTO.setDataRecolhimento(icef.getDataRecolhimento());
+        icefDTO.setNotaEnvio(icef.getNumeroNotaEnvio());
+        icefDTO.setPacotePadrao(produtoEdicao.getPacotePadrao());
+        icefDTO.setPrecoCapa(icef.getPrecoUnitario());
+        icefDTO.setQtdeDevolvida(icef.getQtdeDevolucaoApurada());
+        icefDTO.setQtdeEnviada(icef.getQtdeEnviada());
+        icefDTO.setQtdeVenda(icef.getQtdeVendaApurada());
+        icefDTO.setTipoRecolhimento(tipoRecolhimento);
+        icefDTO.setValorVenda(icef.getValorVendaApurado());
+        icefDTO.setCodigoNotaEnvioMultipla(icef.getCodigoNotaEnvioMultipla());
+        cefDTO.addItem(icefDTO);
+    }
+
+    /**
+     * Processa o resumo das chamdas de encalhe do fornecedor
+     * 
+     * @param chamadasDTO
+     *            DTO com o agrupamento das chamadas de encalhe do fornecedor
+     * @param cefDTO
+     *            Chamada de encalhe do fornecedor para criação do resumo
+     */
+    private static void processarResumo(ChamadasEncalheFornecedorDTO chamadasDTO, ChamadaEncalheFornecedorDTO cefDTO) {
+        ResumoChamadaEncalheFornecedorDTO resumoDTO = chamadasDTO.getResumo();
+        ItemResumoChamadaEncalheFornecedorDTO itemResumoDTO = resumoDTO.newItem();
+        itemResumoDTO.setNumeroDocumento(cefDTO.getNumeroDocumento());
+        itemResumoDTO.setValorDocumento(cefDTO.getTotalLiquido());
+        itemResumoDTO.setValorMargem(cefDTO.getTotalMargemDistribuidor());
+        
+        resumoDTO.setSubTotalVendas(resumoDTO.getSubTotalVendas().add(cefDTO.getTotalLiquido()));
+        resumoDTO.setValorPagar(resumoDTO.getSubTotalVendas().subtract(resumoDTO.getValorProjetosEspeciais()));
+        resumoDTO.setTotalMargemDistribuidor(resumoDTO.getTotalMargemDistribuidor().add(cefDTO.getTotalMargemDistribuidor()));
+    }
     
+    /**
+     * Cria o DTO com as informações do distribuidor
+     * 
+     * @param distribuidor
+     *            distribuidor para criação do DTO
+     * @return DTO com as informações do distribuidor
+     */
     private static PessoaJuridicaChamadaEncalheFornecedorDTO criarDistribuidor(final Distribuidor distribuidor) {
         Endereco endereco = null;
         if (distribuidor.getEnderecoDistribuidor() != null) {
@@ -125,6 +164,13 @@ public class ChamadaEncalheFornecedorDTOAssembler {
         return distribuidorDTO;
     }
     
+    /**
+     * Cria o DTO com as informações do fornecedor
+     * 
+     * @param fornecedor
+     *            fornecedor para criação do DTO
+     * @return DTO com as informações do fornecedor
+     */
     private static PessoaJuridicaChamadaEncalheFornecedorDTO criarFornecedor(final Fornecedor fornecedor) {
         Endereco endereco = null;
         if (fornecedor.getEnderecoPrincipal() != null) {
@@ -136,6 +182,18 @@ public class ChamadaEncalheFornecedorDTOAssembler {
         return distribuidorDTO;
     }
  
+    /**
+     * Cria o DTO com as informações da pessoa jurídica para a chamda de encalhe
+     * do fornecedor
+     * 
+     * @param id
+     *            identificador
+     * @param pj
+     *            pessoa jurídica para criação do DTO
+     * @param endereco
+     *            endereço para criação do DTO
+     * @return DTO criador com as informações da Pessoa Jurídica
+     */
     private static PessoaJuridicaChamadaEncalheFornecedorDTO criarPessoaJuridicaChamadaEncalheFornecedor(Long id, PessoaJuridica pj, Endereco endereco) {
         String logradouro = endereco == null ? "" : String.format("%s %s",
                 Util.nvl(endereco.getTipoLogradouro(), ""),
@@ -151,6 +209,13 @@ public class ChamadaEncalheFornecedorDTOAssembler {
         return pjDTO;
     }
     
+    /**
+     * Cria o DTO com as informações de identificação da chamada de encalhe
+     * 
+     * @param ceFornecedor
+     *            chamada de encalhe do fornecedor
+     * @return DTO com as informações de identificação da chamda de encalhe
+     */
     private static IdentificacaoChamadaEncalheFornecedorDTO criarIdentificacao(ChamadaEncalheFornecedor ceFornecedor) {
         String codigoCFOP = "";
         String descricaoCFOP = ""; 
