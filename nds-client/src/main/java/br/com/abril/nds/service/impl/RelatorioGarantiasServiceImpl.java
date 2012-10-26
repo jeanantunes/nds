e4a3f69c79f1b0482f3a65918f35f42f21652829
Jones Nogueira da Costa <jones.ncosta@gmail.com>
2012-10-26 19:19:54 -0200

    service/impl/RelatorioGarantiasServiceImpl.java

--- nds-client/src/main/java/br/com/abril/nds/service/impl/RelatorioGarantiasServiceImpl.java
+++ nds-client/src/main/java/br/com/abril/nds/service/impl/RelatorioGarantiasServiceImpl.java
@@ -1,8 +1,6 @@
 package br.com.abril.nds.service.impl;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Date;
+import java.util.Calendar;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,75 +24,19 @@ public class RelatorioGarantiasServiceImpl implements RelatorioGarantiasService 
 	@Override
 	public FlexiGridDTO<RelatorioGarantiasDTO> gerarTodasGarantias(FiltroRelatorioGarantiasDTO filtro) {
 		
-		FlexiGridDTO<RelatorioGarantiasDTO> to = new FlexiGridDTO<RelatorioGarantiasDTO>();
-		
-		RelatorioGarantiasDTO dto = new RelatorioGarantiasDTO();
-		
-		dto.setQtdCotas(99);
-		dto.setTipoGarantia("Fiador");
-		dto.setVlrTotal(BigDecimal.TEN);
-		dto.setTipoGarantiaEnum(TipoGarantia.IMOVEL);
-		
-		to.setGrid(new ArrayList<RelatorioGarantiasDTO>());
-		to.getGrid().add(dto);
-		to.getGrid().add(dto);
-		to.getGrid().add(dto);
-		to.getGrid().add(dto);
-		to.getGrid().add(dto);
-		to.getGrid().add(dto);
-		to.getGrid().add(dto);
-		to.getGrid().add(dto);
-		to.getGrid().add(dto);
-		to.getGrid().add(dto);
-		
-		to.setTotalGrid(30);
-		
-		return to;
 		// TODO Auto-generated method stub
-		//return null;
+		return null;
 	}
 
 	@Transactional
 	@Override
 	public FlexiGridDTO<RelatorioDetalheGarantiaDTO> gerarPorTipoGarantia(FiltroRelatorioGarantiasDTO filtro) {
 		
-		//MOCK
-				FlexiGridDTO<RelatorioDetalheGarantiaDTO> to = new FlexiGridDTO<RelatorioDetalheGarantiaDTO>();
-				
-				RelatorioDetalheGarantiaDTO dto = new RelatorioDetalheGarantiaDTO();
-				
-				dto.setCota(999);
-				dto.setFaturamento(BigDecimal.TEN);
-				dto.setGarantia("Fiador");
-				dto.setGarantiaFaturamento(BigDecimal.TEN);
-				dto.setNome("José Maria ");
-				dto.setVencto(new Date());
-				dto.setVlrGarantia(BigDecimal.TEN);
-				
-				to.setGrid(new ArrayList<RelatorioDetalheGarantiaDTO>());
-				to.getGrid().add(dto);
-				to.getGrid().add(dto);
-				to.getGrid().add(dto);
-				to.getGrid().add(dto);
-				to.getGrid().add(dto);
-				to.getGrid().add(dto);
-				to.getGrid().add(dto);
-				to.getGrid().add(dto);
-				
-				
-				
-				
-				return to;
-		
-		
-		
-		/*
-		
 		FlexiGridDTO<RelatorioDetalheGarantiaDTO> to = new FlexiGridDTO<RelatorioDetalheGarantiaDTO>();
 		to.setGrid(this.cotaGarantiaRepository.obterDetalheGarantiaCadastrada(TipoGarantia.valueOf(filtro.getTipoGarantia()), Calendar.getInstance().getTime()));
 		to.setTotalGrid(this.cotaGarantiaRepository.obterCountDetalheGarantiaCadastrada(TipoGarantia.valueOf(filtro.getTipoGarantia()), Calendar.getInstance().getTime()).intValue());
 
-		return to;*/
+		return to;
 	}
 
 }
