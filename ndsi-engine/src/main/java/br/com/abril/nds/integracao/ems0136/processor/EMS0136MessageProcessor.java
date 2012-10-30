ff00ac40463aa3642db83431b126854b567387aa
Alexandre Takashi <alexandre.takashi@gmail.com>
2012-10-30 14:22:38 -0200

    Inclusão do componente para log

--- ndsi-engine/src/main/java/br/com/abril/nds/integracao/ems0136/processor/EMS0136MessageProcessor.java
+++ ndsi-engine/src/main/java/br/com/abril/nds/integracao/ems0136/processor/EMS0136MessageProcessor.java
@@ -1,14 +1,19 @@
 package br.com.abril.nds.integracao.ems0136.processor;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import br.com.abril.nds.integracao.engine.MessageProcessor;
 import br.com.abril.nds.integracao.engine.data.Message;
+import br.com.abril.nds.integracao.engine.log.NdsiLoggerFactory;
 import br.com.abril.nds.repository.impl.AbstractRepository;
 
 @Component
 public class EMS0136MessageProcessor extends AbstractRepository implements
 		MessageProcessor {
+
+	@Autowired
+	private NdsiLoggerFactory ndsiLoggerFactory;
 
 	@Override
 	public void preProcess() {
