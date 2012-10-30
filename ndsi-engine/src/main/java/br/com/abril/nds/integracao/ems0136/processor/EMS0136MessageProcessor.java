3b8a2613f820b785a7c1ca765854e8fad91be0db
Alexandre Takashi <alexandre.takashi@gmail.com>
2012-10-30 15:52:06 -0200

    Inclusão de validação do código do Distribuidor

--- ndsi-engine/src/main/java/br/com/abril/nds/integracao/ems0136/processor/EMS0136MessageProcessor.java
+++ ndsi-engine/src/main/java/br/com/abril/nds/integracao/ems0136/processor/EMS0136MessageProcessor.java
@@ -6,6 +6,10 @@ import org.springframework.stereotype.Component;
 import br.com.abril.nds.integracao.engine.MessageProcessor;
 import br.com.abril.nds.integracao.engine.data.Message;
 import br.com.abril.nds.integracao.engine.log.NdsiLoggerFactory;
+import br.com.abril.nds.integracao.model.canonic.EMS0136Input;
+import br.com.abril.nds.integracao.service.DistribuidorService;
+import br.com.abril.nds.model.cadastro.Distribuidor;
+import br.com.abril.nds.model.integracao.EventoExecucaoEnum;
 import br.com.abril.nds.repository.impl.AbstractRepository;
 
 @Component
@@ -15,6 +19,10 @@ public class EMS0136MessageProcessor extends AbstractRepository implements
 	@Autowired
 	private NdsiLoggerFactory ndsiLoggerFactory;
 
+	@Autowired
+	private DistribuidorService distribuidorService;
+	
+	
 	@Override
 	public void preProcess() {
 		// TODO Auto-generated method stub
@@ -23,8 +31,25 @@ public class EMS0136MessageProcessor extends AbstractRepository implements
 
 	@Override
 	public void processMessage(Message message) {
-		// TODO Auto-generated method stub
-
+		
+		EMS0136Input input = (EMS0136Input) message.getBody();
+		if (input == null) {
+			this.ndsiLoggerFactory.getLogger().logError(message,
+					EventoExecucaoEnum.ERRO_INFRA, "NAO ENCONTROU o Arquivo");
+			return;
+		}
+		
+		// Validar código do distribuidor:
+		Distribuidor distribuidor = this.distribuidorService.obter();
+		if(!distribuidor.getCodigoDistribuidorDinap().equals(
+				input.getCodigoDistribuidor())){			
+			this.ndsiLoggerFactory.getLogger().logWarning(message,
+					EventoExecucaoEnum.RELACIONAMENTO, 
+					"Código do distribuidor do arquivo não é o mesmo do Sistema.");
+			return;
+		}
+		
+		
 	}
 
 	@Override
