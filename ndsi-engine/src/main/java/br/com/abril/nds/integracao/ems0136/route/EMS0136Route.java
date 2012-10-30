c1ea32b5970f1c66d86ea84ce0c3e3bc96fcf322
Alexandre Takashi <alexandre.takashi@gmail.com>
2012-10-30 19:05:38 -0200

    Criação da classe concreta de rota

--- ndsi-engine/src/main/java/br/com/abril/nds/integracao/ems0136/route/EMS0136Route.java
+++ ndsi-engine/src/main/java/br/com/abril/nds/integracao/ems0136/route/EMS0136Route.java
@@ -1,34 +1,40 @@
 package br.com.abril.nds.integracao.ems0136.route;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import br.com.abril.nds.integracao.ems0136.processor.EMS0136MessageProcessor;
 import br.com.abril.nds.integracao.engine.MessageProcessor;
 import br.com.abril.nds.integracao.engine.RouteInterface;
 import br.com.abril.nds.integracao.engine.data.CouchDBImportRouteTemplate;
 import br.com.abril.nds.integracao.model.canonic.InterfaceEnum;
 
+@Component
+@Scope("prototype")
 public class EMS0136Route extends CouchDBImportRouteTemplate {
 
+	@Autowired
+	private EMS0136MessageProcessor messageProcessor;
+	
 	@Override
 	public InterfaceEnum getInterfaceEnum() {
-		// TODO Auto-generated method stub
-		return null;
+		return InterfaceEnum.EMS0136;
 	}
 
 	@Override
 	public String getUri() {
-		// TODO Auto-generated method stub
-		return null;
+		return "EMS0136";
 	}
 
 	@Override
 	public MessageProcessor getMessageProcessor() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.messageProcessor;
 	}
 
 	@Override
 	public RouteInterface getRouteInterface() {
-		// TODO Auto-generated method stub
-		return null;
+		return RouteInterface.EMS0136;
 	}
 
 }
