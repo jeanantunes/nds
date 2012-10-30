74e96d2903496ba20888e49bd1cf13aa1dda3552
Alexandre Takashi <alexandre.takashi@gmail.com>
2012-10-30 18:55:13 -0200

    Criação do esqueleto da classe de route

---
+++ ndsi-engine/src/main/java/br/com/abril/nds/integracao/ems0136/route/EMS0136Route.java
@@ -0,0 +1,34 @@
+package br.com.abril.nds.integracao.ems0136.route;
+
+import br.com.abril.nds.integracao.engine.MessageProcessor;
+import br.com.abril.nds.integracao.engine.RouteInterface;
+import br.com.abril.nds.integracao.engine.data.CouchDBImportRouteTemplate;
+import br.com.abril.nds.integracao.model.canonic.InterfaceEnum;
+
+public class EMS0136Route extends CouchDBImportRouteTemplate {
+
+	@Override
+	public InterfaceEnum getInterfaceEnum() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getUri() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MessageProcessor getMessageProcessor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public RouteInterface getRouteInterface() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
