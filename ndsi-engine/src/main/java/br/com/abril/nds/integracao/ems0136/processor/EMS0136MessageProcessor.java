978c8a690033f85947ae4b2ad9b53a516e841fa0
Alexandre Takashi <alexandre.takashi@gmail.com>
2012-10-30 18:51:10 -0200

    Inclusão do método de salvar.

--- ndsi-engine/src/main/java/br/com/abril/nds/integracao/ems0136/processor/EMS0136MessageProcessor.java
+++ ndsi-engine/src/main/java/br/com/abril/nds/integracao/ems0136/processor/EMS0136MessageProcessor.java
@@ -1,7 +1,11 @@
 package br.com.abril.nds.integracao.ems0136.processor;
 
+import java.math.BigInteger;
+import java.util.Date;
+
 import org.hibernate.Criteria;
 import org.hibernate.FetchMode;
+import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -14,6 +18,12 @@ import br.com.abril.nds.integracao.service.DistribuidorService;
 import br.com.abril.nds.model.cadastro.Distribuidor;
 import br.com.abril.nds.model.cadastro.ProdutoEdicao;
 import br.com.abril.nds.model.integracao.EventoExecucaoEnum;
+import br.com.abril.nds.model.planejamento.Lancamento;
+import br.com.abril.nds.model.planejamento.LancamentoParcial;
+import br.com.abril.nds.model.planejamento.PeriodoLancamentoParcial;
+import br.com.abril.nds.model.planejamento.StatusLancamentoParcial;
+import br.com.abril.nds.model.planejamento.TipoLancamento;
+import br.com.abril.nds.model.planejamento.TipoLancamentoParcial;
 import br.com.abril.nds.repository.impl.AbstractRepository;
 
 @Component
@@ -67,6 +77,18 @@ public class EMS0136MessageProcessor extends AbstractRepository implements
 			return;
 		}
 		
+		LancamentoParcial lancamentoParcial = this.obterLancalmentoParcial(
+				produtoEdicao);
+		if (lancamentoParcial == null) {
+			
+			// new
+			this.salvarNovoLancamentoParcial(input, produtoEdicao);
+			
+		} else {
+			
+			// Update
+		}
+		
 	}
 	
 	/**
@@ -98,6 +120,103 @@ public class EMS0136MessageProcessor extends AbstractRepository implements
 		}
 	}
 
+	/**
+	 * Obtém o Lançamento Parcial já cadastrado para o ProdutoEdição informado.
+	 * 
+	 * @param produtoEdicao
+	 * 
+	 * @return
+	 */
+	private LancamentoParcial obterLancalmentoParcial(
+			ProdutoEdicao produtoEdicao) {
+		
+		Criteria criteria = getSession().createCriteria(LancamentoParcial.class);
+		criteria.add(Restrictions.eq("produtoEdicao", produtoEdicao));
+		
+		return (LancamentoParcial) criteria.uniqueResult();
+	}
+	
+	/**
+	 * Salva um novo Lançamento Parcial.
+	 * 
+	 * @param input
+	 * @param produtoEdicao
+	 */
+	private LancamentoParcial salvarNovoLancamentoParcial(EMS0136Input input, 
+			ProdutoEdicao produtoEdicao) {
+		
+		// Novo Lançamento Parcial:
+		LancamentoParcial lancamentoParcial = new LancamentoParcial();
+		lancamentoParcial.setProdutoEdicao(produtoEdicao);
+		lancamentoParcial.setLancamentoInicial(input.getDataLancamento());
+		lancamentoParcial.setRecolhimentoFinal(input.getDataRecolhimento());
+		lancamentoParcial.setStatus(StatusLancamentoParcial.PROJETADO);
+		
+		this.getSession().persist(lancamentoParcial);
+		
+		Lancamento lancamento = this.obterLancamento(produtoEdicao);
+		if (lancamento == null) {
+			
+			Date dtAgora = new Date();
+			lancamento = new Lancamento();
+			lancamento.setDataCriacao(dtAgora);
+			lancamento.setDataLancamentoPrevista(input.getDataLancamento());
+			lancamento.setDataLancamentoDistribuidor(input.getDataLancamento());
+			lancamento.setDataRecolhimentoDistribuidor(input.getDataRecolhimento());
+			lancamento.setDataRecolhimentoPrevista(input.getDataRecolhimento());
+			lancamento.setProdutoEdicao(produtoEdicao);
+			lancamento.setTipoLancamento(TipoLancamento.PARCIAL);
+			lancamento.setDataStatus(dtAgora);
+			lancamento.setReparte(BigInteger.ZERO);
+			lancamento.setRepartePromocional(BigInteger.ZERO);
+			lancamento.setSequenciaMatriz(Integer.valueOf(0));
+			
+			this.getSession().persist(lancamento);
+		}
+		
+		PeriodoLancamentoParcial pLancamentoParcial = 
+				lancamento.getPeriodoLancamentoParcial();
+		if (pLancamentoParcial == null || pLancamentoParcial.getId() == null) {
+			
+			pLancamentoParcial = new PeriodoLancamentoParcial();
+			pLancamentoParcial.setLancamentoParcial(lancamentoParcial);
+			pLancamentoParcial.setLancamento(lancamento);
+			pLancamentoParcial.setStatus(StatusLancamentoParcial.PROJETADO);
+			
+			TipoLancamentoParcial tipoLancamentoParcial = 
+					"F".equals(input.getTipoRecolhimento()) 
+						? TipoLancamentoParcial.FINAL 
+						: TipoLancamentoParcial.PARCIAL;
+			pLancamentoParcial.setTipo(tipoLancamentoParcial);
+			
+			this.getSession().persist(pLancamentoParcial);
+		} else {
+			
+			//TODO: Estudar cenário em que já existe PeriodoLancamentoParcial:
+			// update
+		}
+		
+		return lancamentoParcial;
+	}
+	
+	/**
+	 * Obtém  o Lançamento associado ao produto Edição.
+	 * 
+	 * @param produtoEdicao
+	 * @return
+	 */
+	private Lancamento obterLancamento(ProdutoEdicao produtoEdicao) {
+
+		Criteria criteria = getSession().createCriteria(Lancamento.class);
+		criteria.add(Restrictions.eq("produtoEdicao", produtoEdicao));
+		criteria.addOrder(Order.asc("dataLancamentoPrevista"));
+		
+		Lancamento lancamento = (Lancamento) criteria.list().get(0);
+		
+		return lancamento;
+	}
+		
+	
 	@Override
 	public void posProcess() {
 		// TODO Auto-generated method stub
