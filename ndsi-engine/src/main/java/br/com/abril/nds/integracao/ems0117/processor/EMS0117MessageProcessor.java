8bc134646ff092af9950ae687a7d54cf05f7d37b
Gabriel Mancini <gabriel.mancini@gmail.com>
2012-12-07 16:15:15 -0200

    fix 117 remove box de cota existente

--- ndsi-engine/src/main/java/br/com/abril/nds/integracao/ems0117/processor/EMS0117MessageProcessor.java
+++ ndsi-engine/src/main/java/br/com/abril/nds/integracao/ems0117/processor/EMS0117MessageProcessor.java
@@ -309,8 +309,7 @@ public class EMS0117MessageProcessor extends AbstractRepository implements
 			
 			setSituacaoCadastro(input, cota);
 
-			cota.setSugereSuspensao(true);
-			cota.setBox(box);
+			cota.setSugereSuspensao(true);			
 			cota.setPessoa(pessoa);
 
 			if (!input.getEndereco().isEmpty()
