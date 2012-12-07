670e93849147f24d26a41b1803ec0735a705861f
leandrolupianhez <leandro.lupianhez@gmail.com>
2012-12-07 10:09:46 -0200

    corrigido NDSHMLNOVO-142

---
+++ nds-model/src/main/java/br/com/abril/nds/dto/filtro/FiltroProdutoDTO.java
@@ -0,0 +1,107 @@
+package br.com.abril.nds.dto.filtro;
+
+import java.io.Serializable;
+
+import br.com.abril.nds.model.cadastro.TipoProduto;
+import br.com.abril.nds.util.export.Export;
+import br.com.abril.nds.util.export.Exportable;
+
+@Exportable
+public class FiltroProdutoDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Export(label="Código")
+	private String codigo;
+	
+	@Export(label="Produto")
+	private String nome;
+	
+	@Export(label="Fornecedor")
+	private String fornecedor;
+	
+	@Export(label="Editor")
+	private String editor;
+	
+	@Export(label="Tipo Produto")
+	private String getDescricaoTipoProduto(){
+		
+		return (this.tipoProduto!= null)?tipoProduto.getDescricao():"";
+	}
+	
+	private TipoProduto tipoProduto;
+	
+	private String sortOrder;
+	
+	private String sortName;
+
+	public FiltroProdutoDTO() {}
+	
+	public FiltroProdutoDTO(String codigo,String nome,String editor,String fornecedor, Long codigoTipoProduto,String sortOrder, String sortName) {
+		
+		this.codigo = codigo;
+		this.nome = nome;
+		this.editor = editor;
+		this.fornecedor = fornecedor;
+		this.sortName = sortName;
+		this.sortOrder = sortOrder;
+		this.tipoProduto = new TipoProduto();
+		this.tipoProduto.setCodigo(codigoTipoProduto);
+	}
+	
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getFornecedor() {
+		return fornecedor;
+	}
+
+	public void setFornecedor(String fornecedor) {
+		this.fornecedor = fornecedor;
+	}
+
+	public String getEditor() {
+		return editor;
+	}
+
+	public void setEditor(String editor) {
+		this.editor = editor;
+	}
+
+	public TipoProduto getTipoProduto() {
+		return tipoProduto;
+	}
+
+	public void setTipoProduto(TipoProduto tipoProduto) {
+		this.tipoProduto = tipoProduto;
+	}
+
+	public String getSortOrder() {
+		return sortOrder;
+	}
+
+	public void setSortOrder(String sortOrder) {
+		this.sortOrder = sortOrder;
+	}
+
+	public String getSortName() {
+		return sortName;
+	}
+
+	public void setSortName(String sortName) {
+		this.sortName = sortName;
+	}
+}
