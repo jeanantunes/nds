e45375c0899f68dd78d11ca313a9186eac28d0bd
sps <sergiopersil@gmail.com>
2012-12-11 00:20:38 -0200

    merge

--- nds-client/src/main/java/br/com/abril/nds/repository/impl/ExpedicaoRepositoryImpl.java
+++ nds-client/src/main/java/br/com/abril/nds/repository/impl/ExpedicaoRepositoryImpl.java
@@ -110,19 +110,11 @@ public class ExpedicaoRepositoryImpl extends AbstractRepositoryModel<Expedicao,L
 							.append(" when (diferenca.tipoDiferenca = 'SOBRA_EM') then (diferenca.qtde)")
 							.append(" else 0")
 						.append(" end )) as qntDiferenca, ")
-<<<<<<< HEAD
-						.append(" produtoEd.precoVenda * estudo.qtdeReparte, ")
-						.append(" juridica.razaoSocial ")
-						
-			.append(" ) ");
-		}	
-=======
 						.append(" sum(estudoCota.qtdeEfetiva)*sum(produtoEd.precoVenda), ")
 						.append(" juridica.razaoSocial ")
 						
 			.append(" ) ");
 		}
->>>>>>> DGBti/master
 		
 		hql.append( "FROM" )
 			.append(" Expedicao expedicao ")
