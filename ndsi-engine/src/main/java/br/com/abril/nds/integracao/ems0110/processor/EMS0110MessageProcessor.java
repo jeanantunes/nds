39be2a782542966583da96eaee5ccfeb63820176
Gabriel Mancini <gabriel.mancini@gmail.com>
2012-12-19 17:09:51 -0200

    fix 110

--- ndsi-engine/src/main/java/br/com/abril/nds/integracao/ems0110/processor/EMS0110MessageProcessor.java
+++ ndsi-engine/src/main/java/br/com/abril/nds/integracao/ems0110/processor/EMS0110MessageProcessor.java
@@ -307,7 +307,7 @@ public class EMS0110MessageProcessor extends AbstractRepository implements
 		Dimensao dimensao = new Dimensao();
 		Brinde brinde = new Brinde();
 
-		if (null == produto.getCodigoContexto() || !produto.getCodigoContexto().equals(input.getContextoProd())) {
+		if (null != produto.getCodigoContexto() && !produto.getCodigoContexto().equals(input.getContextoProd())) {
 
 			produto.setCodigoContexto(input.getContextoProd());
 			this.ndsiLoggerFactory.getLogger().logInfo(
@@ -316,7 +316,7 @@ public class EMS0110MessageProcessor extends AbstractRepository implements
 					"Atualizacao do Codigo Contexto Produto para: "
 							+ input.getContextoProd());
 		}
-		if (null != produto.getTipoProduto() && (null == produto.getTipoProduto().getCodigoNBM() || !produto.getTipoProduto().getCodigoNBM().equals(input.getCodNBM()))) {
+		if (null != produto.getTipoProduto() && null != produto.getTipoProduto().getCodigoNBM() && !produto.getTipoProduto().getCodigoNBM().equals(input.getCodNBM())) {
 
 			produto.getTipoProduto().setCodigoNBM(input.getCodNBM());
 			this.ndsiLoggerFactory.getLogger().logInfo(message,
@@ -324,7 +324,7 @@ public class EMS0110MessageProcessor extends AbstractRepository implements
 					"Atualizacao do Codigo NBM para: " + input.getCodNBM());
 		}
 
-		if (null == produto.getNomeComercial() || !produto.getNomeComercial().equals(input.getNomeComercial())) {
+		if (null != produto.getNomeComercial() && !produto.getNomeComercial().equals(input.getNomeComercial())) {
 
 			produto.setNomeComercial(input.getNomeComercial());
 			this.ndsiLoggerFactory.getLogger().logInfo(
