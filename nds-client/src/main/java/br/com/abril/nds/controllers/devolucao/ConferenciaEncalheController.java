05568cd44d64b4ed2263aa8650a0bf240d7558fd
Francisco Maximo Jr <francisco.maximo@discover.com.br>
2012-12-27 14:37:28 -0200

    Track 171: corrigido: erro de manter valor CE jornaleiro ao trocar de cota, erro de não poder salvar a conferencia após reabertura, erro de não buscar dados do mesmo produto para cota diferente.

--- nds-client/src/main/java/br/com/abril/nds/controllers/devolucao/ConferenciaEncalheController.java
+++ nds-client/src/main/java/br/com/abril/nds/controllers/devolucao/ConferenciaEncalheController.java
@@ -66,8 +66,6 @@ public class ConferenciaEncalheController extends BaseController {
 	
 	private static final String DADOS_DOCUMENTACAO_CONF_ENCALHE_COTA = "dadosDocumentacaoConfEncalheCota";
 	
-	private static final String VALOR_ENCALHE_JORNALEIRO = "valorEncalheJornaleiro";
-	
 	private static final String ID_BOX_LOGADO = "idBoxLogado";
 	
 	private static final String INFO_CONFERENCIA = "infoCoferencia";
@@ -1203,7 +1201,6 @@ public class ConferenciaEncalheController extends BaseController {
 		
 		this.session.removeAttribute(ID_BOX_LOGADO);
 		this.session.removeAttribute(INFO_CONFERENCIA);
-		this.session.removeAttribute(VALOR_ENCALHE_JORNALEIRO);
 		this.session.removeAttribute(NOTA_FISCAL_CONFERENCIA);
 		this.session.removeAttribute(SET_CONFERENCIA_ENCALHE_EXCLUIR);
 		this.session.removeAttribute(HORA_INICIO_CONFERENCIA);
