b5db4097123076c62de7c854d8c246f906e05270
Samuel Mendes <milkdf.dev@gmail.com>
2013-01-05 16:27:43 -0200

    Commit inicial

---
+++ nds-client/src/main/java/br/com/abril/nds/controllers/Distribuicao/PesquisarAreaDeInfluenciaController.java
@@ -0,0 +1,7 @@
+package br.com.abril.nds.controllers.Distribuicao;
+
+import br.com.abril.nds.controllers.BaseController;
+
+public class PesquisarAreaDeInfluenciaController extends BaseController {
+
+}
