067f9a480e28f3d9a30be930629f5ea8ab70bbba
Samuel Mendes <milkdf.dev@gmail.com>
2013-01-11 21:31:18 -0200

    Removendo sujeira

--- nds-client/src/main/java/br/com/abril/nds/controllers/Distribuicao/PesquisarAreaDeInfluenciaController.java
+++
@@ -1,7 +0,0 @@
-package br.com.abril.nds.controllers.Distribuicao;
-
-import br.com.abril.nds.controllers.BaseController;
-
-public class PesquisarAreaDeInfluenciaController extends BaseController {
-
-}
