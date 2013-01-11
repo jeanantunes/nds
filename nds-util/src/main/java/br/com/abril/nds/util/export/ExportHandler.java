86376e6aff2c9010c11fab660274b763febf5430
william-balan <william.balan.machado@gmail.com>
2013-01-11 15:13:11 -0200

    Alteração no componente de exportação PDF/Excel para que seja parametrizado os tamanhos das colunas.

--- nds-util/src/main/java/br/com/abril/nds/util/export/ExportHandler.java
+++ nds-util/src/main/java/br/com/abril/nds/util/export/ExportHandler.java
@@ -460,6 +460,8 @@ public class ExportHandler {
 			
 			exportHeader.setExhibitionOrder(exportAnnotation.exhibitionOrder());
 			
+			exportHeader.setColumnWidth(exportAnnotation.widthPercent() == 0f ? null : exportAnnotation.widthPercent());
+			
 			exportHeaders.add(exportHeader);
 		}
 	}
