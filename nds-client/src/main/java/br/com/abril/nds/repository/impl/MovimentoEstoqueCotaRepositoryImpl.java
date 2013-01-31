331a24070bf2337704f5a496da71e310a58a262c
Francisco Maximo Jr <francisco.maximo@discover.com.br>
2013-01-31 08:46:03 -0200

    Geração de movimentos financeiros e cobrança atualizados.

--- nds-client/src/main/java/br/com/abril/nds/repository/impl/MovimentoEstoqueCotaRepositoryImpl.java
+++ nds-client/src/main/java/br/com/abril/nds/repository/impl/MovimentoEstoqueCotaRepositoryImpl.java
@@ -2705,6 +2705,22 @@ public class MovimentoEstoqueCotaRepositoryImpl extends AbstractRepositoryModel<
 		query.setParameter("idLancamento", idLancamento);
 		
 		return query.list();
-	}  
+	}
+	
+	@Override
+	public Long obterIdProdutoEdicaoPorControleConferenciaEncalhe(Long idControleConferenciaEncalheCota){
 		
+		StringBuilder hql = new StringBuilder(" select produtoEdicao.id  ");
+		
+		hql.append(" from ConferenciaEncalhe conferenciaEncalhe ")
+		   .append(" join conferenciaEncalhe.movimentoEstoqueCota movimentoEstoqueCota ")
+		   .append(" join movimentoEstoqueCota.produtoEdicao produtoEdicao ")
+		   .append(" WHERE conferenciaEncalhe.controleConferenciaEncalheCota.id = :idControleConferenciaEncalheCota ");
+		
+		Query query = getSession().createQuery(hql.toString());
+		
+		query.setParameter("idControleConferenciaEncalheCota", idControleConferenciaEncalheCota);
+		
+		return (Long) query.uniqueResult();
+	}
 }
