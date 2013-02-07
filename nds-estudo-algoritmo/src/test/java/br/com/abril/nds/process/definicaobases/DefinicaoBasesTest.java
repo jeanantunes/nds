a6436f1acce31f10b893482e9fa93c836c91e6ee
pedroxs <pedroxs@gmail.com>
2013-02-07 10:55:13 -0200

    mais configurações...

---
+++ nds-estudo-algoritmo/src/test/java/br/com/abril/nds/process/definicaobases/DefinicaoBasesTest.java
@@ -0,0 +1,19 @@
+package br.com.abril.nds.process.definicaobases;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DefinicaoBasesTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testExecutarProcesso() {
+	fail("Not yet implemented");
+    }
+
+}
