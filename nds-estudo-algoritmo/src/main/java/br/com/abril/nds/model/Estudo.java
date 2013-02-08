7317dd787c0bd3d641932fbc86237925c11f5272
Adenilton Soares <adenilton.soares@infoa2.com.br>
2013-02-08 20:56:53 -0200

    .

--- nds-estudo-algoritmo/src/main/java/br/com/abril/nds/model/Estudo.java
+++ nds-estudo-algoritmo/src/main/java/br/com/abril/nds/model/Estudo.java
@@ -3,12 +3,7 @@ package br.com.abril.nds.model;
 import java.math.BigDecimal;
 import java.util.List;
 
-public class Estudo extends GenericDTO<Estudo> {
-
-    /**
-     * 
-     */
-    private static final long serialVersionUID = -8639160268952528714L;
+public class Estudo {
 
     private Integer id;
     private BigDecimal reparteDistribuir; // Reparte Total a ser distribuído
@@ -22,87 +17,84 @@ public class Estudo extends GenericDTO<Estudo> {
     private BigDecimal somatoriaReparteEdicoesAbertas; // Soma de Reparte de todas as edições abertas
 
     public void calculate() {
-	somatoriaVendaMedia = new BigDecimal(0);
-	for (Cota cota : cotas) {
-	    if (!cota.getClassificacao()
-		    .equals(ClassificacaoCota.ReparteFixado)
-		    || !cota.getClassificacao().equals(
-			    ClassificacaoCota.BancaSoComEdicaoBaseAberta)
-		    || !cota.getClassificacao().equals(
-			    ClassificacaoCota.RedutorAutomatico))
-		somatoriaVendaMedia.add(cota.getVendaMedia());
+    	somatoriaVendaMedia = new BigDecimal(0);
+    	for (Cota cota : cotas) {
+    		if (!cota.getClassificacao().equals(ClassificacaoCota.ReparteFixado)
+    				|| !cota.getClassificacao().equals(ClassificacaoCota.BancaSoComEdicaoBaseAberta)
+    				|| !cota.getClassificacao().equals(ClassificacaoCota.RedutorAutomatico))
+    			somatoriaVendaMedia.add(cota.getVendaMedia());
+    	}
+    }
+    
+	public BigDecimal getReparteDistribuir() {
+		return reparteDistribuir;
 	}
-    }
 
-    public BigDecimal getReparteDistribuir() {
-	return reparteDistribuir;
-    }
+	public void setReparteDistribuir(BigDecimal reparteDistribuir) {
+		this.reparteDistribuir = reparteDistribuir;
+	}
 
-    public void setReparteDistribuir(BigDecimal reparteDistribuir) {
-	this.reparteDistribuir = reparteDistribuir;
-    }
+	public ProdutoEdicao getProduto() {
+		return produto;
+	}
 
-    public ProdutoEdicao getProduto() {
-	return produto;
-    }
+	public void setProduto(ProdutoEdicao produto) {
+		this.produto = produto;
+	}
 
-    public void setProduto(ProdutoEdicao produto) {
-	this.produto = produto;
-    }
+	public List<ProdutoEdicao> getEdicoesBaseInsercaoManual() {
+	    return edicoesBaseInsercaoManual;
+	}
 
-    public List<ProdutoEdicao> getEdicoesBaseInsercaoManual() {
-	return edicoesBaseInsercaoManual;
-    }
+	public void setEdicoesBaseInsercaoManual(
+		List<ProdutoEdicao> edicoesBaseInsercaoManual) {
+	    this.edicoesBaseInsercaoManual = edicoesBaseInsercaoManual;
+	}
 
-    public void setEdicoesBaseInsercaoManual(
-	    List<ProdutoEdicao> edicoesBaseInsercaoManual) {
-	this.edicoesBaseInsercaoManual = edicoesBaseInsercaoManual;
-    }
+	public List<Cota> getCotas() {
+		return cotas;
+	}
 
-    public List<Cota> getCotas() {
-	return cotas;
-    }
+	public void setCotas(List<Cota> cotas) {
+		this.cotas = cotas;
+	}
 
-    public void setCotas(List<Cota> cotas) {
-	this.cotas = cotas;
-    }
+	public Parametro getParametro() {
+		return parametro;
+	}
 
-    public Parametro getParametro() {
-	return parametro;
-    }
-
-    public void setParametro(Parametro parametro) {
-	this.parametro = parametro;
-    }
-
-    @Override
+	public void setParametro(Parametro parametro) {
+		this.parametro = parametro;
+	}
+	
+	@Override
     public String toString() {
-	return "\nEstudo{\n\t" + "id: " + id + ", \n\treparteCalculado: "
-		+ reparteDistribuir + "\n}";
+        return "\nEstudo{\n\t"
+                + "id: " + id
+                + ", \n\treparteCalculado: " + reparteDistribuir +"\n}";
     }
 
-    public BigDecimal getSomatoriaVendaMedia() {
-	return somatoriaVendaMedia;
-    }
+	public BigDecimal getSomatoriaVendaMedia() {
+		return somatoriaVendaMedia;
+	}
 
-    public void setSomatoriaVendaMediaFinal(BigDecimal somatoriaVendaMedia) {
-	this.somatoriaVendaMedia = somatoriaVendaMedia;
-    }
+	public void setSomatoriaVendaMediaFinal(BigDecimal somatoriaVendaMedia) {
+		this.somatoriaVendaMedia = somatoriaVendaMedia;
+	}
 
-    public BigDecimal getReparteDistribuirInicial() {
-	return reparteDistribuirInicial;
-    }
+	public BigDecimal getReparteDistribuirInicial() {
+		return reparteDistribuirInicial;
+	}
 
-    public void setReparteDistribuirInicial(BigDecimal reparteDistribuirInicial) {
-	this.reparteDistribuirInicial = reparteDistribuirInicial;
-    }
+	public void setReparteDistribuirInicial(BigDecimal reparteDistribuirInicial) {
+		this.reparteDistribuirInicial = reparteDistribuirInicial;
+	}
 
-    public BigDecimal getSomatoriaReparteEdicoesAbertas() {
-	return somatoriaReparteEdicoesAbertas;
-    }
+	public BigDecimal getSomatoriaReparteEdicoesAbertas() {
+		return somatoriaReparteEdicoesAbertas;
+	}
 
-    public void setSomatoriaReparteEdicoesAbertas(
-	    BigDecimal somatoriaReparteEdicoesAbertas) {
-	this.somatoriaReparteEdicoesAbertas = somatoriaReparteEdicoesAbertas;
-    }
+	public void setSomatoriaReparteEdicoesAbertas(BigDecimal somatoriaReparteEdicoesAbertas) {
+		this.somatoriaReparteEdicoesAbertas = somatoriaReparteEdicoesAbertas;
+	}
 }
