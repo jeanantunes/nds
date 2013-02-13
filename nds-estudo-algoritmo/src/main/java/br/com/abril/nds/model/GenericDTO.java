e6491ae5534ea294884ff7d6a107de9d64987169
Adenilton Soares <adenilton.soares@infoa2.com.br>
2013-02-13 13:46:18 -0200

    .

--- nds-estudo-algoritmo/src/main/java/br/com/abril/nds/model/GenericDTO.java
+++ nds-estudo-algoritmo/src/main/java/br/com/abril/nds/model/GenericDTO.java
@@ -7,7 +7,5 @@ public abstract class GenericDTO<DTO> implements Serializable {
     /**
      * 
      */
-    private static final long serialVersionUID = -3825276693355678601L;
-
-    
+    private static final long serialVersionUID = -3825276693355678601L;    
 }
