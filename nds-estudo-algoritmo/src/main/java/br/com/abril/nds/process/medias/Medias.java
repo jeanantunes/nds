e3d48e40e3f87a54e19557702bb5c3a67076870c
Adenilton Soares <adenilton.soares@infoa2.com.br>
2013-02-22 13:40:36 -0300

    Processo Medias

--- nds-estudo-algoritmo/src/main/java/br/com/abril/nds/process/medias/Medias.java
+++ nds-estudo-algoritmo/src/main/java/br/com/abril/nds/process/medias/Medias.java
@@ -1,6 +1,11 @@
 package br.com.abril.nds.process.medias;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.TreeMap;
+
 import br.com.abril.nds.model.Cota;
+import br.com.abril.nds.model.ProdutoEdicao;
 import br.com.abril.nds.process.ProcessoAbstrato;
 import br.com.abril.nds.process.bonificacoes.Bonificacoes;
 import br.com.abril.nds.process.correcaovendas.CorrecaoVendas;
@@ -12,30 +17,61 @@ import br.com.abril.nds.process.correcaovendas.CorrecaoVendas;
  * <p style="white-space: pre-wrap;">
  * SubProcessos: - N/A Processo Pai: - N/A
  * 
- * Processo Anterior: {@link CorrecaoVendas} Próximo Processo:
- * {@link Bonificacoes}
- * </p>
+ * Processo Anterior: {@link CorrecaoVendas} Próximo Processo: {@link Bonificacoes} </p>
  */
 public class Medias extends ProcessoAbstrato {
 
     public Medias(Cota cota) {
 	super(cota);
     }
-    
+
     @Override
     protected void executarProcesso() {
-	
-//	BigDecimal vendaMediaCorrigida = BigDecimal.ZERO;
-//	
-//	List<EstoqueProdutoCota> listEstoqueProdutoCota = ((Cota) super.genericDTO).getEstoqueProdutoCotas();
-//	
-//	int qtdeEdicaoBase = listEstoqueProdutoCota.size();
-//	
-//	if(qtdeEdicaoBase < 3) {
-//	    EstoqueProdutoCota estoqueProdutoCota = 
-//	} else {
-//	    
-//	}
+
+	BigDecimal vendaMediaCorrigida = BigDecimal.ZERO;
+
+	Cota cota = (Cota) super.genericDTO;
+
+	List<ProdutoEdicao> listProdutoEdicoa = cota.getEdicoesRecebidas();
+
+	int qtdeEdicaoBase = listProdutoEdicoa.size();
+
+	BigDecimal totalPeso = BigDecimal.ZERO;
+	BigDecimal totalVendaMultiplyPeso = BigDecimal.ZERO;
+
+	TreeMap<BigDecimal, BigDecimal> treeVendaPeso = new TreeMap<BigDecimal, BigDecimal>();
+
+	int iProdutoEdicao = 0;
+	while (iProdutoEdicao < qtdeEdicaoBase) {
+
+	    ProdutoEdicao produtoEdicao = listProdutoEdicoa.get(iProdutoEdicao);
+
+	    BigDecimal peso = new BigDecimal(produtoEdicao.getPeso());
+	    BigDecimal vendaCorrigida = produtoEdicao.getVendaCorrigida();
+
+	    treeVendaPeso.put(vendaCorrigida, peso);
+
+	    totalPeso = totalPeso.add(peso);
+	    totalVendaMultiplyPeso = totalVendaMultiplyPeso.add(vendaCorrigida.multiply(peso));
+
+	    iProdutoEdicao++;
+	}
+
+	if (qtdeEdicaoBase < 3) {
+	    
+	    vendaMediaCorrigida = totalVendaMultiplyPeso.divide(totalPeso, 2, BigDecimal.ROUND_FLOOR);
+	    
+	} else {
+	    
+	    BigDecimal menorValor = treeVendaPeso.firstEntry().getKey();
+	    BigDecimal menorPeso = treeVendaPeso.firstEntry().getValue();
+	    BigDecimal menorMultiply = menorValor.multiply(menorPeso);
+	    
+	    vendaMediaCorrigida = totalVendaMultiplyPeso.subtract(menorMultiply).divide(totalPeso.subtract(menorPeso), 2, BigDecimal.ROUND_FLOOR);
+	    
+	}
+
+	cota.setVendaMedia(vendaMediaCorrigida);
     }
 
 }
---
+++ nds-test/src/main/java/br/com/abril/nds/process/medias/MediasTest.java
@@ -0,0 +1,107 @@
+package br.com.abril.nds.process.medias;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.math.BigDecimal;
+import java.util.Iterator;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import br.com.abril.nds.model.Cota;
+import br.com.abril.nds.model.ProdutoEdicao;
+
+public class MediasTest {
+
+    /**
+     * Testar se a venda médias é calculada conforme a quantidade de edições menor que três.
+     * 
+     * @param cota
+     */
+    @Test(dataProvider = "getCotaQuantidadeEdicoesMenorTresList", dataProviderClass = MediasDataProvider.class)
+    public void quantidadeEdicaoMenorTres(Cota cota) {
+
+	try {
+
+	    StringBuilder sbReporterLog = new StringBuilder();
+
+	    Medias medias = new Medias(cota);
+	    medias.executar();
+
+	    cota = (Cota) medias.getGenericDTO();
+
+	    Iterator<ProdutoEdicao> itProdutoEdicao = cota.getEdicoesRecebidas().iterator();
+
+	    while (itProdutoEdicao.hasNext()) {
+
+		ProdutoEdicao produtoEdicao = itProdutoEdicao.next();
+		gerarProdutoEdicaoLog(sbReporterLog, produtoEdicao);
+	    }
+
+	    BigDecimal vendaMedia = cota.getVendaMedia();
+
+	    boolean assertVendaMedia = (vendaMedia != null && (vendaMedia.compareTo(BigDecimal.ZERO) == 1));
+
+	    assertTrue("Venda Media : " + assertVendaMedia + " Cota : " + cota.getId(), assertVendaMedia);
+	    gerarReporterLog(cota, sbReporterLog, vendaMedia);
+
+	} catch (Exception e) {
+	    fail(e.getMessage());
+	}
+    }
+    
+    /**
+     * Testar se a venda médias é calculada conforme a quantidade de edições menor que três.
+     * 
+     * @param cota
+     */
+    @Test(dataProvider = "getCotaQuantidadeEdicoesMaiorIgualTresList", dataProviderClass = MediasDataProvider.class)
+    public void quantidadeEdicaoMaiorIgualTres(Cota cota) {
+
+	try {
+
+	    StringBuilder sbReporterLog = new StringBuilder();
+
+	    Medias medias = new Medias(cota);
+	    medias.executar();
+
+	    cota = (Cota) medias.getGenericDTO();
+
+	    Iterator<ProdutoEdicao> itProdutoEdicao = cota.getEdicoesRecebidas().iterator();
+
+	    while (itProdutoEdicao.hasNext()) {
+
+		ProdutoEdicao produtoEdicao = itProdutoEdicao.next();
+		gerarProdutoEdicaoLog(sbReporterLog, produtoEdicao);
+	    }
+
+	    BigDecimal vendaMedia = cota.getVendaMedia();
+
+	    boolean assertVendaMedia = (vendaMedia != null && (vendaMedia.compareTo(BigDecimal.ZERO) == 1));
+
+	    assertTrue("Venda Media : " + assertVendaMedia + " Cota : " + cota.getId(), assertVendaMedia);
+	    gerarReporterLog(cota, sbReporterLog, vendaMedia);
+
+	} catch (Exception e) {
+	    fail(e.getMessage());
+	}
+    }
+
+    private void gerarReporterLog(Cota cota, StringBuilder sbReporterLog, BigDecimal vendaMedia) {
+	Reporter.log("<p>Cota </p>");
+	Reporter.log("<p style='margin-left: 50px'>ID : " + cota.getId() + "</p>");
+	Reporter.log("<p style='margin-left: 50px'>-> Venda Media: " + vendaMedia + "</p>");
+	Reporter.log(sbReporterLog.toString());
+    }
+
+    private void gerarProdutoEdicaoLog(StringBuilder sbReporterLog, ProdutoEdicao produtoEdicao) {
+	sbReporterLog.append("<p style='margin-left: 100px'>Produto Edicao : " + produtoEdicao.getId() + "</p>");
+	sbReporterLog.append("<p style='margin-left: 100px'>Produto : " + produtoEdicao.getIdProduto() + "</p>");
+	sbReporterLog.append("<p style='margin-left: 150px'>Venda Corrigida : " + produtoEdicao.getVendaCorrigida() + "</p>");
+	sbReporterLog.append("<p style='margin-left: 150px'>Peso : " + produtoEdicao.getPeso() + "</p>");
+	sbReporterLog.append("<p style='margin-left: 150px'>Reparte : " + produtoEdicao.getReparte() + "</p>");
+	sbReporterLog.append("<p style='margin-left: 150px'>Venda : " + produtoEdicao.getVenda() + "</p>");
+    }
+
+}
