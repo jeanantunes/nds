9b10cd7e74a2daa12accb90e2359614243e5aa7d
foxlol <foxlol@gmail.com>
2013-02-23 11:03:46 -0300

    NDSHMLNOVO-337

--- nds-client/src/main/java/br/com/abril/nds/controllers/expedicao/CotaAusenteController.java
+++ nds-client/src/main/java/br/com/abril/nds/controllers/expedicao/CotaAusenteController.java
@@ -228,20 +228,28 @@ public class CotaAusenteController extends BaseController {
 		
 		List<CotaAusenteDTO> listaCotasAusentes = null;
 		
-		listaCotasAusentes = cotaAusenteService.obterCotasAusentes(filtro) ;
+		listaCotasAusentes = this.cotaAusenteService.obterCotasAusentes(filtro) ;
 		
 		if (listaCotasAusentes == null || listaCotasAusentes.isEmpty()){
 			throw new ValidacaoException(TipoMensagem.WARNING, WARNING_PESQUISA_SEM_RESULTADO);
 		}
 		
-		if(filtro.getData() != null) {
-			for(CotaAusenteDTO cotaAusenteDTO : listaCotasAusentes) {
-				if(!DateUtil.isHoje(DateUtil.parseDataPTBR(cotaAusenteDTO.getData())))
-						cotaAusenteDTO.setIdCotaAusente(null);
+		Date dataOperacao = this.distribuidorService.obterDataOperacaoDistribuidor();
+		
+		String dataOperacaoFormatada = DateUtil.formatarDataPTBR(dataOperacao);
+		
+		if (filtro.getData() != null) {
+			
+			for (CotaAusenteDTO cotaAusenteDTO : listaCotasAusentes) {
+				
+				if (!dataOperacaoFormatada.equals(cotaAusenteDTO.getData())) {
+					
+					cotaAusenteDTO.setIdCotaAusente(null);
+				}
 			}
 		}
 		
-		Long totalRegistros = cotaAusenteService.obterCountCotasAusentes(filtro);
+		Long totalRegistros = this.cotaAusenteService.obterCountCotasAusentes(filtro);
 		
 		TableModel<CellModelKeyValue<CotaAusenteDTO>> tableModel = new TableModel<CellModelKeyValue<CotaAusenteDTO>>();
 
@@ -362,7 +370,7 @@ public class CotaAusenteController extends BaseController {
 	 * @param numCota - Número da Cota
 	 */
 	@Post
-	public void enviarParaSuplementar(Date dataPesquisa, List<Integer> numCotas) {
+	public void enviarParaSuplementar(List<Integer> numCotas) {
 	
 		TipoMensagem status = TipoMensagem.SUCCESS;
 		
@@ -370,10 +378,15 @@ public class CotaAusenteController extends BaseController {
 		
 		try {
 			
-			if(numCotas == null) 
+			if (numCotas == null) {
+				
 				throw new ValidacaoException(TipoMensagem.WARNING, WARNING_NUMERO_COTA_NAO_INFORMADO);
+			}
 						
-			cotaAusenteService.declararCotaAusenteEnviarSuplementar(numCotas, dataPesquisa, this.getUsuarioLogado().getId());
+			Date dataOperacao = this.distribuidorService.obterDataOperacaoDistribuidor();
+			
+			this.cotaAusenteService.declararCotaAusenteEnviarSuplementar(
+				numCotas, dataOperacao, this.getUsuarioLogado().getId());
 			
 			mensagens.add(SUCESSO_ENVIO_SUPLEMENTAR);
 			
@@ -410,10 +423,13 @@ public class CotaAusenteController extends BaseController {
 	 * @param numCota
 	 */
 	@Post
-	public void carregarDadosRateio(Date dataPesquisa, List<Integer> numCotas) {
+	public void carregarDadosRateio(List<Integer> numCotas) {
+		
+		Date dataOperacao = this.distribuidorService.obterDataOperacaoDistribuidor();
 		
 		List<MovimentoEstoqueCotaDTO> movimentos = 
-				movimentoEstoqueCotaService.obterMovimentoDTOCotaPorTipoMovimento(dataPesquisa, numCotas, GrupoMovimentoEstoque.RECEBIMENTO_REPARTE);
+			this.movimentoEstoqueCotaService.obterMovimentoDTOCotaPorTipoMovimento(
+				dataOperacao, numCotas, GrupoMovimentoEstoque.RECEBIMENTO_REPARTE);
 		
 		result.use(Results.json()).from(movimentos, "result").recursive().serialize();
 	}
@@ -425,7 +441,7 @@ public class CotaAusenteController extends BaseController {
 	 * @param numCota
 	 */
 	@Post
-	public void realizarRateio(Date dataPesquisa, List<MovimentoEstoqueCotaDTO> movimentos, List<Integer> numCotas) {
+	public void realizarRateio(List<MovimentoEstoqueCotaDTO> movimentos, List<Integer> numCotas) {
 		
 		TipoMensagem status = TipoMensagem.SUCCESS;
 		
@@ -433,10 +449,15 @@ public class CotaAusenteController extends BaseController {
 		
 		try {
 			
-			if(numCotas == null) 
+			if (numCotas == null) { 
+				
 				throw new ValidacaoException(TipoMensagem.WARNING, WARNING_NUMERO_COTA_NAO_INFORMADO);
+			}
 			
-			cotaAusenteService.declararCotaAusenteRatearReparte(numCotas, dataPesquisa, this.getUsuarioLogado().getId() , movimentos);
+			Date dataOperacao = this.distribuidorService.obterDataOperacaoDistribuidor();
+			
+			this.cotaAusenteService.declararCotaAusenteRatearReparte(
+				numCotas, dataOperacao, this.getUsuarioLogado().getId() , movimentos);
 			
 			mensagens.add(SUCESSO_RATEIO);
 			
