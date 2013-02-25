faba7fbddfc86c6a98b982af68009c7b1a07187e
Roger Maedo <rogerjohnnes@gmail.com>
2013-02-25 13:33:40 -0300

    finalizacao implementacao print ao fechar conferencia encalhe

--- nds-client/src/main/webapp/scripts/scriptConferenciaEncalhe.js
+++ nds-client/src/main/webapp/scripts/scriptConferenciaEncalhe.js
@@ -765,7 +765,7 @@ var ConferenciaEncalhe = $.extend(true, {
 	
 	gerarDocumentosConferenciaEncalhe : function(tiposDocumento) {
 		
-		/*$.postJSON(contextPath + '/devolucao/conferenciaEncalhe/imprimirDocumentosCobranca',
+		$.postJSON(contextPath + '/devolucao/conferenciaEncalhe/imprimirDocumentosCobranca',
 				null,
 				function(resultado){
 					var w = window.open();
@@ -774,7 +774,7 @@ var ConferenciaEncalhe = $.extend(true, {
 					w.document.write('<html>');
 					w.document.write('<body>');
 					w.document.write('<pre>');
-					w.document.write(resultado);
+					w.document.write(resultado.resultado);
 					w.document.write('</pre>');
 					w.document.write('</body>');
 					w.document.write('</html>');
@@ -782,10 +782,10 @@ var ConferenciaEncalhe = $.extend(true, {
 					w.print();
 					w.close();
 				}
-		);*/
+		);
 		
-		var file = contextPath + '/devolucao/conferenciaEncalhe/imprimirDocumentosCobranca';
-		$('#download-iframe', ConferenciaEncalhe.workspace).attr('src', file);		
+		/*var file = contextPath + '/devolucao/conferenciaEncalhe/imprimirDocumentosCobranca';
+		$('#download-iframe', ConferenciaEncalhe.workspace).attr('src', file);*/		
 	},
 	
 	carregarGridItensNotaFiscal : function (modeloConferenciaEncalhe) {
