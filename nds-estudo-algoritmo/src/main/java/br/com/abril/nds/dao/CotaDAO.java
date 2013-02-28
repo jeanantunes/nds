f707391fbaef3f71b06e8918ac1f5e293992e263
pedroxs <pedroxs@gmail.com>
2013-02-28 15:19:54 -0300

    Ajustes na busca de Cotas para seleção de bancas.

--- nds-estudo-algoritmo/src/main/java/br/com/abril/nds/dao/CotaDAO.java
+++ nds-estudo-algoritmo/src/main/java/br/com/abril/nds/dao/CotaDAO.java
@@ -14,162 +14,185 @@ import br.com.abril.nds.model.ProdutoEdicaoBase;
 
 public class CotaDAO {
 
-    public List<Cota> getCotas(int limit) {
-	List<Cota> cotas = new ArrayList<Cota>();
-	try {
-	    StringBuffer sb = new StringBuffer("SELECT C.*, P.NOME FROM COTA C INNER JOIN PESSOA P ON (P.ID = C.PESSOA_ID) ORDER BY C.ID");
-	    if (limit > 0) {
-		sb.append(" limit ");
-		sb.append(limit);
-	    }
-	    PreparedStatement psmt = Conexao.getConexao().prepareStatement(sb.toString());
-	    ResultSet rs = psmt.executeQuery();
-	    while (rs.next()) {
-		Cota cota = new Cota();
-		cota.setId(rs.getLong("ID"));
-		// cota.setNumero(rs.getLong("NUMERO_COTA"));
-		// cota.setNomePessoa(rs.getString("NOME"));
+	public List<Cota> getCotas(int limit) {
+		List<Cota> cotas = new ArrayList<Cota>();
+		try {
+			StringBuffer sb = new StringBuffer("SELECT C.*, P.NOME FROM COTA C INNER JOIN PESSOA P ON (P.ID = C.PESSOA_ID) ORDER BY C.ID");
+			if (limit > 0) {
+				sb.append(" limit ");
+				sb.append(limit);
+			}
+			PreparedStatement psmt = Conexao.getConexao().prepareStatement(sb.toString());
+			ResultSet rs = psmt.executeQuery();
+			while (rs.next()) {
+				Cota cota = new Cota();
+				cota.setId(rs.getLong("ID"));
+				// cota.setNumero(rs.getLong("NUMERO_COTA"));
+				// cota.setNomePessoa(rs.getString("NOME"));
 
-		cotas.add(cota);
-	    }
-	} catch (Exception ex) {
-	    System.out.println("Ocorreu um erro ao tentar consultar as cotas");
-	    ex.printStackTrace();
-	}
-	return cotas;
-    }
-
-    public List<Cota> getCotas() {
-	return this.getCotas(-1);
-    }
-
-    public List<Cota> getCotaWithEstoqueProdutoCota() {
-	List<Cota> cotas = new ArrayList<Cota>();
-	try {
-	    StringBuilder query = new StringBuilder("SELECT C.*, P.NOME FROM COTA C ");
-	    query.append(" INNER JOIN PESSOA P ON (P.ID = C.PESSOA_ID) ");
-	    query.append(" WHERE EXISTS (SELECT EPC.* FROM ESTOQUE_PRODUTO_COTA EPC WHERE EPC.COTA_ID = C.ID) ");
-	    query.append(" ORDER BY C.ID ");
-
-	    PreparedStatement psmt = Conexao.getConexao().prepareStatement(query.toString());
-	    ResultSet rs = psmt.executeQuery();
-	    while (rs.next()) {
-		Cota cota = new Cota();
-		cota.setId(rs.getLong("ID"));
-		// cota.setNumero(rs.getLong("NUMERO_COTA"));
-		// cota.setNomePessoa(rs.getString("NOME"));
-
-		cotas.add(cota);
-	    }
-	} catch (Exception ex) {
-	    System.out.println("Ocorreu um erro ao tentar consultar as cotas");
-	    ex.printStackTrace();
-	}
-	return cotas;
-    }
-
-    public List<Cota> getCotasComEdicoesBase(List<ProdutoEdicaoBase> edicoesBase) {
-	List<Cota> returnListCota = new ArrayList<>();
-	try {
-	    Map<Long, Integer> idsPesos = new HashMap<Long, Integer>();
-	    for (ProdutoEdicaoBase edicao : edicoesBase) {
-		idsPesos.put(edicao.getId(), edicao.getPeso());
-	    }
-	    String idsString = idsPesos.keySet().toString().replaceAll("\\]|\\[", "");
-	    PreparedStatement ps = Conexao.getConexao().prepareStatement(SQL_PRODUTO_EDICAO_POR_COTA.replaceAll("\\?", idsString));
-	    ResultSet rs = ps.executeQuery();
-
-	    long prevIdCota = 0;
-	    while (rs.next()) {
-		long idCota = rs.getLong("COTA_ID");
-
-		if (prevIdCota != idCota) {
-		    Cota cota = new Cota();
-		    cota.setId(idCota);
-		    cota.setNumero(rs.getLong("NUMERO_COTA"));
-		    if (rs.getString("FORMA_AJUSTE").equals("vendaMedia")) {
-			cota.setVendaMediaMaisN(rs.getBigDecimal("AJUSTE_APLICADO"));
-		    } else if (rs.getString("FORMA_AJUSTE").equals("encalheMaximo")) {
-			cota.setPercentualEncalheMaximo(rs.getBigDecimal("AJUSTE_APLICADO"));
-		    }
-		    // TODO: ainda faltam carregar os parâmetros para ajuste de segmento e histórico
-		    // (estarão gravados no banco respectivamente: 'segmento' e 'histórico')
-		    cota.setEdicoesRecebidas(getEdicoes(rs, idsPesos, false));
-		    returnListCota.add(cota);
-		} else {
-		    Cota cota = returnListCota.get(returnListCota.size() - 1);
-		    cota.getEdicoesRecebidas().addAll(getEdicoes(rs, idsPesos, true));
+				cotas.add(cota);
+			}
+		} catch (Exception ex) {
+			System.out.println("Ocorreu um erro ao tentar consultar as cotas");
+			ex.printStackTrace();
 		}
-		prevIdCota = idCota;
-	    }
-
-	} catch (ClassNotFoundException | SQLException e) {
-	    e.printStackTrace();
-	}
-	return returnListCota;
-    }
-
-    private List<ProdutoEdicao> getEdicoes(ResultSet rs, Map<Long, Integer> idsPesos, boolean forceEdicaoFechada) throws SQLException {
-	List<ProdutoEdicao> edicoes = new ArrayList<ProdutoEdicao>();
-	ProdutoEdicao produtoEdicao = new ProdutoEdicao();
-
-	produtoEdicao.setIdProduto(rs.getLong("PRODUTO_ID"));
-	produtoEdicao.setId(rs.getLong("PRODUTO_EDICAO_ID"));
-	produtoEdicao.setIdLancamento(rs.getLong("LANCAMENTO_ID"));
-
-	// FIXME - gambeta loca remover!!!!!!!
-	if (forceEdicaoFechada) {
-	    produtoEdicao.setEdicaoAberta(false);
-	} else {
-	    produtoEdicao.setEdicaoAberta(traduzStatus(rs.getNString("STATUS")));
+		return cotas;
 	}
 
-	produtoEdicao.setParcial(rs.getString("TIPO_LANCAMENTO").equalsIgnoreCase(LANCAMENTO_PARCIAL));
-	produtoEdicao.setColecao(traduzColecionavel(rs.getNString("GRUPO_PRODUTO")));
-	produtoEdicao.setDataLancamento(rs.getDate("DATA_LCTO_DISTRIBUIDOR"));
-	produtoEdicao.setReparte(rs.getBigDecimal("QTDE_RECEBIDA"));
-	produtoEdicao.setVenda(rs.getBigDecimal("QTDE_VENDA"));
-
-	produtoEdicao.setPeso(idsPesos.get(produtoEdicao.getId()));
-
-	produtoEdicao.setNumeroEdicao(rs.getLong("NUMERO_EDICAO"));
-	produtoEdicao.setCodigoProduto(rs.getLong("CODIGO"));
-
-	edicoes.add(produtoEdicao);
-	return edicoes;
-    }
-
-    private boolean traduzStatus(String status) {
-	if (status != null && !status.equalsIgnoreCase(STATUS_FECHADO)) {
-	    return true;
+	public List<Cota> getCotas() {
+		return this.getCotas(-1);
 	}
-	return false;
-    }
 
-    private boolean traduzColecionavel(String grupoProduto) {
-	if (grupoProduto != null && grupoProduto.equalsIgnoreCase(PRODUTO_COLECIONAVEL)) {
-	    return true;
+	public List<Cota> getCotaWithEstoqueProdutoCota() {
+		List<Cota> cotas = new ArrayList<Cota>();
+		try {
+			StringBuilder query = new StringBuilder("SELECT C.*, P.NOME FROM COTA C ");
+			query.append(" INNER JOIN PESSOA P ON (P.ID = C.PESSOA_ID) ");
+			query.append(" WHERE EXISTS (SELECT EPC.* FROM ESTOQUE_PRODUTO_COTA EPC WHERE EPC.COTA_ID = C.ID) ");
+			query.append(" ORDER BY C.ID ");
+
+			PreparedStatement psmt = Conexao.getConexao().prepareStatement(query.toString());
+			ResultSet rs = psmt.executeQuery();
+			while (rs.next()) {
+				Cota cota = new Cota();
+				cota.setId(rs.getLong("ID"));
+				// cota.setNumero(rs.getLong("NUMERO_COTA"));
+				// cota.setNomePessoa(rs.getString("NOME"));
+
+				cotas.add(cota);
+			}
+		} catch (Exception ex) {
+			System.out.println("Ocorreu um erro ao tentar consultar as cotas");
+			ex.printStackTrace();
+		}
+		return cotas;
 	}
-	return false;
-    }
 
-    private static final String LANCAMENTO_PARCIAL = "PARCIAL";
-    private static final String PRODUTO_COLECIONAVEL = "COLECIONAVEL";
-    private static final String STATUS_FECHADO = "FECHADO";
+	public List<Cota> getCotasComEdicoesBase(List<ProdutoEdicaoBase> edicoesBase) {
+		List<Cota> returnListCota = new ArrayList<>();
+		try {
+			Map<Long, Integer> idsPesos = new HashMap<Long, Integer>();
+			for (ProdutoEdicaoBase edicao : edicoesBase) {
+				idsPesos.put(edicao.getId(), edicao.getPeso());
+			}
+			String idsString = idsPesos.keySet().toString().replaceAll("\\]|\\[", "");
+			PreparedStatement ps = Conexao.getConexao().prepareStatement(SQL_PRODUTO_EDICAO_POR_COTA.replaceAll("\\?", idsString));
+			ResultSet rs = ps.executeQuery();
 
-    private static final String SQL_PRODUTO_EDICAO_POR_COTA = " select "
-	    + " c.id as COTA_ID "
-	    + " ,c.NUMERO_COTA "
-	    + " ,p.id as PRODUTO_ID "
-	    + " ,pe.id as PRODUTO_EDICAO_ID "
-	    + " ,l.id as LANCAMENTO_ID "
-	    + " ,l.STATUS " // -- edicao aberta
-	    + " ,l.TIPO_LANCAMENTO " // -- se parcial
-	    + " ,tp.GRUPO_PRODUTO " // -- se colecionavel
-	    + " ,l.DATA_LCTO_DISTRIBUIDOR " + " ,epc.QTDE_RECEBIDA " + " ,(epc.QTDE_RECEBIDA - epc.QTDE_DEVOLVIDA) as QTDE_VENDA "
-	    + " ,pe.PACOTE_PADRAO " + " ,pe.NUMERO_EDICAO " + " ,p.CODIGO " + " ,ar.ajuste_aplicado " + " ,ar.forma_ajuste " + " from "
-	    + " produto_edicao pe " + " ,produto p " + " ,lancamento l " + " ,tipo_produto tp " + " ,cota c " + " ,estoque_produto_cota epc "
-	    + " ,ajuste_reparte ar " + " where " + " pe.id in (?) " + " and pe.PRODUTO_ID = p.id " + " and pe.ID = l.PRODUTO_EDICAO_ID "
-	    + " and tp.ID = p.TIPO_PRODUTO_ID " + " and pe.ID = epc.PRODUTO_EDICAO_ID " + " and c.ID = epc.COTA_ID "
-	    + " order by c.ID, pe.NUMERO_EDICAO desc ";
+			long prevIdCota = 0;
+			while (rs.next()) {
+				long idCota = rs.getLong("COTA_ID");
+
+				if (prevIdCota != idCota) {
+					Cota cota = new Cota();
+					cota.setId(idCota);
+					cota.setNumero(rs.getLong("NUMERO_COTA"));
+					traduzFormaAjuste(rs, cota);
+					// TODO: ainda faltam carregar os parâmetros para ajuste de segmento e histórico
+					// (estarão gravados no banco respectivamente: 'segmento' e 'histórico')
+					cota.setEdicoesRecebidas(getEdicoes(rs, idsPesos, false));
+					returnListCota.add(cota);
+				} else {
+					Cota cota = returnListCota.get(returnListCota.size() - 1);
+					cota.getEdicoesRecebidas().addAll(getEdicoes(rs, idsPesos, true));
+				}
+				prevIdCota = idCota;
+			}
+
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		return returnListCota;
+	}
+
+	private void traduzFormaAjuste(ResultSet rs, Cota cota) throws SQLException {
+		String formaAjuste = rs.getString("FORMA_AJUSTE");
+		if ("vendaMedia".equals(formaAjuste)) {
+			cota.setVendaMediaMaisN(rs.getBigDecimal("AJUSTE_APLICADO"));
+		} else if ("encalheMaximo".equals(formaAjuste)) {
+			cota.setPercentualEncalheMaximo(rs.getBigDecimal("AJUSTE_APLICADO"));
+		}
+	}
+
+	private List<ProdutoEdicao> getEdicoes(ResultSet rs, Map<Long, Integer> idsPesos, boolean forceEdicaoFechada) throws SQLException {
+		List<ProdutoEdicao> edicoes = new ArrayList<ProdutoEdicao>();
+		ProdutoEdicao produtoEdicao = new ProdutoEdicao();
+
+		produtoEdicao.setIdProduto(rs.getLong("PRODUTO_ID"));
+		produtoEdicao.setId(rs.getLong("PRODUTO_EDICAO_ID"));
+		produtoEdicao.setIdLancamento(rs.getLong("LANCAMENTO_ID"));
+
+		// FIXME - gambeta loca remover!!!!!!!
+		if (forceEdicaoFechada) {
+			produtoEdicao.setEdicaoAberta(false);
+		} else {
+			produtoEdicao.setEdicaoAberta(traduzStatus(rs.getNString("STATUS")));
+		}
+
+		produtoEdicao.setParcial(rs.getString("TIPO_LANCAMENTO").equalsIgnoreCase(LANCAMENTO_PARCIAL));
+		produtoEdicao.setColecao(traduzColecionavel(rs.getNString("GRUPO_PRODUTO")));
+		produtoEdicao.setDataLancamento(rs.getDate("DATA_LCTO_DISTRIBUIDOR"));
+		produtoEdicao.setReparte(rs.getBigDecimal("QTDE_RECEBIDA"));
+		produtoEdicao.setVenda(rs.getBigDecimal("QTDE_VENDA"));
+
+		produtoEdicao.setPeso(idsPesos.get(produtoEdicao.getId()));
+
+		produtoEdicao.setNumeroEdicao(rs.getLong("NUMERO_EDICAO"));
+		produtoEdicao.setCodigoProduto(rs.getLong("CODIGO"));
+
+		edicoes.add(produtoEdicao);
+		return edicoes;
+	}
+
+	private boolean traduzStatus(String status) {
+		if (status != null && !status.equalsIgnoreCase(STATUS_FECHADO)) {
+			return true;
+		}
+		return false;
+	}
+
+	private boolean traduzColecionavel(String grupoProduto) {
+		if (grupoProduto != null && grupoProduto.equalsIgnoreCase(PRODUTO_COLECIONAVEL)) {
+			return true;
+		}
+		return false;
+	}
+
+	private static final String LANCAMENTO_PARCIAL = "PARCIAL";
+	private static final String PRODUTO_COLECIONAVEL = "COLECIONAVEL";
+	private static final String STATUS_FECHADO = "FECHADO";
+
+	private static final String SQL_PRODUTO_EDICAO_POR_COTA = " select  "
+			+ "     c.id as COTA_ID, "
+			+ "     c.NUMERO_COTA, "
+			+ "     p.id as PRODUTO_ID, "
+			+ "     pe.id as PRODUTO_EDICAO_ID, "
+			+ "     l.id as LANCAMENTO_ID, "
+			+ "     l.STATUS, "
+			+ "     l.TIPO_LANCAMENTO, "
+			+ "     tp.GRUPO_PRODUTO, "
+			+ "     l.DATA_LCTO_DISTRIBUIDOR, "
+			+ "     epc.QTDE_RECEBIDA, "
+			+ "     (epc.QTDE_RECEBIDA - epc.QTDE_DEVOLVIDA) as QTDE_VENDA, "
+			+ "     pe.PACOTE_PADRAO, "
+			+ "     pe.NUMERO_EDICAO, "
+			+ "     p.CODIGO, "
+			+ "     ar.ajuste_aplicado, "
+			+ "     ar.forma_ajuste "
+			+ " from "
+			+ "     produto_edicao pe, "
+			+ "     produto p, "
+			+ "     lancamento l, "
+			+ "     tipo_produto tp, "
+			+ "     cota c, "
+			+ "     estoque_produto_cota epc "
+			+ " left outer join ajuste_reparte ar on ar.COTA_ID = epc.COTA_ID "
+			+ " where "
+			+ "     pe.id in (?) "
+			+ "         and pe.PRODUTO_ID = p.id "
+			+ "         and pe.ID = l.PRODUTO_EDICAO_ID "
+			+ "         and tp.ID = p.TIPO_PRODUTO_ID "
+			+ "         and pe.ID = epc.PRODUTO_EDICAO_ID "
+			+ "         and c.ID = epc.COTA_ID "
+			+ " order by c.ID , pe.NUMERO_EDICAO desc ";
 }
--- nds-estudo-algoritmo/src/test/java/br/com/abril/nds/GenericTest.java
+++ nds-estudo-algoritmo/src/test/java/br/com/abril/nds/GenericTest.java
@@ -1,86 +1,13 @@
 package br.com.abril.nds;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
-import java.util.TreeMap;
-
+import org.junit.Ignore;
 import org.junit.Test;
 
-import br.com.abril.nds.model.ProdutoEdicaoBase;
-
+@Ignore
 public class GenericTest {
 
-    @Test
-    public void test() throws IllegalArgumentException, IllegalAccessException, InvocationTargetException {
-	ProdutoEdicaoBase edicao = new ProdutoEdicaoBase();
-	edicao.setCodigoProduto(123L);
-	edicao.setColecao(false);
-	
-	Class<? extends ProdutoEdicaoBase> clazz = edicao.getClass();
-	Method[] methods = clazz.getMethods();
-	
-	for (Method method : methods) {
-	    String name = method.getName();
-	    if((name.startsWith("g") || name.startsWith("i")) && !name.equalsIgnoreCase("getClass")) {
-		System.out.println(name + ": " + method.invoke(edicao));
-	    }
+	@Test
+	public void test() {
 	}
 	
-	System.out.println("\r\nGenerics:\r\n");
-	fazTabela(edicao);
-	
-	List<ProdutoEdicaoBase> list = new ArrayList<>();
-	list.add(edicao);
-	list.add(edicao);
-	
-	fazTabela(list);
-    }
-    
-    private <T> void fazTabela(T type) {
-	
-	
-	
-	if(type instanceof List) {
-	    //print cabecalho
-	    for (Object t : (List<?>) type) {
-		fazTabela(t);
-	    }
-	}
-	
-	Class<? extends Object> clazz = type.getClass();
-	
-	Method[] methods = clazz.getMethods();
-	
-	Map<String, Method> sortedMethods = new TreeMap<String, Method>();
-	
-	for (Method method : methods) {
-	    String name = method.getName();
-	    if((name.startsWith("get") || name.startsWith("is")) && !name.equalsIgnoreCase("getClass")) {
-		sortedMethods.put(name.replaceAll("is|get", ""), method);
-	    }
-	}
-	
-	Map<String, Method> finalOrderMethods = new LinkedHashMap<String, Method>();
-	if(sortedMethods.containsKey("Id")) {
-	    finalOrderMethods.put("Id", sortedMethods.remove("Id"));
-	}
-	if(sortedMethods.containsKey("Numero")) {
-	    finalOrderMethods.put("Numero", sortedMethods.remove("Numero"));
-	}
-	if(sortedMethods.containsKey("NumeroEdicao")) {
-	    finalOrderMethods.put("NumeroEdicao", sortedMethods.remove("NumeroEdicao"));
-	}
-	finalOrderMethods.putAll(sortedMethods);
-	
-	Set<Entry<String, Method>> entrySet = finalOrderMethods.entrySet();
-	for (Entry<String, Method> entry : entrySet) {
-	    System.out.println(entry.getKey());
-	}
-    }
 }
