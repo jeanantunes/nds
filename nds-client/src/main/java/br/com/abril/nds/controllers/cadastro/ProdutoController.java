a38677d3f8da54f8949d097ec31b32dfc62deeec
Francisco Maximo Jr <francisco.maximo@discover.com.br>
2013-03-07 09:31:33 -0300

    Tracs 314 e 397

--- nds-client/src/main/java/br/com/abril/nds/controllers/cadastro/ProdutoController.java
+++ nds-client/src/main/java/br/com/abril/nds/controllers/cadastro/ProdutoController.java
@@ -554,6 +554,12 @@ public class ProdutoController extends BaseController {
 				listaMensagens.add("O preenchimento do campo [% Desconto] é obrigatório!");
 			}
 			
+			if (produto.getDesconto() != null && 
+					(produto.getDesconto().compareTo(new BigDecimal(100)) > 0 ||
+					produto.getDesconto().compareTo(BigDecimal.ZERO) < 0)){
+				listaMensagens.add("Preenchimento do campo [% Desconto] inválido!");
+			}
+			
 			if (codigoTipoProduto == null || codigoTipoProduto.intValue() == 0) {
 				listaMensagens.add("O preenchimento do campo [Tipo de Produto] é obrigatório!");
 			}
