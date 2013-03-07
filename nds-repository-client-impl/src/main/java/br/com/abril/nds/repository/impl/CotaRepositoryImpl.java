d04cbb5bb61de46687ae62b54d00bd7b7aebb694
Lazaro Junior <linkout.lazaro@gmail.com>
2013-03-07 18:28:02 -0300

    Fix 2028

--- nds-repository-client-impl/src/main/java/br/com/abril/nds/repository/impl/CotaRepositoryImpl.java
+++ nds-repository-client-impl/src/main/java/br/com/abril/nds/repository/impl/CotaRepositoryImpl.java
@@ -2436,7 +2436,7 @@ public class CotaRepositoryImpl extends AbstractRepositoryModel<Cota, Long>
 		
 		hql.append(" FROM RankingFaturamento rankingFaturamento ");
 		hql.append(" INNER JOIN rankingFaturamento.rankingFaturamentoGerado as rankingFaturamentoGerado ");
-		hql.append(" LEFT JOIN rankingFaturamento.cota as cota ");
+		hql.append(" RIGHT JOIN rankingFaturamento.cota as cota ");
 		hql.append(" LEFT JOIN cota.pessoa as pessoa ");
 		
 		hql.append(" WHERE ");
