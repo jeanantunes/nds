35e051ea946f3ef01c25ee19ef21d8537faf0737
wallace7souza <wallace.c.souza@gmail.com>
2013-03-08 12:23:25 -0300

    Versionamento

--- nds-estudo-algoritmo/src/main/java/br/com/abril/nds/process/calculoreparte/ReparteComplementarPorCota.java
+++ nds-estudo-algoritmo/src/main/java/br/com/abril/nds/process/calculoreparte/ReparteComplementarPorCota.java
@@ -3,6 +3,7 @@ package br.com.abril.nds.process.calculoreparte;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.com.abril.nds.dao.RankingSegmentoDAO;
 import br.com.abril.nds.model.ClassificacaoCota;
 import br.com.abril.nds.model.Cota;
 import br.com.abril.nds.model.Estudo;
@@ -22,10 +23,12 @@ import br.com.abril.nds.process.ProcessoAbstrato;
 public class ReparteComplementarPorCota extends ProcessoAbstrato {
 
 	private List<Ordenador> ordenadorList = new ArrayList<Ordenador>();
+	private RankingSegmentoDAO rankingSegmentoDAO; 
 	
     public ReparteComplementarPorCota(Estudo estudo) {
 	super(estudo);
-	
+
+	this.rankingSegmentoDAO = new RankingSegmentoDAO();
 //	Prioridade de recebimento de reparte:
 	
 	/*
@@ -34,14 +37,19 @@ public class ReparteComplementarPorCota extends ProcessoAbstrato {
 	 */
 	
 	ordenadorList.add(new Ordenador("A"){
+		
 		@Override
-		boolean filtrar(Cota cota) {
-			if(cota.getClassificacao().equals(ClassificacaoCota.BancaComReparteZeroMinimoZeroCotaAntiga)){
-				
-				return true;
+		void filtrar(List<Cota> cotaListRecebeComplementar) {
+
+			List<Cota> cList = new ArrayList<Cota>();
+			for (Cota cota : cotaListRecebeComplementar) {
+				if(cota.getClassificacao().equals(ClassificacaoCota.BancaComReparteZeroMinimoZeroCotaAntiga)){
+					cList.add(cota);
+				}
 			}
 			
-			return false;
+//			rankingSegmentoDAO.getCotasOrdenadaPorSegmentoEdicaoAberta(cList, getEstudo().getEdicoesBase());
+			
 		}
 	});
 	
@@ -50,26 +58,33 @@ public class ReparteComplementarPorCota extends ProcessoAbstrato {
 	 */
 	ordenadorList.add(new Ordenador("B"){
 		@Override
-		boolean filtrar(Cota cota) {
-			if(cota.getClassificacao().equals(ClassificacaoCota.BancaComReparteZeroMinimoZeroCotaAntiga)){
-				
-				return true;
+		void filtrar(List<Cota> cotaListRecebeComplementar) {
+			
+			List<Cota> cList = new ArrayList<Cota>();
+			for (Cota cota : cotaListRecebeComplementar) {
+				if(cota.getClassificacao().equals(ClassificacaoCota.BancaComReparteZeroMinimoZeroCotaAntiga)){
+					cList.add(cota);
+				}
 			}
-			return false;
+			
+			List<Long> cotasOrdenadaPorSegmentoSemEdicaoBase = rankingSegmentoDAO.getCotasOrdenadaPorSegmentoSemEdicaoBase(cList, getEstudo().getEdicoesBase());
 		}
 	});
 	
 	/*
-	 * C: As que receberam 1 edi��o das edi��es-base, da maior para a menor no ranking de segmento da publica��o (cotas VZ);
+	 * C: As que receberam 1 ediçãoo das ediçõees-base, da maior para a menor no ranking de segmento da publicação (cotas VZ);
 	 */
 	ordenadorList.add(new Ordenador("C"){
 		@Override
-		boolean filtrar(Cota cota) {
-			if(cota.getClassificacao().equals(ClassificacaoCota.BancaComTotalVendaZeraMinimoZeroCotaAntiga)){
-				
-				return true;
+		void filtrar(List<Cota> cotaListRecebeComplementar) {
+			List<Cota> cList = new ArrayList<Cota>();
+			for (Cota cota : cotaListRecebeComplementar) {
+				if(cota.getClassificacao().equals(ClassificacaoCota.BancaComTotalVendaZeraMinimoZeroCotaAntiga)){
+					cList.add(cota);
+				}
 			}
-			return false;
+			
+			List<Long> cotasOrdenadaPorSegmento = rankingSegmentoDAO.getCotasOrdenadaPorSegmento(cList, getEstudo().getEdicoesBase(),1);
 		}
 	});
 	
@@ -78,12 +93,15 @@ public class ReparteComplementarPorCota extends ProcessoAbstrato {
 	 */
 	ordenadorList.add(new Ordenador("D"){
 		@Override
-		boolean filtrar(Cota cota) {
-			if(cota.getClassificacao().equals(ClassificacaoCota.BancaComTotalVendaZeraMinimoZeroCotaAntiga)){
-				
-				return true;
+		void filtrar(List<Cota> cotaListRecebeComplementar) {
+			List<Cota> cList = new ArrayList<Cota>();
+			for (Cota cota : cotaListRecebeComplementar) {
+				if(cota.getClassificacao().equals(ClassificacaoCota.BancaComTotalVendaZeraMinimoZeroCotaAntiga)){
+					cList.add(cota);
+				}
 			}
-			return false;
+			
+			List<Long> cotasOrdenadaPorSegmento = rankingSegmentoDAO.getCotasOrdenadaPorSegmento(cList, getEstudo().getEdicoesBase(),2);
 		}
 	});
 	
@@ -92,12 +110,15 @@ public class ReparteComplementarPorCota extends ProcessoAbstrato {
 	 */
 	ordenadorList.add(new Ordenador("E"){
 		@Override
-		boolean filtrar(Cota cota) {
-			if(cota.getClassificacao().equals(ClassificacaoCota.BancaComTotalVendaZeraMinimoZeroCotaAntiga)){
-				
-				return true;
+		void filtrar(List<Cota> cotaListRecebeComplementar) {
+			List<Cota> cList = new ArrayList<Cota>();
+			for (Cota cota : cotaListRecebeComplementar) {
+				if(cota.getClassificacao().equals(ClassificacaoCota.BancaComTotalVendaZeraMinimoZeroCotaAntiga)){
+					cList.add(cota);
+				}
 			}
-			return false;
+			
+			List<Long> cotasOrdenadaPorSegmento = rankingSegmentoDAO.getCotasOrdenadaPorSegmento(cList, getEstudo().getEdicoesBase(),3);
 		}
 	});
 	
@@ -122,18 +143,13 @@ public class ReparteComplementarPorCota extends ProcessoAbstrato {
    	
 //    	3)	Orden�-las na seguinte prioridade de recebimento de reparte:
     	for(Ordenador ordenador:this.ordenadorList){
-    		for(Cota c:cotaListRecebeComplementar){
-    			if(ordenador.filtrar(c)){
-    				//TODO: FAZER ORDENACAO
-    				cotaListOrdenada.add(c);
-    			}
-    		}
+    		ordenador.filtrar(cotaListRecebeComplementar);
     	}
     	
     	/*
-    	 * 4)	As bancas receber�o a quantidade de reparte por banca definido no estudo (default = 2 exemplares)
-    	 *  ou 1 pacote-padr�o se a distribui��o for por m�ltiplos 
-    	 * at� acabar o reparte complementar, sempre considerando-se a prioriza��o acima. 
+    	 * 4)	As bancas receberao a quantidade de reparte por banca definido no estudo (default = 2 exemplares)
+    	 *  ou 1 pacote-padr�o se a distribuicao for por multiplos 
+    	 * ate acabar o reparte complementar, sempre considerando-se a prioriza��o acima. 
     	 * Caso haja saldo a distribuir e todas as bancas selecionadas j� receberam, 
     	 * enviar 1 exemplar ou 1 pacote-padr�o se a distribui��o for por m�ltiplos para as bancas do estudo normal, 
     	 * da maior para a menor at� finalizar o estoque. 
@@ -145,15 +161,13 @@ public class ReparteComplementarPorCota extends ProcessoAbstrato {
     				&& !c.getClassificacao().equals(ClassificacaoCota.MaximoMinimo)){
     			//TODO: FAZER REDISTRIBUICAO
     			
-//    			5)	Marcar cotas com �CP�
+//    			5)	Marcar cotas com 'CP'
     			c.setClassificacao(ClassificacaoCota.BancaEstudoComplementar);
     		}
     	}
     	
-    	getEstudo().setCotas(cotaListOrdenada);
     	
     }
-    
     
    //FIXME talvez usar o Guava do google para ordenar? 
    private abstract class Ordenador{
@@ -161,7 +175,7 @@ public class ReparteComplementarPorCota extends ProcessoAbstrato {
 	   public Ordenador(String name){
 		   this.name=name;
 	   }
-    	abstract boolean filtrar(Cota cota);
+    	abstract void filtrar(List<Cota> cotaListRecebeComplementar) ;
 		public String getName() {
 			return name;
 		}
