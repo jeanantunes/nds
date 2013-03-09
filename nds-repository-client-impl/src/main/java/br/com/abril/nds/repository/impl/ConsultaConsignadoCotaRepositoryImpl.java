4548dbfdac49589282f4c0568fec626938a7e151
direx <diogo.vecchiati@gmail.com>
2013-03-09 14:03:28 -0300

    fix consulta consignado

--- nds-repository-client-impl/src/main/java/br/com/abril/nds/repository/impl/ConsultaConsignadoCotaRepositoryImpl.java
+++ nds-repository-client-impl/src/main/java/br/com/abril/nds/repository/impl/ConsultaConsignadoCotaRepositoryImpl.java
@@ -47,25 +47,26 @@ public class ConsultaConsignadoCotaRepositoryImpl extends AbstractRepositoryMode
 		   .append(" pe.id as produtoEdicaoId, 				")	
 		   .append(" pe.numeroEdicao as numeroEdicao, 		")
 		   .append(" pessoa.razaoSocial as nomeFornecedor, 	")
-		   .append(" lancamento.dataLancamentoDistribuidor as dataLancamento,")
+		   .append(" case when lancamento is not null then lancamento.dataLancamentoDistribuidor else movimento.data end as dataLancamento,")
 		   .append(" coalesce(movimento.valoresAplicados.precoVenda, pe.precoVenda, 0) as precoCapa, ")
 		   
 		   .append(" coalesce(movimento.valoresAplicados.valorDesconto, 0) as desconto, ")
 		   .append(" coalesce(movimento.valoresAplicados.precoComDesconto, pe.precoVenda, 0) as precoDesconto, ")
-		   .append(" movimento.qtde as reparte, ")
+		   .append("	    (sum((case when tipoMovimento.operacaoEstoque = 'ENTRADA'  then movimento.qtde else 0 end) ")
+		   .append("	      - (case when tipoMovimento.operacaoEstoque = 'SAIDA' then movimento.qtde else 0 end) )) as reparte, ")
 		   .append(" ( coalesce( ")
 		   .append("		movimento.valoresAplicados.precoVenda, pe.precoVenda, 0) * ")
-		   .append("	    sum((case when tipoMovimento = 'ENTRADA' then movimento.qtde else 0 end) ")
-		   .append("	      - (case when tipoMovimento = 'SAIDA' then movimento.qtde else 0 end) )  ")
+		   .append("	    (sum((case when tipoMovimento.operacaoEstoque = 'ENTRADA' then movimento.qtde else 0 end) ")
+		   .append("	      - (case when tipoMovimento.operacaoEstoque = 'SAIDA' then movimento.qtde else 0 end) ))  ")
 		   .append(" ) as total, ")
 		   .append(" ( coalesce( ")
 		   .append("        movimento.valoresAplicados.precoComDesconto, pe.precoVenda, 0) * ")
-		   .append("	    sum((case when tipoMovimento = 'ENTRADA' then movimento.qtde else 0 end) ")
-		   .append("	      - (case when tipoMovimento = 'SAIDA' then movimento.qtde else 0 end) )  ")
+		   .append("	    (sum((case when tipoMovimento.operacaoEstoque = 'ENTRADA' then movimento.qtde else 0 end) ")
+		   .append("	      - (case when tipoMovimento.operacaoEstoque = 'SAIDA' then movimento.qtde else 0 end) ) ) ")
 		   .append(" ) as totalDesconto ");
 		   
 		hql.append(" FROM MovimentoEstoqueCota movimento, ProdutoEdicao pe ");
-		hql.append(" LEFT join movimento.lancamento lancamento ");
+		hql.append("  LEFT JOIN movimento.lancamento lancamento ");
 		hql.append("  JOIN movimento.cota as cota ");
 		hql.append("  JOIN movimento.tipoMovimento as tipoMovimento ");
 		hql.append("  JOIN pe.produto as produto ");
@@ -73,7 +74,6 @@ public class ConsultaConsignadoCotaRepositoryImpl extends AbstractRepositoryMode
 		hql.append("  JOIN produto.fornecedores as fornecedor ");
 		hql.append("  JOIN fornecedor.juridica as pessoa ");		
 		hql.append("  JOIN cota.pessoa as pessoaCota ");
-		hql.append("  LEFT JOIN movimento.movimentoEstoqueCotaFuro as movimentoEstoqueCotaFuro ");
 		
 		hql.append(" WHERE movimento.produtoEdicao.id = pe.id " );
 		
@@ -84,7 +84,7 @@ public class ConsultaConsignadoCotaRepositoryImpl extends AbstractRepositoryMode
 			hql.append("   AND fornecedor.id = :idFornecedor");
 		}
 		
-		hql.append(" GROUP BY lancamento.dataLancamentoDistribuidor ");
+		hql.append(" GROUP BY case when lancamento is not null then lancamento.dataLancamentoDistribuidor else movimento.data end ");
 		
 		if (filtro.getPaginacao().getSortColumn() != null) {
 			hql.append(" ORDER BY ");
@@ -98,7 +98,13 @@ public class ConsultaConsignadoCotaRepositoryImpl extends AbstractRepositoryMode
 
 		Query query =  getSession().createQuery(hql.toString());
 				
-		buscarParametrosConsignadoCota(query, filtro);
+		if(filtro.getIdCota() != null ) { 
+			query.setParameter("idCota", filtro.getIdCota());
+		}
+		
+		if(filtro.getIdFornecedor() != null ) { 
+			query.setParameter("idFornecedor", filtro.getIdFornecedor());
+		}
 		
 		query.setResultTransformer(new AliasToBeanResultTransformer(
 				ConsultaConsignadoCotaDTO.class));
