c76269438900c059d6371a9117a51b78eddbb77e
Thiago <thiagoandrade6@gmail.com>
2013-03-12 18:55:45 -0300

    desenglobacao

---
+++ nds-service-client/src/main/java/br/com/abril/nds/service/DesenglobacaoService.java
@@ -0,0 +1,14 @@
+package br.com.abril.nds.service;
+
+import java.util.List;
+
+import br.com.abril.nds.client.vo.DesenglobaVO;
+import br.com.abril.nds.dto.DesenglobacaoDTO;
+import br.com.abril.nds.model.seguranca.Usuario;
+
+public interface DesenglobacaoService {
+	
+	List<DesenglobacaoDTO> obterDesenglobacaoPorCota(Long cotaId);
+	
+	boolean inserirDesenglobacao(List<DesenglobaVO> desenglobaDTO, Usuario usuario);
+}
