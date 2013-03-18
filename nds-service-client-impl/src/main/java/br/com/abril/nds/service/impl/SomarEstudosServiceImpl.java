624f66aa0bfc14e21964012c5f2f13c8cc28f4ee
unknown <INFOA2@NOTE-INFOA2-004.(none)>
2013-03-18 17:55:08 -0300

    EMS 2030

---
+++ nds-service-client-impl/src/main/java/br/com/abril/nds/service/impl/SomarEstudosServiceImpl.java
@@ -0,0 +1,122 @@
+package br.com.abril.nds.service.impl;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.abril.nds.client.vo.ProdutoDistribuicaoVO;
+import br.com.abril.nds.enums.TipoMensagem;
+import br.com.abril.nds.exception.ValidacaoException;
+import br.com.abril.nds.model.cadastro.Cota;
+import br.com.abril.nds.model.planejamento.Estudo;
+import br.com.abril.nds.model.planejamento.EstudoCota;
+import br.com.abril.nds.repository.DistribuicaoRepository;
+import br.com.abril.nds.repository.EstudoCotaRepository;
+import br.com.abril.nds.repository.EstudoRepository;
+import br.com.abril.nds.repository.ProdutoEdicaoRepository;
+import br.com.abril.nds.service.SomarEstudosService;
+
+@Service
+public class SomarEstudosServiceImpl implements SomarEstudosService {
+	
+	@Autowired
+	private EstudoRepository estudoRepository;
+	
+	@Autowired
+	private EstudoCotaRepository estudoCotaRepository;
+	
+	@Autowired
+	private ProdutoEdicaoRepository produtoEdicaoRepository;
+
+	@Autowired
+	private DistribuicaoRepository distribuicaoRepository;
+
+
+	@Override
+	@Transactional
+	public void somarEstudos(Long idEstudoBase, ProdutoDistribuicaoVO distribuicaoVO) {
+		
+		if (!produtoEdicaoRepository.estudoPodeSerSomado(idEstudoBase, distribuicaoVO.getCodigoProduto())) {
+			
+			return;
+			//throw new ValidacaoException(TipoMensagem.WARNING, "Estudo não pode ser somado.");
+		}
+		
+		Estudo estudoBase = estudoRepository.buscarPorId(idEstudoBase);
+		
+		Map<Long,EstudoCota> mapEstudoCota = new HashMap<Long,EstudoCota>();
+		
+		if (estudoBase.getEstudoCotas() != null && !estudoBase.getEstudoCotas().isEmpty()) {
+			
+			Iterator<EstudoCota> it =  estudoBase.getEstudoCotas().iterator(); 
+			
+			while (it.hasNext()) {
+				
+				EstudoCota estudoCota = it.next();
+				Cota cota = estudoCota.getCota();
+				
+				if(cota != null) {
+					
+					mapEstudoCota.put(cota.getId(), estudoCota);
+				}
+			}
+		}
+		
+		if (mapEstudoCota.isEmpty()) {
+			
+			return; 
+		}
+		
+		Long idEstudo = distribuicaoVO.getIdEstudo().longValue();
+		
+		Estudo estudo = estudoRepository.buscarPorId(idEstudo);
+		
+		if (estudo.getEstudoCotas() != null && !estudo.getEstudoCotas().isEmpty()) {
+			
+			Iterator<EstudoCota> it =  estudo.getEstudoCotas().iterator(); 
+			
+			while (it.hasNext()) {
+				
+				EstudoCota estudoCota = it.next();
+				
+				if (estudoCota.getCota() != null) {
+					
+					Long idCota = estudoCota.getCota().getId();
+					
+					if (mapEstudoCota.containsKey(idCota)) {
+					  	
+						EstudoCota estudoCotaBase = mapEstudoCota.remove(idCota);
+						
+						if (estudoCotaBase.getReparte() != null && estudoCota.getReparte() != null) {
+							
+							estudoCota.setReparte(estudoCota.getReparte().add(estudoCotaBase.getReparte()));
+						}
+					}
+				}
+			}
+		
+			Iterator<Entry<Long,EstudoCota>> itMap = mapEstudoCota.entrySet().iterator();
+			
+			while (itMap.hasNext()) {
+				
+				EstudoCota estudoCota = itMap.next().getValue();
+				estudoCota.setEstudo(estudo);
+				estudoCotaRepository.alterar(estudoCota);
+			}
+			
+			estudoRepository.alterar(estudo);
+			estudoRepository.remover(estudoBase);
+		}
+		
+	}
+	
+	
+	
+}
