869180e0f6d29a5a19001f32b11683e73c751684
Thiago <thiagoandrade6@gmail.com>
2013-03-18 18:19:57 -0300

    servico agora contemplando XLS e XLSX

--- nds-util/src/main/java/br/com/abril/nds/util/upload/XlsUploaderUtils.java
+++ nds-util/src/main/java/br/com/abril/nds/util/upload/XlsUploaderUtils.java
@@ -12,36 +12,34 @@ import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-public abstract class XlsUploaderUtils {
+public class XlsUploaderUtils {
+	
+	private static final String XLS = ".xls";
+	private static final String XLSX = ".xlsx";
 	
 	public static List<KeyValue> returnKeyValueFromXls(File file) {
+		
+		String extension = file.getName().substring(file.getName().lastIndexOf("."));
 		
 		List<KeyValue> list = new ArrayList<KeyValue>();
 		
 		try {
 			
 			FileInputStream xls = new FileInputStream(file);
-			HSSFWorkbook workbook = new HSSFWorkbook(xls);
-			HSSFSheet sheet = workbook.getSheetAt(0);
 			
-			int header = 0;
-			int content = 1;
-			
-			Iterator<Row> rowIterator = sheet.rowIterator();
-			while (rowIterator.hasNext()) {
-				Row row = (Row) rowIterator.next();
-				Iterator<Cell> cellIterator = row.cellIterator();
-				while (cellIterator.hasNext()) {
-					Cell cell = (Cell) cellIterator.next();
-					if (sheet.getRow(content) != null) {
-						KeyValue keyValue = new KeyValue();
-						keyValue.setKey(sheet.getRow(header).getCell(cell.getColumnIndex()).getStringCellValue());
-						keyValue.setValue(returnCellValue(sheet.getRow(content).getCell(cell.getColumnIndex())));
-						list.add(keyValue);
-					}
-				}
-				content++;
+			if (extension.equals(XLS)) {
+				HSSFWorkbook workbook = new HSSFWorkbook(xls);
+				HSSFSheet sheet = workbook.getSheetAt(0);
+				getContent(list, sheet);
+				
+			} else if (extension.equals(XLSX)) {
+				XSSFWorkbook workbook = new XSSFWorkbook(xls);
+				XSSFSheet sheet = workbook.getSheetAt(0);
+				getContent(list, sheet);
 			}
 			
 			xls.close();
@@ -53,6 +51,27 @@ public abstract class XlsUploaderUtils {
 		}
 		
 		return list;
+	}
+
+	private static <T extends Sheet> void getContent(List<KeyValue> list, T sheet) {
+		int header = 0;
+		int content = 1;
+		
+		Iterator<Row> rowIterator = sheet.rowIterator();
+		while (rowIterator.hasNext()) {
+			Row row = (Row) rowIterator.next();
+			Iterator<Cell> cellIterator = row.cellIterator();
+			while (cellIterator.hasNext()) {
+				Cell cell = (Cell) cellIterator.next();
+				if (sheet.getRow(content) != null) {
+					KeyValue keyValue = new KeyValue();
+					keyValue.setKey(sheet.getRow(header).getCell(cell.getColumnIndex()).getStringCellValue());
+					keyValue.setValue(returnCellValue(sheet.getRow(content).getCell(cell.getColumnIndex())));
+					list.add(keyValue);
+				}
+			}
+			content++;
+		}
 	}
 
 	private static Object returnCellValue(Cell cell) {
