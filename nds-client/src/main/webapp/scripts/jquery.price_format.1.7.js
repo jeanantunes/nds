1d09fcd95157a4bbe82ab6d5117f5990abe092a2
Sérgio Peres <sergio@HDU100213.(none)>
2013-03-18 21:57:54 -0300

    formatacao duas casas monetarias

--- nds-client/src/main/webapp/scripts/jquery.price_format.1.7.js
+++ nds-client/src/main/webapp/scripts/jquery.price_format.1.7.js
@@ -164,7 +164,7 @@
 			// inster formatted price as a value of an input field
 			function price_it ()
 			{
-				var str = obj.val();
+				var str = parseFloat(obj.val()).toFixed(centsLimit);
 				var price = price_format(str);
 				if (str != price) obj.val(price);
 			}
