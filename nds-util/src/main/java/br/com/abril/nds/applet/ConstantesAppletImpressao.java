8c86973469e089b7efa560a9a7494fc46d7e2cc2
Robson Martins <robson.java@gmail.com>
2013-04-04 20:45:41 -0300

    update

---
+++ nds-util/src/main/java/br/com/abril/nds/applet/ConstantesAppletImpressao.java
@@ -0,0 +1,49 @@
+package br.com.abril.nds.applet;
+
+public interface ConstantesAppletImpressao {
+
+	//Constantes ref a impressao na matricial
+		int MAX_UNITS = 127; //for vertical positioning range is between 0 - 255 (0 <= n <= 255) according to epson ref. but 255 gives weird errors at 1.5f, 127 as max (0 - 128) seems to be working
+	    float CM_PER_INCH = 2.54f;
+
+	    /* decimal ascii values for epson ESC/P commands */
+	    
+	    String ESC = "\u001B"; //escape
+	    String AT =(char) 64+""; //@
+	    String LINE_FEED = (char) 10+""; //line feed/new line
+	    String PARENTHESIS_LEFT = (char)40+"";
+	    String BACKSLASH = (char)92+"";
+	    String CR = (char)13+""; //carriage return
+	    String TAB = (char)9+""; //horizontal tab
+	    String FF = (char)12+""; //form feed
+	    String SI = (char)15+""; //condense
+	    String g = (char)103+""; //15cpi pitch
+	    String p = (char)112+""; //used for choosing proportional mode or fixed-pitch
+	    String t = (char)116+""; //used for Stringacter set assignment/selection
+	    String k = (char)107+""; //used for font
+	    String l = (char)108+""; //used for setting left margin
+	    String x = (char)120+""; //used for setting draft or letter quality (LQ) printing
+	    String C = (char)67+""; //used for page lenght
+	    String E = (char)69+""; //bold font on
+	    String F = (char)70+""; //bold font off
+	    String J = (char)74+""; //used for advancing paper vertically
+	    String P = (char)80+""; //10cpi pitch
+	    String Q = (char)81+""; //used for setting right margin
+	    String NULL = (char)0 +""; //used for setting right margin
+
+	    String $ = (char)36+""; //used for absolute horizontal positioning
+	    String ARGUMENT_0 = (char)0+"";
+	    String ARGUMENT_1 = (char)1+"";
+	    String ARGUMENT_2 = (char)2+"";
+	    String ARGUMENT_3 = (char)3+"";
+	    String ARGUMENT_4 = (char)4+"";
+	    String ARGUMENT_5 = (char)5+"";
+	    String ARGUMENT_6 = (char)6+"";
+	    String ARGUMENT_7 = (char)7+"";
+	    String ARGUMENT_25 = (char)25+"";
+
+	    /* Stringacter sets */
+	    String USA = ARGUMENT_1;
+	    String BRAZIL = ARGUMENT_25;
+	    String CARACTER_INDENT_LINEFEED_SCAPE = "###";
+}
---
+++ nds-util/src/main/java/br/com/abril/nds/util/TirarAcento.java
@@ -0,0 +1,64 @@
+package br.com.abril.nds.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TirarAcento {
+
+    private static Map<Character, Character> mapCharSubstituir = new HashMap();
+
+    static {
+        mapCharSubstituir.put('á', 'a');
+        mapCharSubstituir.put('Á', 'A');
+        mapCharSubstituir.put('à', 'a');
+        mapCharSubstituir.put('À', 'A');
+        mapCharSubstituir.put('â', 'a');
+        mapCharSubstituir.put('Â', 'A');
+        mapCharSubstituir.put('ã', 'a');
+        mapCharSubstituir.put('Ã', 'A');
+        mapCharSubstituir.put('é', 'e');
+        mapCharSubstituir.put('É', 'E');
+        mapCharSubstituir.put('è', 'e');
+        mapCharSubstituir.put('È', 'E');
+        mapCharSubstituir.put('ê', 'e');
+        mapCharSubstituir.put('Ê', 'E');
+        mapCharSubstituir.put('í', 'i');
+        mapCharSubstituir.put('Í', 'I');
+        mapCharSubstituir.put('ì', 'i');
+        mapCharSubstituir.put('Ì', 'I');
+        mapCharSubstituir.put('î', 'i');
+        mapCharSubstituir.put('Î', 'I');
+        mapCharSubstituir.put('Ó', 'O');
+        mapCharSubstituir.put('ò', 'o');
+        mapCharSubstituir.put('Ò', 'O');
+        mapCharSubstituir.put('õ', 'o');
+        mapCharSubstituir.put('Õ', 'O');
+        mapCharSubstituir.put('ô', 'o');
+        mapCharSubstituir.put('Ô', 'O');
+        mapCharSubstituir.put('ú', 'u');
+        mapCharSubstituir.put('Ú', 'U');
+        mapCharSubstituir.put('ù', 'u');
+        mapCharSubstituir.put('Ù', 'U');
+        mapCharSubstituir.put('ü', 'u');
+        mapCharSubstituir.put('Ü', 'U');
+        mapCharSubstituir.put('û', 'u');
+        mapCharSubstituir.put('Û', 'U');
+        mapCharSubstituir.put('ç', 'c');
+        mapCharSubstituir.put('Ç', 'C');
+        mapCharSubstituir.put('ñ', 'n');
+        mapCharSubstituir.put('Ñ', 'N');
+    }
+
+    public static String removerAcentuacao(String texto) {
+        StringBuilder sb = new StringBuilder(texto);
+        {
+            for (int i = 0; i < sb.length(); i++) {
+                Character c = mapCharSubstituir.get(sb.charAt(i));
+                if (c != null) {
+                    sb.setCharAt(i, c);
+                }
+            }
+            return sb.toString();
+        }
+    }
+}
