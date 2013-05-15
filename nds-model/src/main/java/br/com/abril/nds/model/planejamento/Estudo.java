292c8988ad5609c8d31615c109296234c6cf8bff
Samuel Mendes <milkdf.dev@gmail.com>
2013-05-15 17:57:29 -0300

    finalizando as alterações pedidas no jtrac 47

--- nds-model/src/main/java/br/com/abril/nds/model/planejamento/Estudo.java
+++ nds-model/src/main/java/br/com/abril/nds/model/planejamento/Estudo.java
@@ -88,6 +88,9 @@ public class Estudo implements Serializable {
 	@Column(name = "REPARTE_DISTRIBUIR")
 	private BigInteger reparteDistribuir;
 	
+	@Column(name = "SOBRA")
+	private BigInteger sobra;
+	
 	@Column(name = "DISTRIBUICAO_POR_MULTIPLOS")
 	private Integer distribuicaoPorMultiplos; //TODO no estudo usa boolean, verificar alteração
 	
@@ -228,5 +231,12 @@ public class Estudo implements Serializable {
 		this.lancamentoID = lancamentoID;
 	}
 
+	public BigInteger getSobra() {
+		return sobra;
+	}
+
+	public void setSobra(BigInteger sobra) {
+		this.sobra = sobra;
+	}
 
 }
