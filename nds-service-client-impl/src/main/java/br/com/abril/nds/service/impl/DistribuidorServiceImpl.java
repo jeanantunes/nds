90fc58d04133096a1cfdfc4d4649dc77c1aaf037
Sérgio Peres <sergiopersil@gmail.com>
2013-06-28 22:11:04 -0300

    acerto de import

--- nds-service-client-impl/src/main/java/br/com/abril/nds/service/impl/DistribuidorServiceImpl.java
+++ nds-service-client-impl/src/main/java/br/com/abril/nds/service/impl/DistribuidorServiceImpl.java
@@ -3,7 +3,6 @@ package br.com.abril.nds.service.impl;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
