b993e486d19e6385bebbf96f3357879bfee7e5d5
Robson Martins <robson.java@gmail.com>
2013-08-19 18:20:07 -0300

    Correcoes interface

--- nds-integration-client/src/main/java/br/com/abril/nds/integracao/ems0116/route/EMS0116Route.java
+++ nds-integration-client/src/main/java/br/com/abril/nds/integracao/ems0116/route/EMS0116Route.java
@@ -1,17 +1,11 @@
 package br.com.abril.nds.integracao.ems0116.route;
 
-import java.util.Date;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-import br.com.abril.nds.integracao.ems0106.route.EMS0106Route;
 import br.com.abril.nds.integracao.ems0116.inbound.EMS0116Input;
 import br.com.abril.nds.integracao.ems0116.processor.EMS0116MessageProcessor;
-import br.com.abril.nds.integracao.ems0117.route.EMS0117Route;
 import br.com.abril.nds.integracao.engine.MessageProcessor;
 import br.com.abril.nds.integracao.engine.RouteInterface;
 import br.com.abril.nds.integracao.engine.data.FixedLengthRouteTemplate;
@@ -19,7 +13,6 @@ import br.com.abril.nds.integracao.engine.data.FixedLengthRouteTemplate;
 @Component
 @Scope("prototype")
 public class EMS0116Route extends FixedLengthRouteTemplate {
-	private static final Logger LOGGER = LoggerFactory.getLogger(EMS0106Route.class);
 	
 	@Autowired
 	private EMS0116MessageProcessor messageProcessor;
