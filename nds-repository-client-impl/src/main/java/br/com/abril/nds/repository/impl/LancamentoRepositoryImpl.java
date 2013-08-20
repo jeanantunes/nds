3ef12c2366eceadf2c84b2a8e79a02a7efbeb0f0
lhmarcilli <lhmarcilli@gmail.com>
2013-08-19 21:58:52 -0300

    Correção de obtenção de Lançamento na Gravação de Faltas e Sobras por Cota;

--- nds-repository-client-impl/src/main/java/br/com/abril/nds/repository/impl/LancamentoRepositoryImpl.java
+++ nds-repository-client-impl/src/main/java/br/com/abril/nds/repository/impl/LancamentoRepositoryImpl.java
@@ -830,6 +830,31 @@ public class LancamentoRepositoryImpl extends
 		return (lancamento!=null) ? (Lancamento) lancamento : null ;		
 	}
 
+	@Override
+	public Lancamento obterUltimoLancamentoDaEdicaoParaCota(Long idProdutoEdicao, Long idCota) {
+
+		StringBuilder hql = new StringBuilder();
+
+		hql.append(" select lancamento ")
+		   .append(" from Lancamento lancamento ")
+		   .append(" join lancamento.movimentoEstoqueCotas mec ")
+		   .append(" join mec.cota cota ")
+		   .append(" where lancamento.dataLancamentoPrevista = ")
+		   .append(" (select max(lancamentoMaxDate.dataLancamentoPrevista) ")
+		   .append(" from Lancamento lancamentoMaxDate where lancamentoMaxDate.produtoEdicao.id=:idProdutoEdicao ) ")
+		   .append(" and lancamento.produtoEdicao.id=:idProdutoEdicao ")
+		   .append(" and cota.id=:idCota ");
+		
+		Query query = getSession().createQuery(hql.toString());
+		
+		query.setParameter("idProdutoEdicao", idProdutoEdicao);
+		query.setParameter("idCota", idCota);
+		
+		Object lancamento = query.uniqueResult();
+		
+		return (lancamento!=null) ? (Lancamento) lancamento : null ;		
+	}
+	
 	@SuppressWarnings("unchecked")
 	@Override
 	public List<Lancamento> obterLancamentosEdicao(Long idProdutoEdicao, String sortorder, String sortname) {
