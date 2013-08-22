dcb20ad1c598be53e5f21813227985ad36b655a2
wallace7souza <wallace.c.souza@gmail.com>
2013-08-22 19:03:45 -0300

    Versionando execucao do Quartz

--- nds-client/src/main/java/br/com/abril/nds/client/listener/ApplicationContextListener.java
+++ nds-client/src/main/java/br/com/abril/nds/client/listener/ApplicationContextListener.java
@@ -66,20 +66,20 @@ public class ApplicationContextListener implements ServletContextListener {
 	@Override
 	public void contextInitialized(ServletContextEvent servletContextEvent) {
 
-//		this.agendarIntegracaoOperacionalDistribuidor();
-//		this.agendaExclusaoAjusteReparte();
-//		this.agendarExclusaoDeEstudos();
-//		this.agendarGeracaoRankings();
-//		this.agendaExeclusaoFixacaoReparte();
-//		this.agendaExeclusaoRegiao();
-//		
-//		try {
-//			StdSchedulerFactory.getDefaultScheduler().start();
-//		} catch (SchedulerException e) {
-//			logger.fatal("Falha ao inicializar agendador do Quartz", e);
-//
-//			throw new RuntimeException(e);
-//		}
+		this.agendarIntegracaoOperacionalDistribuidor();
+		this.agendaExclusaoAjusteReparte();
+		this.agendarExclusaoDeEstudos();
+		this.agendarGeracaoRankings();
+		this.agendaExeclusaoFixacaoReparte();
+		this.agendaExeclusaoRegiao();
+		
+		try {
+			StdSchedulerFactory.getDefaultScheduler().start();
+		} catch (SchedulerException e) {
+			logger.fatal("Falha ao inicializar agendador do Quartz", e);
+
+			throw new RuntimeException(e);
+		}
 
 	}
 
