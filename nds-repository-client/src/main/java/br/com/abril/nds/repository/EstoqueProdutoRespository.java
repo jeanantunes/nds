b7fbd2b7c6f7e405603fc88e91fce36b57f064d6
Sérgio Peres <sergiopersil@gmail.com>
2013-08-27 22:41:46 -0300

    Fix - Trac 802 - Adicao de historico de estoque ao Fechamento Diario

--- nds-repository-client/src/main/java/br/com/abril/nds/repository/EstoqueProdutoRespository.java
+++ nds-repository-client/src/main/java/br/com/abril/nds/repository/EstoqueProdutoRespository.java
@@ -1,11 +1,16 @@
 package br.com.abril.nds.repository;
 
+import java.util.List;
+
 import br.com.abril.nds.model.estoque.EstoqueProduto;
+import br.com.abril.nds.model.estoque.EstoqueProdutoDTO;
 
 public interface EstoqueProdutoRespository extends Repository<EstoqueProduto, Long> {
 
 	EstoqueProduto buscarEstoquePorProduto(Long idProdutoEdicao);
 	
 	EstoqueProduto buscarEstoqueProdutoPorProdutoEdicao(Long idProdutoEdicao);
+	
+	List<EstoqueProdutoDTO> buscarEstoquesProdutos();
 
 }
