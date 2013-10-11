b09b63573045b6b77f66a294ab46117a326d8412
lhmarcilli <lhmarcilli@gmail.com>
2013-10-11 12:18:57 -0300

    Item 77 - Planilha S4;

--- nds-service-client-impl/src/main/java/br/com/abril/nds/service/impl/FechamentoEncalheServiceImpl.java
+++ nds-service-client-impl/src/main/java/br/com/abril/nds/service/impl/FechamentoEncalheServiceImpl.java
@@ -644,13 +644,13 @@ public class FechamentoEncalheServiceImpl implements FechamentoEncalheService {
 			
 		}
 		
-		//CANCELA DIVIDA EXCLUI CONSOLIDADO E MOVIMENTOS FINANCEIROS DE REPARTE X ENCALHE (ENVIO_ENCALHE E DEBITO_SOBRE_FATURAMENTO) PARA QUE SEJAM RECRIADOS
+		//CANCELA DIVIDA EXCLUI CONSOLIDADO E MOVIMENTOS FINANCEIROS DE REPARTE X ENCALHE (RECEBIMENTO_REPARTE E ENVIO_ENCALHE) PARA QUE SEJAM RECRIADOS
 		this.gerarCobrancaService.cancelarDividaCobranca(null, 
 														 cota.getId(), 
 				                                         dataOperacaoDistribuidor, 
 				                                         true);
 
-		//CRIA MOVIMENTOS FINANCEIROS DE REPARTE X ENCALHE (ENVIO_ENCALHE E DEBITO_SOBRE_FATURAMENTO)
+		//CRIA MOVIMENTOS FINANCEIROS DE REPARTE X ENCALHE (RECEBIMENTO_REPARTE E ENVIO_ENCALHE)
 		movimentoFinanceiroCotaService.gerarMovimentoFinanceiroCota(cota, 
 																	dataOperacaoDistribuidor,
 																	usuario,
