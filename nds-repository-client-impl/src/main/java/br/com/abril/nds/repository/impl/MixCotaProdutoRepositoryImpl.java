01798e5cf3cc378b2dceadad0c9d24c171932582
pedroxs <pedroxs@gmail.com>
2013-10-12 18:59:03 -0300

    Ajustes no atributo TIPO_CLASSIFICACAO_PRODUTO_ID de Produto para ProdutoEdicao.

--- nds-repository-client-impl/src/main/java/br/com/abril/nds/repository/impl/MixCotaProdutoRepositoryImpl.java
+++ nds-repository-client-impl/src/main/java/br/com/abril/nds/repository/impl/MixCotaProdutoRepositoryImpl.java
@@ -81,10 +81,10 @@ public class MixCotaProdutoRepositoryImpl extends
 //		.append(" coalesce((select round(lc.reparte,0) from lancamento lc where lc.produto_edicao_id=produto_edicao.id and lancamento.status in ('LANÇADA','CALCULADA') limit 1),0) as ultimoReparte ")
 		.append(" FROM mix_cota_produto ") 
 		.append(" LEFT join produto on mix_cota_produto.ID_PRODUTO = produto.ID ")
-//		.append(" LEFT join produto_edicao on produto_edicao.PRODUTO_ID = produto.ID ") 
+		.append(" LEFT join produto_edicao on produto_edicao.PRODUTO_ID = produto.ID ")
 //		.append(" LEFT join lancamento on lancamento.PRODUTO_EDICAO_ID = produto_edicao.ID")
 //		.append(" LEFT join estoque_produto_cota epc on epc.cota_id = mix_cota_produto.ID_COTA ")
-		.append(" LEFT join tipo_classificacao_produto ON tipo_classificacao_produto.ID = produto.TIPO_CLASSIFICACAO_PRODUTO_ID ")
+		.append(" LEFT join tipo_classificacao_produto ON tipo_classificacao_produto.ID = produto_edicao.TIPO_CLASSIFICACAO_PRODUTO_ID ")
 		.append(" LEFT join usuario on usuario.ID = mix_cota_produto.ID_USUARIO ")
 
 		.append(" where ");
@@ -200,11 +200,11 @@ public class MixCotaProdutoRepositoryImpl extends
 		.append(" FROM mix_cota_produto ") 
 		.append(" LEFT join produto on mix_cota_produto.ID_PRODUTO = produto.ID ")
 		.append(" LEFT join cota on mix_cota_produto.ID_COTA = cota.ID ")
-//		.append(" LEFT join produto_edicao on produto_edicao.PRODUTO_ID = produto.ID ") 
+		.append(" LEFT join produto_edicao on produto_edicao.PRODUTO_ID = produto.ID ")
 //		.append(" LEFT join lancamento on lancamento.PRODUTO_EDICAO_ID = produto_edicao.ID")
 //		.append(" LEFT join estoque_produto_cota epc on epc.cota_id = cota.id ")
 //		.append(" and epc.produto_edicao_id in (select id from produto_edicao where produto_id = (produto.id)) ")
-		.append(" LEFT join tipo_classificacao_produto ON tipo_classificacao_produto.ID = produto.TIPO_CLASSIFICACAO_PRODUTO_ID ")
+		.append(" LEFT join tipo_classificacao_produto ON tipo_classificacao_produto.ID = produto_edicao.TIPO_CLASSIFICACAO_PRODUTO_ID ")
 		.append(" LEFT join usuario on usuario.ID = mix_cota_produto.ID_USUARIO ")
 		.append(" LEFT join pessoa on cota.pessoa_id = pessoa.id ")
 		
