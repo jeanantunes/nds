4492aae2dd6a37a213782ce9076746ebcd035100
leandrolupianhez <leandro.lupianhez@gmail.com>
2013-10-17 15:22:01 -0300

    correção item 97

--- nds-client/src/main/webapp/scripts/baixaFinanceira.js
+++ nds-client/src/main/webapp/scripts/baixaFinanceira.js
@@ -1061,7 +1061,7 @@ var baixaFinanceiraController = $.extend(true, {
 			allowNegative: true,
 		    centsSeparator: ',',
 		    thousandsSeparator: '.',
-			centsLimit: 4
+			centsLimit: 2
 		});
 		
 		$('#multaDividas', baixaFinanceiraController.workspace).priceFormat({
@@ -1089,7 +1089,7 @@ var baixaFinanceiraController = $.extend(true, {
 			allowNegative: true,
 			centsSeparator: ',',
 		    thousandsSeparator: '.',
-			centsLimit: 4
+			centsLimit: 2
 		});
 		
 		$('#valorSaldoDividas', baixaFinanceiraController.workspace).priceFormat({
@@ -1110,7 +1110,7 @@ var baixaFinanceiraController = $.extend(true, {
 			allowNegative: true,
 			centsSeparator: ',',
 		    thousandsSeparator: '.',
-			centsLimit: 4
+			centsLimit: 2
 		});
 		
 		$("#valorSaldoDividas", baixaFinanceiraController.workspace).html($("#valorSaldoDividasHidden", baixaFinanceiraController.workspace).val());
@@ -1139,7 +1139,7 @@ var baixaFinanceiraController = $.extend(true, {
 			allowNegative: true,
 			centsSeparator: ',',
 		    thousandsSeparator: '.',
-			centsLimit: 4
+			centsLimit: 2
 		});
 	},
 
