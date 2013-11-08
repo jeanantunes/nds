efc4d3cb7fccb6faf35a4b2d997844bed7d302a3
Robson Martins <robson.java@gmail.com>
2013-11-08 20:26:20 -0200

    Retirado sugeiras antigas

--- nds-integration-canonical/scripts_carga/Campinas/sqlProducao/22_INSERT_CONTROLE_CONFERENCIA_ENCALHE.sql
+++ nds-integration-canonical/scripts_carga/Campinas/sqlProducao/22_INSERT_CONTROLE_CONFERENCIA_ENCALHE.sql
@@ -9,21 +9,3 @@ where tipo_movimento_id = 31
 group by 1, 2);
 
 
-select * from CONTROLE_CONFERENCIA_ENCALHE
-
-;
-
--- Scrip de limpeza p/ reprocessamento do 22 até o 32
-delete from fechamento_encalhe_box;
-delete from conferencia_encalhe;
-delete from fechamento_encalhe;
-delete from controle_conferencia_encalhe_cota;
-delete from chamada_encalhe_cota;
-delete from chamada_encalhe_lancamento;
-delete from chamada_encalhe;
-delete from controle_conferencia_encalhe;
-
-truncate controle_fechamento_encalhe;
-truncate cobranca_controle_conferencia_encalhe_cota;
-truncate conferencia_enc_parcial;
-truncate conferencia_encalhe;
