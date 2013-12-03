b5bf0003313f2ded34ccc7e016d378ae94632b08
leandrolupianhez <leandro.lupianhez@gmail.com>
2013-12-03 10:22:01 -0200

    Criação de dto para manipulação de dados referente a redistribuição de parcial

---
+++ nds-model/src/main/java/br/com/abril/nds/dto/RedisrtibuicaoParcialDTO.java
@@ -0,0 +1,75 @@
+package br.com.abril.nds.dto;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class RedisrtibuicaoParcialDTO implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private Long idLancamentoRedistribuicao;
+	
+	private Long idPeriodo;
+	
+	private Integer numeroPeriodo;
+	
+	private Integer numeroLancamento;
+	
+	private Date dataLancamento;
+	
+	private Date dataRecolhimento;
+
+	public Long getIdLancamentoRedistribuicao() {
+		return idLancamentoRedistribuicao;
+	}
+
+	public void setIdLancamentoRedistribuicao(Long idLancamentoRedistribuicao) {
+		this.idLancamentoRedistribuicao = idLancamentoRedistribuicao;
+	}
+
+	public Long getIdPeriodo() {
+		return idPeriodo;
+	}
+
+	public void setIdPeriodo(Long idPeriodo) {
+		this.idPeriodo = idPeriodo;
+	}
+
+	public Integer getNumeroPeriodo() {
+		return numeroPeriodo;
+	}
+
+	public void setNumeroPeriodo(Integer numeroPeriodo) {
+		this.numeroPeriodo = numeroPeriodo;
+	}
+
+	public Integer getNumeroLancamento() {
+		return numeroLancamento;
+	}
+
+	public void setNumeroLancamento(Integer numeroLancamento) {
+		this.numeroLancamento = numeroLancamento;
+	}
+
+	public Date getDataLancamento() {
+		return dataLancamento;
+	}
+
+	public void setDataLancamento(Date dataLancamento) {
+		this.dataLancamento = dataLancamento;
+	}
+
+	public Date getDataRecolhimento() {
+		return dataRecolhimento;
+	}
+
+	public void setDataRecolhimento(Date dataRecolhimento) {
+		this.dataRecolhimento = dataRecolhimento;
+	}
+	
+	
+	
+}
