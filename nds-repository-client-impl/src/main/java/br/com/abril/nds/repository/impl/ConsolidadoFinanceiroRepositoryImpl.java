0f4510e078551bc487e1ed4be83d3c0ee18ee27b
Washington Paiva <wrpaiva@gmail.com>
2013-12-05 15:12:20 -0200

    Retirado da query o relacionamento da Cota com o BOX

--- nds-repository-client-impl/src/main/java/br/com/abril/nds/repository/impl/ConsolidadoFinanceiroRepositoryImpl.java
+++ nds-repository-client-impl/src/main/java/br/com/abril/nds/repository/impl/ConsolidadoFinanceiroRepositoryImpl.java
@@ -1146,7 +1146,6 @@ public class ConsolidadoFinanceiroRepositoryImpl extends
 		sql.append(" cfc.ID as id, ")
 		   .append(" cfc.COTA_ID as cotaId, ")
 		   .append(" COTA.NUMERO_COTA as numeroCota, ")
-		   .append(" concat(box.codigo, ' - ', box.nome) as nomeBox, ")
 		   .append(" cfc.CONSIGNADO as consignado, ")
 		   
 		   .append(" cfc.DT_CONSOLIDADO as dataConsolidado, ")
@@ -1224,7 +1223,6 @@ public class ConsolidadoFinanceiroRepositoryImpl extends
 		   
 		   .append(" from CONSOLIDADO_FINANCEIRO_COTA cfc ")
 		   .append(" inner join COTA cota on cota.ID = cfc.COTA_ID")
-		   .append(" inner join BOX box on cota.BOX_ID = box.ID ")
 		   .append(" left join DIVIDA divida on divida.CONSOLIDADO_ID = cfc.ID ")
 		   .append(" left join DIVIDA dividaRaiz on divida.DIVIDA_RAIZ_ID = dividaRaiz.ID ")
 		   .append(" where cota.NUMERO_COTA = :numeroCota ");
@@ -1242,7 +1240,6 @@ public class ConsolidadoFinanceiroRepositoryImpl extends
 		   .append(" null as id, ")
 		   .append(" mfc.COTA_ID as cotaId, ")
 		   .append(" null as numeroCota, ")
-		   .append(" concat(box.codigo, ' - ', box.nome) as nomeBox, ")
 		   
 		   //consignado
 		   .append("coalesce((select sum(m.VALOR) ")
@@ -1565,7 +1562,6 @@ public class ConsolidadoFinanceiroRepositoryImpl extends
 		   
 		   .append(" from MOVIMENTO_FINANCEIRO_COTA mfc ")
 		   .append(" inner join COTA on COTA.ID = mfc.COTA_ID")
-		   .append(" inner join BOX box on cota.BOX_ID = box.ID ")
 		   .append(" where COTA.NUMERO_COTA = :numeroCota ")
 		   .append(" and mfc.ID not in (")
 		   .append("     select MVTO_FINANCEIRO_COTA_ID ")
@@ -1598,7 +1594,6 @@ public class ConsolidadoFinanceiroRepositoryImpl extends
 		query.addScalar("id", StandardBasicTypes.LONG);
 		query.addScalar("cotaId", StandardBasicTypes.LONG);
 		query.addScalar("numeroCota", StandardBasicTypes.INTEGER);
-		query.addScalar("nomeBox", StandardBasicTypes.STRING);
 		query.addScalar("consignado", StandardBasicTypes.BIG_DECIMAL);
 		query.addScalar("dataConsolidado", StandardBasicTypes.DATE);
 		query.addScalar("debitoCredito", StandardBasicTypes.BIG_DECIMAL);
