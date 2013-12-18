3e520d92ff1b1853da031f05270ae5d0a6f3aded
Washington Paiva <wrpaiva@gmail.com>
2013-12-18 16:23:49 -0200

    desenvolvimento nfe

---
+++ nds-nfe/src/main/java/br/com/abril/nds/nfe/model/OrigemItemNotaFiscalEstoque.java
@@ -0,0 +1,16 @@
+package br.com.abril.nds.nfe.model;
+
+import br.com.abril.nds.model.cadastro.ProdutoEdicao;
+
+public class OrigemItemNotaFiscalEstoque {
+	
+	private ProdutoEdicao produtoEdicao;
+
+	public ProdutoEdicao getProdutoEdicao() {
+		return produtoEdicao;
+	}
+
+	public void setProdutoEdicao(ProdutoEdicao produtoEdicao) {
+		this.produtoEdicao = produtoEdicao;
+	}
+}
---
+++ nds-nfe/src/main/java/br/com/abril/nds/nfe/model/TipoMovimentoEstoque.java
@@ -0,0 +1,5 @@
+package br.com.abril.nds.nfe.model;
+
+public class TipoMovimentoEstoque {
+
+}
