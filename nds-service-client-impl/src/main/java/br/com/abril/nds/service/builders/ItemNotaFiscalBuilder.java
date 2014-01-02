e6818e8d55bb015cea42d242ebc80cced2dd2b12
Washington Paiva <wrpaiva@gmail.com>
2014-01-02 11:22:33 -0200

    Criação do Builders das notas

---
+++ nds-service-client-impl/src/main/java/br/com/abril/nds/service/builders/ItemNotaFiscalBuilder.java
@@ -0,0 +1,10 @@
+package br.com.abril.nds.service.builders;
+
+public class ItemNotaFiscalBuilder  {
+	
+	public void montaItemNotaFiscal (){
+		
+		
+	}
+	
+}
---
+++ nds-service-client-impl/src/main/java/br/com/abril/nds/service/builders/NotaFiscalBuilder.java
@@ -0,0 +1,12 @@
+package br.com.abril.nds.service.builders;
+
+public class NotaFiscalBuilder {
+	
+	public void montaNotaFiscal (){
+		
+		// obter as notas fiscais pelas cotas que estão na tela
+		
+		// builder Header Nota fiscal
+	}
+	
+}
