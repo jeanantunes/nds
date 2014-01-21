b9e91e424fc8bc5903cdbafa50d39420cb50a4ed
Diogo Vecchiati <diogo.vecchiati@gmail.com>
2014-01-21 15:45:27 -0200

    commit to merge

---
+++ nds-integration-canonical/src/main/java/br/com/abril/nds/integracao/model/canonic/EMS0140Input.java
@@ -0,0 +1,9 @@
+package br.com.abril.nds.integracao.model.canonic;
+
+import java.io.Serializable;
+
+public class EMS0140Input extends IntegracaoDocumentDetail implements Serializable {
+
+	private static final long serialVersionUID = -1611339433305392806L;
+
+}
