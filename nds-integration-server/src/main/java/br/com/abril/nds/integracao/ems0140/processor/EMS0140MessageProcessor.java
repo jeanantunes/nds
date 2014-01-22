0fa0dd3278fce5999001decc1b5927aab29d0319
Diogo Vecchiati <diogo.vecchiati@gmail.com>
2014-01-22 16:46:27 -0200

    fix tests

--- nds-integration-server/src/main/java/br/com/abril/nds/integracao/ems0140/processor/EMS0140MessageProcessor.java
+++ nds-integration-server/src/main/java/br/com/abril/nds/integracao/ems0140/processor/EMS0140MessageProcessor.java
@@ -2,11 +2,14 @@ package br.com.abril.nds.integracao.ems0140.processor;
 
 import java.util.concurrent.atomic.AtomicReference;
 
+import org.springframework.stereotype.Component;
+
 import br.com.abril.nds.integracao.model.canonic.EMS0140Input;
 import br.com.abril.nds.model.integracao.Message;
 import br.com.abril.nds.model.integracao.MessageProcessor;
 import br.com.abril.nds.repository.AbstractRepository;
 
+@Component
 public class EMS0140MessageProcessor extends AbstractRepository implements MessageProcessor {
 
 	@Override
@@ -16,7 +19,7 @@ public class EMS0140MessageProcessor extends AbstractRepository implements Messa
 
 	@Override
 	public void processMessage(Message message) {
-		EMS0140Input input = null;
+		EMS0140Input input = new EMS0140Input();
 		input.get_id();
 	}
 
