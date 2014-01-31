120d3fd6e58e42dbd7623eec4dc0e144253a174c
Sérgio Peres <sergiopersil@gmail.com>
2014-01-31 20:17:42 -0200

    Ajustes na modelagem de notas e parametros do distribuidor

---
+++ nds-service-client-impl/src/main/java/br/com/abril/nds/service/impl/RegimeTributarioServiceImpl.java
@@ -0,0 +1,26 @@
+package br.com.abril.nds.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.abril.nds.model.fiscal.notafiscal.RegimeTributario;
+import br.com.abril.nds.repository.RegimeTributarioRepository;
+import br.com.abril.nds.service.integracao.RegimeTributarioService;
+
+@Service
+public class RegimeTributarioServiceImpl implements RegimeTributarioService {
+
+	@Autowired
+	private RegimeTributarioRepository regimeTributarioRepository;
+	
+	@Override
+	@Transactional
+	public List<RegimeTributario> obterRegimesTributarios() {
+		
+		return regimeTributarioRepository.buscarTodos();
+	}
+
+}
