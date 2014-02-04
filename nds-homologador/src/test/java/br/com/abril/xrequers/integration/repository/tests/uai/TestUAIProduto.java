b42b5edee4f4e91befa3dfc1e472ef315cb4e9db
michel.jader <fiateternan@gmail.com>
2014-02-04 16:33:58 -0200

    Correção Trac 103 Tipo Desconto Cota
    Corrigido método de exclusão de desconto de produtos

--- nds-homologador/src/test/java/br/com/abril/xrequers/integration/repository/tests/uai/TestUAIProduto.java
+++ nds-homologador/src/test/java/br/com/abril/xrequers/integration/repository/tests/uai/TestUAIProduto.java
@@ -1,6 +1,8 @@
 package br.com.abril.xrequers.integration.repository.tests.uai;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -12,6 +14,42 @@ import br.com.abril.nds.model.cadastro.TipoProduto;
 import br.com.abril.xrequers.integration.repository.tests.ProdutoRepositoryTest;
 
 public class TestUAIProduto extends ProdutoRepositoryTest {
+	
+	
+	@Test
+	public void test_update_by_id_produto_campo_null() {
+		
+		Map<String, String> campos = new HashMap<>();
+		
+		campos.put("nome", "primeiro");
+		campos.put("nomeComercial", null);
+		
+		produtoRepository.alterarPorId(100L, campos);
+
+		Produto produto = this.produtoRepository.buscarPorId(100L);
+		
+		Assert.assertTrue("primeiro".equals(produto.getNome()));
+		Assert.assertNull(produto.getNomeComercial());
+
+		
+	}
+	
+	@Test
+	public void test_update_by_id_produto_() {
+		
+		Map<String, String> campos = new HashMap<>();
+		
+		campos.put("nome", "primeiro");
+		campos.put("nomeComercial", "segundo");
+		
+		produtoRepository.alterarPorId(100L, campos);
+
+		Produto produto = this.produtoRepository.buscarPorId(100L);
+		
+		Assert.assertTrue("primeiro".equals(produto.getNome()));
+		Assert.assertTrue("segundo".equals(produto.getNomeComercial()));
+		
+	}
 	
 	@Test
 	public void testInsertProduto() {
@@ -34,4 +72,11 @@ public class TestUAIProduto extends ProdutoRepositoryTest {
 		
 		Assert.assertNotNull(produto);
 	}
+	
+	
+	
+	
+	
+	
+	
 }
--- nds-repository-client-impl/src/main/java/br/com/abril/nds/repository/impl/HistoricoDescontoCotaProdutoRepositoryImpl.java
+++ nds-repository-client-impl/src/main/java/br/com/abril/nds/repository/impl/HistoricoDescontoCotaProdutoRepositoryImpl.java
@@ -3,14 +3,9 @@ package br.com.abril.nds.repository.impl;
 import java.util.List;
 
 import org.hibernate.Query;
-import org.hibernate.transform.Transformers;
 import org.springframework.stereotype.Repository;
 
-import br.com.abril.nds.dto.TipoDescontoDTO;
-import br.com.abril.nds.dto.filtro.FiltroTipoDescontoDTO;
-import br.com.abril.nds.model.cadastro.Cota;
-import br.com.abril.nds.model.cadastro.Fornecedor;
-import br.com.abril.nds.model.cadastro.desconto.Desconto;
+import br.com.abril.nds.dto.filtro.FiltroConsultaHistoricoDescontoDTO;
 import br.com.abril.nds.model.cadastro.desconto.HistoricoDescontoCotaProdutoExcessao;
 import br.com.abril.nds.repository.AbstractRepositoryModel;
 import br.com.abril.nds.repository.HistoricoDescontoCotaProdutoRepository;
@@ -28,98 +23,68 @@ public class HistoricoDescontoCotaProdutoRepositoryImpl extends AbstractReposito
 		super(HistoricoDescontoCotaProdutoExcessao.class);
 	}
 	
-	@SuppressWarnings("unchecked")
 	@Override
-	public List<TipoDescontoDTO> buscarDescontos(FiltroTipoDescontoDTO filtro) {
-		
-		StringBuilder hql = new StringBuilder();
-		
-		hql.append("select 0L as sequencial ");
-		hql.append(", 1L as idTipoDesconto ");
-		hql.append(", 'Anonimo' as usuario ");
-		hql.append(", f.desconto as desconto ");
-		hql.append(", f.juridica.razaoSocial as fornecedor ");
-		hql.append(", 'Geral' as descTipoDesconto ");
-		hql.append("from Fornecedor f ");
-		
-		if(filtro.getIdFornecedores()!=null && !filtro.getIdFornecedores().isEmpty()) {
-			hql.append(" where f.id in (:idFornecedores) ");
-		}
-		
-		hql.append(getOrdenacao(filtro));
-		
-		Query query  = getSession().createQuery(hql.toString());
-		
-		//ResultTransformer resultTransformer = new AliasToBeanResultTransformer(TipoDescontoDTO.class);
-
-		query.setResultTransformer(Transformers.aliasToBean(TipoDescontoDTO.class));
-		
-		if(filtro.getIdFornecedores()!=null && !filtro.getIdFornecedores().isEmpty()) {
-			query.setParameterList("idFornecedores", filtro.getIdFornecedores());
-		}
-		
-		if(filtro.getPaginacao()!= null && filtro.getPaginacao().getPosicaoInicial() != null) 
-			query.setFirstResult(filtro.getPaginacao().getPosicaoInicial());
-		
-		if(filtro.getPaginacao()!= null && filtro.getPaginacao().getQtdResultadosPorPagina() != null) 
-			query.setMaxResults(filtro.getPaginacao().getQtdResultadosPorPagina());
-		
-		return query.list();
-		
-		//hql.append(" group by desconto.id ");
-		
-		/*hql.append(" select desconto.id as sequencial, ")
-			.append(" desconto.id as idTipoDesconto ,")
-			.append(" desconto.usuario.nome as usuario ,")
-			.append(" desconto.desconto as desconto ,")
-			.append(" desconto.dataAlteracao as dataAlteracao , ")
-			.append("(case ")
-				.append("when (select count(fornecedor.id) from DescontoDistribuidor descontoFor JOIN descontoFor.fornecedores fornecedor  ")
-					.append(" where descontoFor.id = desconto.id ) > 1 ")
-				.append("then 'Diversos' ")
-				.append("when (select count(fornecedor.id) from DescontoDistribuidor descontoFor JOIN descontoFor.fornecedores fornecedor  ")
-					.append("where descontoFor.id = desconto.id ) = 1 then pessoa.razaoSocial ")
-			.append("else null end) as fornecedor, 	")
-		    .append(" 'Geral' as descTipoDesconto 	");
-		
-		hql.append(" from DescontoDistribuidor desconto		")
-			.append(" JOIN desconto.fornecedores fornecedor ")
-			.append(" JOIN fornecedor.juridica pessoa 		");
-			
-		
-		if(filtro.getIdFornecedores()!=null && !filtro.getIdFornecedores().isEmpty()) {
-			hql.append(" where fornecedor.id in (:idFornecedores) ");
-		}
-		
-		hql.append(" group by desconto.id ");*/
-		
-		//hql.append(getOrdenacao(filtro));
-		
-	}
-	
-
-	@Override
-	public Integer buscarQuantidadeDescontos(FiltroTipoDescontoDTO filtro) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public HistoricoDescontoCotaProdutoExcessao buscarUltimoDescontoValido(Desconto desconto, Cota cota, Fornecedor fornecedor) {
+	public HistoricoDescontoCotaProdutoExcessao buscarUltimoDescontoValido(FiltroConsultaHistoricoDescontoDTO filtro) {
 
 		StringBuilder hql = new StringBuilder();
 		
-		hql.append(" select hdcpe ")
-			.append("from HistoricoDescontoCotaProdutoExcessao hdcpe ")
-			.append("where hdcpe.desconto.id = :idDesconto ")
-			.append("and hdcpe.cota.id = :idCota ")
-			.append("and hdcpe.fornecedor.id = :idFornecedor ");
+		hql.append(" select hdcpe ");
+		hql.append(" from HistoricoDescontoCotaProdutoExcessao hdcpe ");
+		
+		boolean indWhere = false;
+		
+		if(filtro.getIdCota() != null) {
+			hql.append( " where hdcpe.cota.id = :idCota " );
+			indWhere = true;
+		}
+		
+		if(filtro.getIdDesconto() != null) {
+			hql.append( indWhere ? " and " : " where " );
+			hql.append(" hdcpe.desconto.id = :idDesconto ");
+			indWhere = true;
+		}
+		
+		if(filtro.getIdFornecedor() != null) {
+			hql.append( indWhere ? " and " : " where " );
+			hql.append(" hdcpe.fornecedor.id = :idFornecedor ");
+			indWhere = true;
+		}
+		
+		if(filtro.getIdProduto() != null) {
+			hql.append( indWhere ? " and " : " where " );
+			hql.append(" hdcpe.produto.id = :idProduto ");
+			indWhere = true;
+		}
+		
+		if(filtro.getIdProdutoEdicao() != null) {
+			hql.append( indWhere ? " and " : " where " );
+			hql.append(" hdcpe.produtoEdicao.id = :idProdutoEdicao ");
+			indWhere = true;
+		}
 	
+		hql.append(" order by hdcpe.dataAlteracao desc ");
+		
 		Query query = getSession().createQuery(hql.toString());
 		
-		query.setParameter("idDesconto", desconto.getId());
-		query.setParameter("idCota", cota.getId());
-		query.setParameter("idFornecedor", fornecedor.getId());
+		if(filtro.getIdCota() != null) {
+			query.setParameter("idCota", filtro.getIdCota());
+		}
+		
+		if(filtro.getIdDesconto() != null) {
+			query.setParameter("idDesconto", filtro.getIdDesconto());
+		}
+		
+		if(filtro.getIdFornecedor() != null) {
+			query.setParameter("idFornecedor", filtro.getIdFornecedor());
+		}
+		
+		if(filtro.getIdProduto() != null) {
+			query.setParameter("idProduto", filtro.getIdProduto());
+		}
+		
+		if(filtro.getIdProdutoEdicao() != null) {
+			query.setParameter("idProdutoEdicao", filtro.getIdProdutoEdicao());
+		}
 		
 		query.setMaxResults(1);
 		
@@ -127,49 +92,72 @@ public class HistoricoDescontoCotaProdutoRepositoryImpl extends AbstractReposito
 		
 	}
 	
-	private String getOrdenacao(FiltroTipoDescontoDTO filtro){
-		
-		if (filtro == null || filtro.getOrdenacaoColuna() == null) {
-			return "";
-		}
+	@Override
+	public List<HistoricoDescontoCotaProdutoExcessao> buscarListaHistoricoDescontoCotaProdutoExcessao(FiltroConsultaHistoricoDescontoDTO filtro) {
 
 		StringBuilder hql = new StringBuilder();
-
-		switch (filtro.getOrdenacaoColuna()) {
-			
-			case DATA_ALTERACAO:
-				hql.append(" order by dataAlteracao ");
-				break;
-				
-			case DESCONTO:
-				hql.append(" order by desconto  ");
-				break;
-					
-			case SEQUENCIAL:
-				hql.append(" order by sequencial  ");
-				break;
-			
-			case USUARIO:
-				hql.append(" order by usuario ");
-				break;
-				
-			case FORNECEDORES:
-				hql.append(" order by fornecedor ");
-				break;	
-				
-			case TIPO_DESCONTO:
-				hql.append(" order by descTipoDesconto ");
-				break;		
-				
-			default:
-				hql.append(" order by sequencial ");
+		
+		hql.append(" select hdcpe ");
+		hql.append(" from HistoricoDescontoCotaProdutoExcessao hdcpe ");
+		
+		boolean indWhere = false;
+		
+		if(filtro.getIdCota() != null) {
+			hql.append( " where hdcpe.cota.id = :idCota " );
+			indWhere = true;
 		}
-
-		if (filtro.getPaginacao()!= null && filtro.getPaginacao().getOrdenacao() != null) {
-			hql.append(filtro.getPaginacao().getOrdenacao().toString());
+		
+		if(filtro.getIdDesconto() != null) {
+			hql.append( indWhere ? " and " : " where " );
+			hql.append(" hdcpe.desconto.id = :idDesconto ");
+			indWhere = true;
 		}
-
-		return hql.toString();
-	}
+		
+		if(filtro.getIdFornecedor() != null) {
+			hql.append( indWhere ? " and " : " where " );
+			hql.append(" hdcpe.fornecedor.id = :idFornecedor ");
+			indWhere = true;
+		}
+		
+		if(filtro.getIdProduto() != null) {
+			hql.append( indWhere ? " and " : " where " );
+			hql.append(" hdcpe.produto.id = :idProduto ");
+			indWhere = true;
+		}
+		
+		if(filtro.getIdProdutoEdicao() != null) {
+			hql.append( indWhere ? " and " : " where " );
+			hql.append(" hdcpe.produtoEdicao.id = :idProdutoEdicao ");
+			indWhere = true;
+		}
+	
+		hql.append(" group by hdcpe.id order by hdcpe.dataAlteracao desc ");
+		
+		Query query = getSession().createQuery(hql.toString());
+		
+		if(filtro.getIdCota() != null) {
+			query.setParameter("idCota", filtro.getIdCota());
+		}
+		
+		if(filtro.getIdDesconto() != null) {
+			query.setParameter("idDesconto", filtro.getIdDesconto());
+		}
+		
+		if(filtro.getIdFornecedor() != null) {
+			query.setParameter("idFornecedor", filtro.getIdFornecedor());
+		}
+		
+		if(filtro.getIdProduto() != null) {
+			query.setParameter("idProduto", filtro.getIdProduto());
+		}
+		
+		if(filtro.getIdProdutoEdicao() != null) {
+			query.setParameter("idProdutoEdicao", filtro.getIdProdutoEdicao());
+		}
+		
+		return query.list();
+		
+	}	
+	
 	
 }
