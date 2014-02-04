4b993bb6517a79e765bcb6c76a5766f2f1972200
Washington Paiva <wrpaiva@gmail.com>
2014-02-04 10:52:42 -0200

    FIX - MERGE

--- nds-model/src/main/java/br/com/abril/nds/model/planejamento/Estudo.java
+++ nds-model/src/main/java/br/com/abril/nds/model/planejamento/Estudo.java
@@ -1,7 +1,9 @@
 package br.com.abril.nds.model.planejamento;
 
+import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Entity
 @Table(name = "ESTUDO")
 @SuppressWarnings("serial")
 public class Estudo extends AbstractEstudo {
