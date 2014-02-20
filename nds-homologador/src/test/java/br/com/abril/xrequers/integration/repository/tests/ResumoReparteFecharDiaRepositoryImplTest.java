2d61e27efe9d210b030d9b4202705be0e8208d8f
michel.jader <fiateternan@gmail.com>
2014-02-20 10:13:34 -0300

    Testes
    Testes

---
+++ nds-homologador/src/test/java/br/com/abril/xrequers/integration/repository/tests/ResumoReparteFecharDiaRepositoryImplTest.java
@@ -0,0 +1,38 @@
+package br.com.abril.xrequers.integration.repository.tests;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.junit.Test;
+
+import br.com.abril.nds.dto.fechamentodiario.SumarizacaoReparteDTO;
+import br.com.abril.nds.repository.ResumoReparteFecharDiaRepository;
+
+public class ResumoReparteFecharDiaRepositoryImplTest extends AbstractRepositoryTest  {
+
+	private ResumoReparteFecharDiaRepository resumoReparteFecharDiaRepository;
+	
+	@Test
+	public void test_obter_sumarizacao_reparte(){
+		
+		Calendar c = Calendar.getInstance();
+		
+		c.clear();
+		
+		c.set(2014, Calendar.FEBRUARY, 19);
+		Date data =  c.getTime();
+		
+		c.set(2014, Calendar.FEBRUARY, 18);
+		Date dataReparteHistoico = c.getTime();
+		
+		SumarizacaoReparteDTO sumarizacaoReparte = resumoReparteFecharDiaRepository.obterSumarizacaoReparte(data, dataReparteHistoico);
+		
+		assertNotNull(sumarizacaoReparte);
+		
+	}
+	
+}
+
