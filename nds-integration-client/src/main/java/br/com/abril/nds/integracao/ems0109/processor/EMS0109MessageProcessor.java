0c4089f33649b4b7eb40f03ddee583a03eb31abd
douglashg <douglashg@hotmail.com>
2014-03-20 11:27:20 -0300

    remover o if inserido na 109

--- nds-integration-client/src/main/java/br/com/abril/nds/integracao/ems0109/processor/EMS0109MessageProcessor.java
+++ nds-integration-client/src/main/java/br/com/abril/nds/integracao/ems0109/processor/EMS0109MessageProcessor.java
@@ -524,15 +524,7 @@ public class EMS0109MessageProcessor extends AbstractRepository implements
 						EventoExecucaoEnum.INF_DADO_ALTERADO,
 						"Atualizacao da Forma de Comercializacao para: " + formaComercializacaoInput.getValue());
 		}
-		/*
-		if(produto.getTipoProduto()==null ){
-			
-			this.ndsiLoggerFactory.getLogger().logInfo(message,
-					EventoExecucaoEnum.INF_DADO_ALTERADO,
-					"Erro na Inserção do Produto: " + produto.getCodigo()
-					+". Tipo Produto nulo.");
-		}
-		*/
+
 		this.getSession().update(produto);
 	}
 
