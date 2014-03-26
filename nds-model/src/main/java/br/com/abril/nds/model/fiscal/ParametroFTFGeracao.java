3f7ae04a2509691ae9566bb09c3c4aac50e7e18d
Washington Paiva <wrpaiva@gmail.com>
2014-03-26 16:24:00 -0300

    Ajuste no modelo de FTF

---
+++ nds-model/src/main/java/br/com/abril/nds/model/fiscal/ParametroFTFGeracao.java
@@ -0,0 +1,162 @@
+package br.com.abril.nds.model.fiscal;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import br.com.abril.nds.model.cadastro.TipoAtividade;
+
+@Entity
+@Table(name="PARAMETROS_FTF_GERACAO")
+public class ParametroFTFGeracao implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -235863705621160503L;
+
+	@Id
+	@GeneratedValue
+	@Column(name="ID")
+	private long id;
+	
+	@Column(name="NOME")
+	private String nome;
+	
+	@Column(name="CENTRO+EMISSOR")
+	private String centroEmissor;
+	
+	@Column(name="CNPJ_EMISSOR")
+	private String cnpjEmissor;
+	
+	@Column(name="CNPJ_DESTINATARIO")
+	private String cnpjDestinatario; 
+	
+	@Column(name="ESTABELECIMENTO")
+	private String estabelecimento; 
+	
+	@Column(name="TIPO_PEDIDO")
+	private String tipoPedido; 
+	
+	@Column(name="CODIGO_SOLICITANTE")
+	private String codigoSolicitante; 
+	
+	@Column(name="MENSAGEM")
+	private String mensagem; 
+	
+	@OneToOne
+	@JoinColumn(name="NATUREZA_OPERACAO_ID", updatable=true, insertable=true)
+	private NaturezaOperacao naturezaOperacao; 
+	
+	@OneToOne
+	@JoinColumn(name="CFOP", updatable=true, insertable=true)
+	private CFOP cfop; 
+	
+	@Enumerated(EnumType.STRING)
+	@Column(name = "TIPO_ATIVIDADE", nullable = false)
+	private TipoAtividade tipoAtividade;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCentroEmissor() {
+		return centroEmissor;
+	}
+
+	public void setCentroEmissor(String centroEmissor) {
+		this.centroEmissor = centroEmissor;
+	}
+
+	public String getCnpjEmissor() {
+		return cnpjEmissor;
+	}
+
+	public void setCnpjEmissor(String cnpjEmissor) {
+		this.cnpjEmissor = cnpjEmissor;
+	}
+
+	public String getCnpjDestinatario() {
+		return cnpjDestinatario;
+	}
+
+	public void setCnpjDestinatario(String cnpjDestinatario) {
+		this.cnpjDestinatario = cnpjDestinatario;
+	}
+
+	public String getEstabelecimento() {
+		return estabelecimento;
+	}
+
+	public void setEstabelecimento(String estabelecimento) {
+		this.estabelecimento = estabelecimento;
+	}
+
+	public String getTipoPedido() {
+		return tipoPedido;
+	}
+
+	public void setTipoPedido(String tipoPedido) {
+		this.tipoPedido = tipoPedido;
+	}
+
+	public String getCodigoSolicitante() {
+		return codigoSolicitante;
+	}
+
+	public void setCodigoSolicitante(String codigoSolicitante) {
+		this.codigoSolicitante = codigoSolicitante;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+
+	public NaturezaOperacao getNaturezaOperacao() {
+		return naturezaOperacao;
+	}
+
+	public void setNaturezaOperacao(NaturezaOperacao naturezaOperacao) {
+		this.naturezaOperacao = naturezaOperacao;
+	}
+
+	public CFOP getCfop() {
+		return cfop;
+	}
+
+	public void setCfop(CFOP cfop) {
+		this.cfop = cfop;
+	}
+
+	public TipoAtividade getTipoAtividade() {
+		return tipoAtividade;
+	}
+
+	public void setTipoAtividade(TipoAtividade tipoAtividade) {
+		this.tipoAtividade = tipoAtividade;
+	}
+}
