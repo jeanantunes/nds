b80cdb263cafc9345bc05c54c08ed45bbf3b96a3
douglashg <douglashg@hotmail.com>
2014-05-13 15:55:02 -0300

    track interfaces 168

--- nds-integration-client/src/main/java/br/com/abril/nds/integracao/ems0111/processor/EMS0111MessageProcessor.java
+++ nds-integration-client/src/main/java/br/com/abril/nds/integracao/ems0111/processor/EMS0111MessageProcessor.java
@@ -273,7 +273,7 @@ public class EMS0111MessageProcessor extends AbstractRepository implements
 			// Remover a hora, minuto, segundo e milissegundo para comparação:
 			final Date dtLancamentoAtual = this.normalizarDataSemHora(
 					lancamento.getDataLancamentoPrevista());
-			Date dtLancamentoNovo = this.normalizarDataSemHora(dataLancamento);
+			Date dtLancamentoNovo = lancamentoService.obterDataLancamentoValido(this.normalizarDataSemHora(dataLancamento),new Long(produtoEdicao.getProduto().getFornecedor().getId()));
 			if (null != dtLancamentoAtual && !dtLancamentoAtual.equals(dtLancamentoNovo)) {
 				this.ndsiLoggerFactory.getLogger().logInfo(message,
 						EventoExecucaoEnum.INF_DADO_ALTERADO,
