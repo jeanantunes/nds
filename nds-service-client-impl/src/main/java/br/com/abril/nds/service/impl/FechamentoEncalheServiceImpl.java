5a3eaa61b599f9c61ef043d06a616c3611f17a78
murilolocatelli <murilolocatelli@gmail.com>
2014-05-16 13:59:50 -0300

    trac 400

--- nds-service-client-impl/src/main/java/br/com/abril/nds/service/impl/FechamentoEncalheServiceImpl.java
+++ nds-service-client-impl/src/main/java/br/com/abril/nds/service/impl/FechamentoEncalheServiceImpl.java
@@ -500,7 +500,7 @@ public class FechamentoEncalheServiceImpl implements FechamentoEncalheService {
     	
     	if(encalhe.getFechado()) {
     		
-    		if("P".equals(encalhe.getTipo())) {
+    		if(isEstoqueLancamento(encalhe)) {
     			
                 encalhe.setEstoque(TipoEstoque.LANCAMENTO.getDescricao());
                 
@@ -1188,7 +1188,7 @@ public class FechamentoEncalheServiceImpl implements FechamentoEncalheService {
                 
                 gerarMovimentoFaltasSobras(item, usuario);
                 
-                if (item.isParcial()) {
+                if (item.getRecolhimento() != null && TipoLancamentoParcial.PARCIAL.name().equals(item.getRecolhimento())) {
                 	
                 	this.tratarEncalheProdutoEdicaoParcial(item, usuario, item.getFisico());
                 	
