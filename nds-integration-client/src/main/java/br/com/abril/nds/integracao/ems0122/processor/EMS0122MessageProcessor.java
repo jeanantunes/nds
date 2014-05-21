cdae5f50a173d1dd601bde4851c91f840da15c30
douglashg <douglashg@hotmail.com>
2014-05-21 13:25:23 -0300

    track interfaces 203

--- nds-integration-client/src/main/java/br/com/abril/nds/integracao/ems0122/processor/EMS0122MessageProcessor.java
+++ nds-integration-client/src/main/java/br/com/abril/nds/integracao/ems0122/processor/EMS0122MessageProcessor.java
@@ -1,24 +1,24 @@
 package br.com.abril.nds.integracao.ems0122.processor;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Date;
-import java.util.List;
+//import java.io.FileWriter;
+//import java.io.IOException;
+//import java.io.PrintWriter;
+//import java.util.Date;
+//import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
-import org.hibernate.Query;
+//import org.hibernate.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import br.com.abril.nds.enums.integracao.MessageHeaderProperties;
-import br.com.abril.nds.integracao.ems0122.outbound.EMS0122Detalhe;
-import br.com.abril.nds.integracao.ems0122.outbound.EMS0122Header;
-import br.com.abril.nds.integracao.ems0122.outbound.EMS0122Trailer;
+//import br.com.abril.nds.enums.integracao.MessageHeaderProperties;
+//import br.com.abril.nds.integracao.ems0122.outbound.EMS0122Detalhe;
+//import br.com.abril.nds.integracao.ems0122.outbound.EMS0122Header;
+//import br.com.abril.nds.integracao.ems0122.outbound.EMS0122Trailer;
 import br.com.abril.nds.integracao.engine.MessageProcessor;
 import br.com.abril.nds.integracao.engine.log.NdsiLoggerFactory;import br.com.abril.nds.model.estoque.GrupoMovimentoEstoque;
-import br.com.abril.nds.model.estoque.MovimentoEstoqueCota;
-import br.com.abril.nds.model.integracao.EventoExecucaoEnum;
+//import br.com.abril.nds.model.estoque.MovimentoEstoqueCota;
+//import br.com.abril.nds.model.integracao.EventoExecucaoEnum;
 import br.com.abril.nds.model.integracao.Message;
 import br.com.abril.nds.repository.AbstractRepository;
 import br.com.abril.nds.service.integracao.DistribuidorService;
@@ -27,18 +27,17 @@ import com.ancientprogramming.fixedformat4j.format.FixedFormatManager;
 
 
 @Component
-
 public class EMS0122MessageProcessor extends AbstractRepository implements MessageProcessor {
 
 	
-	@Autowired
-	private FixedFormatManager fixedFormatManager;
+	//Autowired
+	//private FixedFormatManager fixedFormatManager;
 	
 	@Autowired
 	private NdsiLoggerFactory ndsiLoggerFactory;
 	
-	@Autowired
-	private DistribuidorService distribuidorService;
+	//Autowired
+	//private DistribuidorService distribuidorService;
 	
 	public EMS0122MessageProcessor() {
 
@@ -51,6 +50,8 @@ public class EMS0122MessageProcessor extends AbstractRepository implements Messa
 
 	@Override
 	public void processMessage(Message message) {
+		
+		/*
 	
 		StringBuilder sql = new  StringBuilder();
 		sql.append("SELECT mec ");
@@ -112,7 +113,7 @@ public class EMS0122MessageProcessor extends AbstractRepository implements Messa
 			ndsiLoggerFactory.getLogger().logError(message, EventoExecucaoEnum.GERACAO_DE_ARQUIVO, "Não foi possível gerar o arquivo");
 		}
 				
-		
+		*/
 		
 	}
 
