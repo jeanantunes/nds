25fe0182c4ca7e90358c497e387c2014fdeee422
william-balan <william.balan.machado@gmail.com>
2014-06-24 16:15:41 -0300

    [Regressiva] 30 - Boleto em branco.

--- nds-repository-client/src/main/java/br/com/abril/nds/repository/CotaRepository.java
+++ nds-repository-client/src/main/java/br/com/abril/nds/repository/CotaRepository.java
@@ -28,6 +28,7 @@ import br.com.abril.nds.dto.filtro.FiltroMapaAbastecimentoDTO;
 import br.com.abril.nds.model.cadastro.Cota;
 import br.com.abril.nds.model.cadastro.EnderecoCota;
 import br.com.abril.nds.model.cadastro.Fornecedor;
+import br.com.abril.nds.model.cadastro.GrupoCota;
 import br.com.abril.nds.model.cadastro.ProdutoEdicao;
 import br.com.abril.nds.model.cadastro.SituacaoCadastro;
 import br.com.abril.nds.model.cadastro.TelefoneCota;
@@ -319,6 +320,7 @@ public interface CotaRepository extends Repository<Cota, Long> {
     
     Cota buscarCotaPorID(Long id);
     
+    GrupoCota obterOperacaoVigenteCota(Long idCota, Date dataInicio, Date dataFim);
 	    /**
      * 
      * Obtém todas as cotas que possuem a média de reparte dentro do range
