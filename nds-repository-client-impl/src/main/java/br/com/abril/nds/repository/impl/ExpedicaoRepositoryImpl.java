bdff988e3801d94ea7d282c205d42fc958d6cbdf
foxlol <foxlol@gmail.com>
2014-06-25 09:25:16 -0300

    MNDS-433

--- nds-repository-client-impl/src/main/java/br/com/abril/nds/repository/impl/ExpedicaoRepositoryImpl.java
+++ nds-repository-client-impl/src/main/java/br/com/abril/nds/repository/impl/ExpedicaoRepositoryImpl.java
@@ -580,10 +580,10 @@ public class ExpedicaoRepositoryImpl extends AbstractRepositoryModel<Expedicao,L
 		sql.append("( SELECT ");
 		
 		if(calcularValorFaturado){
-			sql.append(" COALESCE(sum(case when (tp_movimento.GRUPO_MOVIMENTO_ESTOQUE = 'SUPLEMENTAR_COTA_AUSENTE') then (mec_st.QTDE ) else (mec_st.QTDE *-1) end) * precoCapa,0)");
+			sql.append(" COALESCE(sum(case when (tp_movimento.GRUPO_MOVIMENTO_ESTOQUE = 'SUPLEMENTAR_COTA_AUSENTE') then (mec_st.QTDE *-1) else (mec_st.QTDE) end) * precoCapa,0)");
 		}
 		else{
-			sql.append(" COALESCE(sum(case when (tp_movimento.GRUPO_MOVIMENTO_ESTOQUE = 'SUPLEMENTAR_COTA_AUSENTE') then (mec_st.QTDE ) else (mec_st.QTDE *-1) end),0)");
+			sql.append(" COALESCE(sum(case when (tp_movimento.GRUPO_MOVIMENTO_ESTOQUE = 'SUPLEMENTAR_COTA_AUSENTE') then (mec_st.QTDE *-1) else (mec_st.QTDE) end),0)");
 		}
 		
 		sql.append("  FROM ");
