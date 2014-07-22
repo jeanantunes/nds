005f10122ee1fa1f284def53ad7b75e33ff4137d
Washington Paiva <wrpaiva@gmail.com>
2014-07-22 15:13:58 -0300

    Comentando o throw nao gravar

--- nds-service-client-impl/src/main/java/br/com/abril/nds/service/impl/NFeServiceImpl.java
+++ nds-service-client-impl/src/main/java/br/com/abril/nds/service/impl/NFeServiceImpl.java
@@ -490,7 +490,7 @@ public class NFeServiceImpl implements NFeService {
 		} else {
 			
 			this.notaFiscalService.exportarNotasFiscais(notas);
-			throw new ValidacaoException(TipoMensagem.ERROR, "Não gravar!!!!");
+//			throw new ValidacaoException(TipoMensagem.ERROR, "Não gravar!!!!");
 			
 		}
 		
