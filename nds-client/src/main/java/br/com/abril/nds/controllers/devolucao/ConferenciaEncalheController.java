3df15906b1a10458eeba94eba0b3cbc05cc59845
Washington Paiva <wrpaiva@gmail.com>
2014-08-14 12:26:47 -0300

    Ajuste para retirar a mensagem de OK que apresenta ao usuario

--- nds-client/src/main/java/br/com/abril/nds/controllers/devolucao/ConferenciaEncalheController.java
+++ nds-client/src/main/java/br/com/abril/nds/controllers/devolucao/ConferenciaEncalheController.java
@@ -1539,7 +1539,7 @@ public class ConferenciaEncalheController extends BaseController {
     
         if(!isVendaNegativaProduto){
         	
-            this.result.use(Results.json()).from("OK", "result").serialize();
+            this.result.use(Results.json()).from("", "result").serialize();
         }
 	}
 
