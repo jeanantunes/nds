ff43b819ba8e089aec0b0b97aa91d82c62180d19
william-balan <william.balan.machado@gmail.com>
2014-08-22 10:53:39 -0300

    Remoção de código morto.

--- nds-integration-client/src/main/java/br/com/abril/nds/integracao/ems0128/processor/EMS0128MessageProcessor.java
+++ nds-integration-client/src/main/java/br/com/abril/nds/integracao/ems0128/processor/EMS0128MessageProcessor.java
@@ -71,8 +71,7 @@ public class EMS0128MessageProcessor extends AbstractRepository implements Messa
 		
 		view.startKey(new Object[] {"EMS0128"});
 		view.endKey("EMS0128", "");
-						
-//		view.key("EMS0128");
+
 		view.includeDocs(true);
 		try {
 			ViewResult<String, Void, ?> result = view.queryView(String.class, Void.class, EMS0128Input.class);
