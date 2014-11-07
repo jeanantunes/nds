a7fa46999482b941bc95cbb2d3c7ce35ee05623c
Washington Paiva <wrpaiva@gmail.com>
2014-11-07 17:15:45 -0200

    Ajuste relatorio mapa de abastecimento cota por box

--- nds-repository-client/src/main/java/br/com/abril/nds/repository/MovimentoEstoqueCotaRepository.java
+++ nds-repository-client/src/main/java/br/com/abril/nds/repository/MovimentoEstoqueCotaRepository.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import br.com.abril.nds.dto.AbastecimentoBoxCotaDTO;
 import br.com.abril.nds.dto.AbastecimentoDTO;
 import br.com.abril.nds.dto.ConsultaEncalheDTO;
 import br.com.abril.nds.dto.ConsultaEncalheDetalheDTO;
@@ -524,5 +525,5 @@ public interface MovimentoEstoqueCotaRepository extends Repository<MovimentoEsto
     
 	List<ProdutoAbastecimentoDTO> obterDadosAbastecimentoBoxVersusCota(FiltroMapaAbastecimentoDTO filtro);
 	
-	public List<ProdutoAbastecimentoDTO> obterMapaDeImpressaoPorBoxVersusCotaQuebrandoPorCota(FiltroMapaAbastecimentoDTO filtro);
+	public List<AbastecimentoBoxCotaDTO> obterMapaDeImpressaoPorBoxVersusCotaQuebrandoPorCota(FiltroMapaAbastecimentoDTO filtro);
 }
