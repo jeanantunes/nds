6b55c313aadd2ed027bd512070dcf4fe2dcc5947
Sérgio Peres <sergiopersil@gmail.com>
2015-01-21 11:34:01 -0200

    Fix - Ajuste no pacote da classe PrecoReparte

--- nds-util/src/test/java/br/com/abril/nds/util/RoundingCurrencyUtilTest.java
+++ nds-util/src/test/java/br/com/abril/nds/util/RoundingCurrencyUtilTest.java
@@ -76,28 +76,4 @@ public class RoundingCurrencyUtilTest {
 		
 	}
 
-}
-
-
-class PrecoReparte {
-	
-	private BigDecimal precoDesconto;
-	
-	private BigDecimal quantidade;
-
-	public BigDecimal getPrecoDesconto() {
-		return precoDesconto;
-	}
-
-	public void setPrecoDesconto(BigDecimal precoDesconto) {
-		this.precoDesconto = precoDesconto;
-	}
-
-	public BigDecimal getQuantidade() {
-		return quantidade;
-	}
-
-	public void setQuantidade(BigDecimal quantidade) {
-		this.quantidade = quantidade;
-	}
 }	
