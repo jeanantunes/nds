ed5e8c36113897c69ed24680a9c9ff46e8942218
Washington Paiva <wrpaiva@gmail.com>
2015-01-22 12:00:43 -0200

    Nova funcionalidade do extratificador globo

---
+++ nds-integration-canonical/src/main/java/br/com/abril/nds/integracao/model/canonic/Extratificacao.java
@@ -0,0 +1,73 @@
+package br.com.abril.nds.integracao.model.canonic;
+
+import java.io.Serializable;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class Extratificacao implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String tipoDocumento;
+	
+	private String codigoDistribuidor;
+	
+	private Date dataCriacao;
+	
+	private Date dataOperacao;
+
+	private List<ExtratificacaoItem> itens;
+
+	public String getTipoDocumento() {
+		return tipoDocumento;
+	}
+
+	public void setTipoDocumento(String tipoDocumento) {
+		this.tipoDocumento = tipoDocumento;
+	}
+
+	public String getCodigoDistribuidor() {
+		return codigoDistribuidor;
+	}
+
+	public void setCodigoDistribuidor(String codigoDistribuidor) {
+		this.codigoDistribuidor = codigoDistribuidor;
+	}
+	
+	public Date getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(Date dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+	public Date getDataOperacao() {
+		return dataOperacao;
+	}
+
+	public void setDataOperacao(Date dataOperacao) {
+		this.dataOperacao = dataOperacao;
+	}
+	
+	public void setDataOperacao(String dataOperacao) {
+		SimpleDateFormat sdf = new SimpleDateFormat("MMM d, yyyy");
+		try {
+			this.dataOperacao = sdf.parse(dataOperacao);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public List<ExtratificacaoItem> getItens() {
+		return itens;
+	}
+
+	public void setItens(List<ExtratificacaoItem> itens) {
+		this.itens = itens;
+	}
+
+}
