67728228644ede260821880a9a55958d5f7c54ec
RomuloH <romulohpa@gmail.com>
2015-02-09 09:55:06 -0200

    correcao busca por cod barras, conf encalhe

--- nds-repository-client-impl/src/main/java/br/com/abril/nds/repository/impl/ConferenciaEncalheRepositoryImpl.java
+++ nds-repository-client-impl/src/main/java/br/com/abril/nds/repository/impl/ConferenciaEncalheRepositoryImpl.java
@@ -533,7 +533,6 @@ public class ConferenciaEncalheRepositoryImpl extends
 			.append(" inner join cec.cota cota					 		")
 			.append(" inner join ce.produtoEdicao produtoEdicao 		")
 			.append(" inner join produtoEdicao.produto produto			")
-			.append(" inner join produto.fornecedores fornecedor  		")
 			.append(" inner join produtoEdicao.lancamentos lancamentos 	")
 			
 			.append(" where ");
@@ -544,8 +543,12 @@ public class ConferenciaEncalheRepositoryImpl extends
 			
 			hql.append(" and upper(produtoEdicao.codigoDeBarras) like upper(:codigoBarras) ");
 			
-			hql.append(" and (ce.dataRecolhimento = :dataOperacao ");
-			hql.append(" 	or ce.dataRecolhimento in (:datasRecolhimentoValidas)) ");
+			
+			hql.append(" and ce.dataRecolhimento = :dataOperacao ");
+			
+			if(!datasRecolhimentoValidas.isEmpty()){
+				hql.append(" 	or ce.dataRecolhimento in (:datasRecolhimentoValidas)) ");
+			}
 			
 			hql.append(" group by produtoEdicao.id			")
 			   .append(" order by produto.nome asc,			")
@@ -561,7 +564,10 @@ public class ConferenciaEncalheRepositoryImpl extends
 		query.setParameter("numeroCota", numeroCota);
 		query.setParameter("lancamentoFechado", StatusLancamento.FECHADO);
 		query.setParameter("dataOperacao", dataOperacao);
-		query.setParameterList("datasRecolhimentoValidas", datasRecolhimentoValidas);
+		
+		if(!datasRecolhimentoValidas.isEmpty()){
+			query.setParameterList("datasRecolhimentoValidas", datasRecolhimentoValidas);
+		}
 		
 		query.setResultTransformer(Transformers.aliasToBean(ItemAutoComplete.class));
 		
