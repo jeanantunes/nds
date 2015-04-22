08696edab946c3ab274177c3e4427a214c9211ee
Sérgio Peres <sergiopersil@gmail.com>
2015-04-22 11:30:55 -0300

    NDS-39 - Ajuste na busca de forma de cobranca

--- nds-service-client-impl/src/main/java/br/com/abril/nds/service/impl/ConferenciaEncalheServiceImpl.java
+++ nds-service-client-impl/src/main/java/br/com/abril/nds/service/impl/ConferenciaEncalheServiceImpl.java
@@ -2153,8 +2153,7 @@ public class ConferenciaEncalheServiceImpl implements ConferenciaEncalheService 
 					cotaUnificada = this.cotaUnificacaoRepository.verificarCotaUnificadora(
 							controleConferenciaEncalheCota.getCota().getNumeroCota());
 			
-			if (!cotaUnificadora && !cotaUnificada){
-			
+			if (!cotaUnificadora && !cotaUnificada) {
 				
 				FormaCobranca fc = formaCobrancaService.obterFormaCobrancaCota(controleConferenciaEncalheCota.getCota().getId()
 										, null, controleConferenciaEncalheCota.getDataOperacao());
