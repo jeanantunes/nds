b885bc5ecab0991cbc61fe1fdc9a9f4e13400a1b
Washington Paiva <wrpaiva@gmail.com>
2015-04-22 09:23:37 -0300

    Ajuste na impressao nfe

---
+++ nfe-ws/src/main/java/br/com/abril/nfe/homologacao/NfeConsulta2.java
@@ -0,0 +1,18 @@
+/**
+ * NfeConsulta2.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package br.com.abril.nfe.homologacao;
+
+public interface NfeConsulta2 extends javax.xml.rpc.Service {
+
+/**
+ * Servi�o destinado ao atendimento de solicita��es de consulta da
+ * situa��o
+ * 			atual da NFC-e na Base de Dados do Portal da Secretaria de Fazenda
+ * 			Estadual.
+ */
+}
---
+++ nfe-ws/src/main/java/br/com/abril/nfe/homologacao/NfeConsulta2Locator.java
@@ -0,0 +1,69 @@
+/**
+ * NfeConsulta2Locator.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package br.com.abril.nfe.homologacao;
+
+public class NfeConsulta2Locator extends org.apache.axis.client.Service implements NfeConsulta2 {
+
+/**
+ * Servi�o destinado ao atendimento de solicita��es de consulta da
+ * situa��o
+ * 			atual da NFC-e na Base de Dados do Portal da Secretaria de Fazenda
+ * 			Estadual.
+ */
+
+    public NfeConsulta2Locator() {
+    }
+
+
+    public NfeConsulta2Locator(org.apache.axis.EngineConfiguration config) {
+        super(config);
+    }
+
+    public NfeConsulta2Locator(java.lang.String wsdlLoc, javax.xml.namespace.QName sName) throws javax.xml.rpc.ServiceException {
+        super(wsdlLoc, sName);
+    }
+    /**
+     * For the given interface, get the stub implementation.
+     * If this service has no port for the given interface,
+     * then ServiceException is thrown.
+     */
+    public java.rmi.Remote getPort(Class serviceEndpointInterface) throws javax.xml.rpc.ServiceException {
+        throw new javax.xml.rpc.ServiceException("There is no stub implementation for the interface:  " + (serviceEndpointInterface == null ? "null" : serviceEndpointInterface.getName()));
+    }
+
+    /**
+     * For the given interface, get the stub implementation.
+     * If this service has no port for the given interface,
+     * then ServiceException is thrown.
+     */
+    public java.rmi.Remote getPort(javax.xml.namespace.QName portName, Class serviceEndpointInterface) throws javax.xml.rpc.ServiceException {
+        if (portName == null) {
+            return getPort(serviceEndpointInterface);
+        }
+        java.lang.String inputPortName = portName.getLocalPart();
+         {
+            java.rmi.Remote _stub = getPort(serviceEndpointInterface);
+            ((org.apache.axis.client.Stub) _stub).setPortName(portName);
+            return _stub;
+        }
+    }
+
+    public javax.xml.namespace.QName getServiceName() {
+        return new javax.xml.namespace.QName("http://www.portalfiscal.inf.br/nfe/wsdl/NfeConsulta2", "NfeConsulta2");
+    }
+
+    private java.util.HashSet ports = null;
+
+    public java.util.Iterator getPorts() {
+        if (ports == null) {
+            ports = new java.util.HashSet();
+        }
+        return ports.iterator();
+    }
+
+}
