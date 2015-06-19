3a7241d45a0c654f8ec2877c14e86f60e9572bde
Washington Paiva <wrpaiva@gmail.com>
2015-06-19 17:37:45 -0300

    Ajuste no certificado por distribuidor.

--- nds-client/src/main/java/br/com/abril/nds/controllers/nfe/CertificadoNFEController.java
+++ nds-client/src/main/java/br/com/abril/nds/controllers/nfe/CertificadoNFEController.java
@@ -4,9 +4,11 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.abril.nds.client.annotation.Rules;
 import br.com.abril.nds.controllers.BaseController;
+import br.com.abril.nds.dto.CertificadoNFEDTO;
 import br.com.abril.nds.enums.TipoMensagem;
 import br.com.abril.nds.model.seguranca.Permissao;
 import br.com.abril.nds.vo.ValidacaoVO;
@@ -22,7 +24,6 @@ import br.com.caelum.vraptor.view.Results;
 @Rules(Permissao.ROLE_NFE_CERTICADO_NFE)
 public class CertificadoNFEController extends BaseController {
 	
-
 	@Autowired
 	private HttpServletResponse httpResponse;
 	
@@ -36,11 +37,12 @@ public class CertificadoNFEController extends BaseController {
 	@Rules(Permissao.ROLE_NFE_CERTICADO_NFE)
 	public void index() {	
 		
-	}
+	} 
 	
-	@Post("/confirmar.json")
-	@Rules(Permissao.ROLE_NFE_CERTICADO_NFE_ALTERACAO)
-	public void confirmar() {
+	@Post
+	@Transactional
+	public void confirmar(CertificadoNFEDTO filtro) {
+		
 		
 		this.result.use(Results.json()).from(new ValidacaoVO(TipoMensagem.SUCCESS, "Upload do certificado efetuado com sucesso."), "result").recursive().serialize();
 	}
