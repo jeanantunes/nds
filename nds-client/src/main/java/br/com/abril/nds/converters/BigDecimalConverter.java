901799e547869febaf21b4ef81d6ba9aef68c082
odemirolivatti <odemir.olivatti@gmail.com>
2015-08-20 08:43:15 -0300

    novos converters

---
+++ nds-client/src/main/java/br/com/abril/nds/converters/BigDecimalConverter.java
@@ -0,0 +1,68 @@
+package br.com.abril.nds.converters;
+
+import static com.google.common.base.Strings.isNullOrEmpty;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.MessageFormat;
+import java.text.NumberFormat;
+import java.util.ResourceBundle;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import br.com.caelum.vraptor.Convert;
+import br.com.caelum.vraptor.Converter;
+import br.com.caelum.vraptor.converter.ConversionError;
+import br.com.caelum.vraptor.ioc.ApplicationScoped;
+
+/**
+ * VRaptor's BigDecimal converter.
+ *
+ * @author Cecilia Fernandes
+ */
+@Convert(BigDecimal.class)
+@ApplicationScoped
+public class BigDecimalConverter implements Converter<BigDecimal>{
+
+	 private static final Logger LOGGER = LoggerFactory.getLogger(BigDecimalConverter.class);
+	public BigDecimal convert(String value, Class<? extends BigDecimal> type, ResourceBundle bundle) {
+		if (isNullOrEmpty(value)) {
+			return null;
+		}
+		if (isNullOrEmpty(value)) {
+			return null;
+		}
+		if ("null".equalsIgnoreCase(value)) {
+			return null;
+		}
+		if ( value.toLowerCase().startsWith("selecione")) {
+			return null;
+		}
+		if ("undefined".equalsIgnoreCase(value)) {
+			return null;
+		}
+
+		
+		try {
+			//return (BigDecimal) getNumberFormat().parse(value);
+			return new BigDecimal(value.replaceAll(",","."));
+		}   catch (Exception e ) {
+	    	e.printStackTrace();
+	    	LOGGER.error("ERRO CONVERSAO BIGDECIMAL='"+value+"'");
+	    	throw new ConversionError(MessageFormat.format(bundle.getString("is_not_a_valid_number"), value));    
+	    }
+	}
+		
+		protected NumberFormat getNumberFormat() {
+			DecimalFormatSymbols symbols = new DecimalFormatSymbols();
+			symbols.setGroupingSeparator('.');
+			symbols.setDecimalSeparator(',');
+			String pattern = "#.##0,0#";
+			DecimalFormat decimalFormat = new DecimalFormat(pattern, symbols);
+			return decimalFormat;
+		}
+
+	
+}
---
+++ nds-client/src/main/java/br/com/abril/nds/converters/BooleanConverter.java
@@ -0,0 +1,68 @@
+package br.com.abril.nds.converters;
+
+import static com.google.common.base.Strings.isNullOrEmpty;
+
+import java.text.MessageFormat;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import br.com.caelum.vraptor.Convert;
+import br.com.caelum.vraptor.Converter;
+import br.com.caelum.vraptor.converter.ConversionError;
+import br.com.caelum.vraptor.ioc.ApplicationScoped;
+
+
+@Convert(Boolean.class)
+@ApplicationScoped
+public class BooleanConverter implements Converter<Boolean> {
+	
+	 private static final Logger LOGGER = LoggerFactory.getLogger(BooleanConverter.class);
+	 
+	private static final Set<String> IS_TRUE  = new HashSet<String>(Arrays.asList("TRUE", "1", "YES", "Y", "ON"));
+	private static final Set<String> IS_FALSE = new HashSet<String>(Arrays.asList("FALSE", "0", "NO", "N", "OFF"));
+
+	public Boolean convert(String value, Class<? extends Boolean> type, ResourceBundle bundle) {
+	try {
+		
+		if (isNullOrEmpty(value)) {
+			return null;
+		}
+		if (isNullOrEmpty(value)) {
+			return null;
+		}
+		if ("null".equalsIgnoreCase(value)) {
+			return null;
+		}
+		if ( value.toLowerCase().startsWith("selecione")) {
+			return null;
+		}
+		if ("undefined".equalsIgnoreCase(value)) {
+			return null;
+		}
+		
+		value = value.toUpperCase();
+		
+		if (matches(IS_TRUE, value)) {
+			return true;
+		} else if (matches(IS_FALSE, value)) {
+			return false;
+		}
+		return false;
+	} catch (Exception e ) {
+    	e.printStackTrace();
+    	LOGGER.error("ERRO CONVERSAO Boolean='"+value+"'");
+    	throw new ConversionError(MessageFormat.format(bundle.getString("is_not_a_valid_boolean"), value));    
+    }	
+		
+		
+	}
+
+	private boolean matches(Set<String> words, String value) {
+		return words.contains(value);
+	}
+}
