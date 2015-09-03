5addaba881603deb5ce56967da7ac9b7de25a4bb
Washington Paiva <wrpaiva@gmail.com>
2015-09-03 17:49:44 -0300

    Ajuste no resumo da expedição para apresentar valor igual ao da capinha

--- nds-repository-client-impl/src/main/java/br/com/abril/nds/repository/impl/ExpedicaoRepositoryImpl.java
+++ nds-repository-client-impl/src/main/java/br/com/abril/nds/repository/impl/ExpedicaoRepositoryImpl.java
@@ -360,7 +360,7 @@ public class ExpedicaoRepositoryImpl extends AbstractRepositoryModel<Expedicao,L
 		     .append("     where d.produto_edicao_id = produtoEdicaoId 			 ")
 		     .append("     and d.STATUS_CONFIRMACAO <> 'CANCELADO' 			 	 ")
 		     .append("     and d.TIPO_DIRECIONAMENTO IN ('COTA', 'NOTA') ")
-		     // .append("     and l.DATA_LCTO_DISTRIBUIDOR = innerQuery.dataLancamento ")
+		     .append("     and l.DATA_LCTO_DISTRIBUIDOR = innerQuery.dataLancamento ")
 		     .append("     and rd.DATA_MOVIMENTO = innerQuery.dataLancamento ")
 		     // .append("     and tm.id not in (199, 200, 201, 202, 203) ")
 		     .append("     group by d.PRODUTO_EDICAO_ID ")
