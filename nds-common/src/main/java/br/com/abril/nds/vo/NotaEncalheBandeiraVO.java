4f986dd303bb35f93d9bd724797a098fe3d7dfe9
odemirolivatti <odemir.olivatti@gmail.com>
2015-09-18 19:03:12 -0300

    ajuste impressao bandeira

---
+++ nds-common/src/main/java/br/com/abril/nds/vo/NotaEncalheBandeiraVO.java
@@ -0,0 +1,80 @@
+/**
+ * NotaEncalheBandeiraVO.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package br.com.abril.nds.vo;
+
+
+public class NotaEncalheBandeiraVO  implements java.io.Serializable {
+
+	
+	private Integer notaId;
+	
+	public Integer getNotaId() {
+		  return this.notaId;
+	}
+	
+	public void setNotaId(Integer notaId ) {
+		this.notaId = notaId;
+	}
+
+    private  Integer tipoNota ;
+	public Integer getTipoNota() {
+		return tipoNota;
+	}
+
+	public void setTipoNota(Integer tipoNota) {
+		this.tipoNota = tipoNota;
+	}
+
+	private Integer numNota ;
+	public Integer getNumNota() {
+		return numNota;
+	}
+
+	public void setNumNota(Integer numNota) {
+		this.numNota = numNota;
+	}
+
+	private Integer codVolume;
+	public Integer getCodVolume() {
+		return codVolume;
+	}
+
+	public void setCodVolume(Integer codVolume) {
+		this.codVolume = codVolume;
+	}
+
+	private Integer qtdSacosPaletes ;
+	public Integer getQtdSacosPaletes() {
+		return qtdSacosPaletes;
+	}
+
+	public void setQtdSacosPaletes(Integer qtdSacosPaletes) {
+		this.qtdSacosPaletes = qtdSacosPaletes;
+	}
+
+	private String nomeDestinoEncalhe ;
+	
+    public String getNomeDestinoEncalhe() {
+		return nomeDestinoEncalhe;
+	}
+
+	public void setNomeDestinoEncalhe(String nomeDestinoEncalhe) {
+		this.nomeDestinoEncalhe = nomeDestinoEncalhe;
+	}
+
+	public NotaEncalheBandeiraVO() {
+    }
+
+
+
+    
+    
+   
+
+
+}
