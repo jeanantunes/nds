9e6f87b7005e1414fdb0a8563bcf9a8548580962
odemirolivatti <odemir.olivatti@gmail.com>
2015-09-23 15:53:36 -0300

    desfazendo alteracao que foi feito pull por engano

--- nds-integration-server/src/main/java/br/com/abril/nds/integracao/repository/ParametroSistemaRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.abril.nds.integracao.repository;
-
-import br.com.abril.nds.model.integracao.ParametroSistema;
-public interface ParametroSistemaRepository extends Repository<ParametroSistema, Long> {
-
-	String getParametro(String parametro);
-
-}
