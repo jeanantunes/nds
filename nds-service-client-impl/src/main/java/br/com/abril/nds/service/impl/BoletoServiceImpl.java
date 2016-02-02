434154ac6f25726ace9485b7e7e4a0838e68562a
Washington Paiva <wrpaiva@gmail.com>
2016-02-02 10:55:05 -0200

    Ajuste na baixa bancaria do boleto consolidado

--- nds-service-client-impl/src/main/java/br/com/abril/nds/service/impl/BoletoServiceImpl.java
+++ nds-service-client-impl/src/main/java/br/com/abril/nds/service/impl/BoletoServiceImpl.java
@@ -1243,22 +1243,18 @@ public class BoletoServiceImpl implements BoletoService {
         
         Divida dividaRaiz = null;
         
+        dividaRaiz = divida.getDividaRaiz();
+    	
         if (divida != null) {
+    		dividaRaiz.setStatus(StatusDivida.QUITADA);
+    		
+    		dividaRaiz.getCobranca().setStatusCobranca(StatusCobranca.PAGO);
+    		dividaRaiz.getCobranca().setDataPagamento(dataOperacao);
+    		
+    		dividaRepository.alterar(dividaRaiz);
+    	}
+
             
-            dividaRaiz = divida.getDividaRaiz();
-        }
-        
-        while (dividaRaiz != null) {
-            
-            dividaRaiz.setStatus(StatusDivida.QUITADA);
-            
-            dividaRaiz.getCobranca().setStatusCobranca(StatusCobranca.PAGO);
-            dividaRaiz.getCobranca().setDataPagamento(dataOperacao);
-            
-            dividaRepository.alterar(dividaRaiz);
-            
-            dividaRaiz = dividaRaiz.getDividaRaiz();
-        }
     }
     
     private MovimentoFinanceiroCotaDTO getMovimentoFinanceiroCotaDTO(final Cota cota,
