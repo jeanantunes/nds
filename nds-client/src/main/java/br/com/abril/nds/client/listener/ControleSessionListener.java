93d065f1930293391691f9ffacc770e7e08837fc
odemirolivatti <odemir.olivatti@gmail.com>
2016-02-10 09:45:07 -0200

    ajuste: mostrar mais informacoes na versao quando usuario admin

--- nds-client/src/main/java/br/com/abril/nds/client/listener/ControleSessionListener.java
+++ nds-client/src/main/java/br/com/abril/nds/client/listener/ControleSessionListener.java
@@ -1,20 +1,24 @@
 package br.com.abril.nds.client.listener;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
-import org.jfree.util.Log;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import br.com.abril.nds.client.component.BloqueioConferenciaEncalheComponent;
-import br.com.abril.nds.controllers.devolucao.ConferenciaEncalheController;
 import br.com.abril.nds.controllers.devolucao.MatrizRecolhimentoController;
 import br.com.abril.nds.controllers.distribuicao.HistogramaPosEstudoController;
 import br.com.abril.nds.controllers.lancamento.MatrizLancamentoController;
@@ -25,6 +29,8 @@ public class ControleSessionListener implements HttpSessionListener {
 	private static final Logger LOGGER = LoggerFactory.getLogger(ControleSessionListener.class);
 	
 	public static final String USUARIO_LOGADO="usuario_logado";
+	
+	public static Map <String,List> usuariosLogado=new HashMap();
 	
 	@Override
 	public void sessionCreated(HttpSessionEvent sessionEvent) {
@@ -46,6 +52,20 @@ public class ControleSessionListener implements HttpSessionListener {
 		removerTravaMatrizRecolhimento(session);
 		
 		removerTravaMatrizLancamento(session);
+		
+		  String usuario= this.getUsuario();
+	       
+	        if (usuario == null) {
+	            
+	       	 usuario = (String) session.getAttribute(USUARIO_LOGADO);
+	       }
+	       
+	       if (usuario != null) {
+	         
+	        	      usuariosLogado.remove(usuario);
+	        	   
+	        
+	      }
 		
 			
 		
