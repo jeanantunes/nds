a80e3dd4f98d46663c8272308882fc112fdd298c
odemirolivatti <odemir.olivatti@gmail.com>
2016-06-27 16:35:29 -0300

    ajuste:log da interface parcial

--- nds-integration-client/src/main/java/br/com/abril/nds/integracao/ems0136/processor/EMS0136MessageProcessor.java
+++ nds-integration-client/src/main/java/br/com/abril/nds/integracao/ems0136/processor/EMS0136MessageProcessor.java
@@ -620,7 +620,6 @@ public class EMS0136MessageProcessor extends AbstractRepository implements Messa
 		}
 	}
 	
-	
 	// Caso tenha lancamentos_parcial com mais de um periodo com tipo FINAL, ajustar  para ter PARCIAL e um FINAL
 	private void atualizarLancamentosComMaisdeUmPeriodoFinal() {
 		LOGGER.warn("ATUALIZANDO LANCAMENTOPARCIAL COM MAIS DE UM PERIODO COM TIPO FINAL");
