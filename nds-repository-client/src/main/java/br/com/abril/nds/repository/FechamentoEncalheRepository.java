a9bbbe23109cec6212f40f7b988ce6cc831285d1
Romulo <romulohpa@gmail.com>
2016-12-09 18:23:36 -0200

    ExtracaoCC_ajustesFinais

--- nds-repository-client/src/main/java/br/com/abril/nds/repository/FechamentoEncalheRepository.java
+++ nds-repository-client/src/main/java/br/com/abril/nds/repository/FechamentoEncalheRepository.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.hibernate.SQLQuery;
+
 import br.com.abril.nds.dto.AnaliticoEncalheDTO;
 import br.com.abril.nds.dto.CotaAusenteEncalheDTO;
 import br.com.abril.nds.dto.ExtracaoContaCorrenteDTO;
