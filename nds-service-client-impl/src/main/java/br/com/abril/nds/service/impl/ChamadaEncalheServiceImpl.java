626f4c09d16fba6d019c3469d27d0daf31207135
Washington <wrpaiva@gmail.com>
2017-03-15 09:54:27 -0300

    Ajuste no PDF de Emissão CE

--- nds-service-client-impl/src/main/java/br/com/abril/nds/service/impl/ChamadaEncalheServiceImpl.java
+++ nds-service-client-impl/src/main/java/br/com/abril/nds/service/impl/ChamadaEncalheServiceImpl.java
@@ -893,6 +893,8 @@ public class ChamadaEncalheServiceImpl implements ChamadaEncalheService {
 		
 		Date dataOperacaoDistribuidor = distribuidorService.obterDataOperacaoDistribuidor();
 		
+		Integer numeroSemana = distribuidorService.obterNumeroSemana(filtro.getDtRecolhimentoDe());
+		
 		final List<ChamadaEncalheImpressaoWrapper> listaCEWrapper = new ArrayList<ChamadaEncalheImpressaoWrapper>();
 		
 		List<ChamadaEncalheImpressaoWrapper> listaCEWrapperImpressaoEemail = new ArrayList<ChamadaEncalheImpressaoWrapper>();
@@ -945,6 +947,7 @@ public class ChamadaEncalheServiceImpl implements ChamadaEncalheService {
 				}
 			}
 			
+			dto.setNumeroSemana(numeroSemana);
 			dto.setEmissorNome(distribuidor.getRazaoSocial());
 			dto.setEmissorCNPJ(distribuidor.getCnpj());
 			dto.setEmissorInscricaoEstadual(distribuidor.getInscricaoEstatual());
