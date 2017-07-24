5a91ec91d4488712a33f7539b3baef5971eac8ad
Romulo <romulohpa@gmail.com>
2017-07-24 20:11:36 -0300

    ajuste economia papel

--- nds-client/src/main/java/br/com/abril/nds/controllers/lancamento/MatrizLancamentoController.java
+++ nds-client/src/main/java/br/com/abril/nds/controllers/lancamento/MatrizLancamentoController.java
@@ -53,9 +53,9 @@ import br.com.abril.nds.util.CurrencyUtil;
 import br.com.abril.nds.util.DateUtil;
 import br.com.abril.nds.util.TableModel;
 import br.com.abril.nds.util.export.Export;
+import br.com.abril.nds.util.export.Export.Alignment;
 import br.com.abril.nds.util.export.Exportable;
 import br.com.abril.nds.util.export.FileExporter;
-import br.com.abril.nds.util.export.Export.Alignment;
 import br.com.abril.nds.util.export.FileExporter.FileType;
 import br.com.abril.nds.vo.ConfirmacaoVO;
 import br.com.abril.nds.vo.PaginacaoVO;
@@ -1497,8 +1497,8 @@ public class MatrizLancamentoController extends BaseController {
         
         if (listaProdutoBalanceamento != null && !listaProdutoBalanceamento.isEmpty()) {
             
-//            final Double valorTotal = getValorTotal(listaProdutoBalanceamento);
-            
+        	filtro.setValorTotal(CurrencyUtil.formatarValor(getValorTotal(listaProdutoBalanceamento)));
+        	
             final List<ProdutoLancamentoVO> listaProdutoBalanceamentoVO = getProdutosLancamentoVO(listaProdutoBalanceamento);
             
 //            final RodapeDTO rodape = new RodapeDTO(CurrencyUtil.formatarValor(valorTotal));
