7dacdd334b42f8ff182e958a44ab87abd0e90adc
Romulo <romulohpa@gmail.com>
2017-12-06 17:15:22 -0200

    ajustes mudar base analise estudo parcial e normal

--- nds-client/src/main/java/br/com/abril/nds/controllers/distribuicao/AnaliseParcialController.java
+++ nds-client/src/main/java/br/com/abril/nds/controllers/distribuicao/AnaliseParcialController.java
@@ -295,6 +295,37 @@ public class AnaliseParcialController extends BaseController {
                      Long faixaDe, Long faixaAte, List<EdicoesProdutosDTO> edicoesBase, String modoAnalise, String codigoProduto, Long numeroEdicao, 
                      String numeroCotaStr,Long estudoOrigem,String dataLancamentoEdicao, Integer numeroParcial, String cotasFiltro, boolean isMudarBaseVisualizacao) {
     	
+    	
+    	
+    	if(edicoesBase != null){
+    		List<EdicoesProdutosDTO> edicoesValidadas = new ArrayList<>();
+    		List<EdicoesProdutosDTO> edicoesParaRemocao = new ArrayList<>();
+    		
+    		for (EdicoesProdutosDTO edicao : edicoesBase) {
+    			
+    			if(edicoesValidadas.isEmpty()){
+    				edicoesValidadas.add(edicao);
+    				continue;
+    			}
+    			
+    			for (EdicoesProdutosDTO edicaoValidada : edicoesValidadas) {
+    				if(edicao.getCodigoProduto().equals(edicaoValidada.getCodigoProduto()) &&
+    						edicao.getEdicao().equals(edicaoValidada.getEdicao())){
+    					if(edicao.isParcial()){
+    						if(edicao.getPeriodo().equals(edicaoValidada.getPeriodo())){
+    							edicoesParaRemocao.add(edicao);
+	    					}
+    					}else{
+    						edicoesParaRemocao.add(edicao);
+    					}
+    				}
+    			}
+    			edicoesValidadas.add(edicao);
+    		}
+    		edicoesValidadas.removeAll(edicoesParaRemocao);
+    		edicoesBase = edicoesValidadas;	
+		}
+    	
         AnaliseParcialQueryDTO filtroQueryDTO = new AnaliseParcialQueryDTO();
         filtroQueryDTO.setSortName(sortname);
         filtroQueryDTO.setSortOrder(sortorder);
