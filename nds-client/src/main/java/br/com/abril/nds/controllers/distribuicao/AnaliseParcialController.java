30af3f319081080bae697132bcd820376f77cbe5
Romulo <romulohpa@gmail.com>
2017-12-06 17:33:54 -0200

    ajustes mudar base analise estudo parcial e normal

--- nds-client/src/main/java/br/com/abril/nds/controllers/distribuicao/AnaliseParcialController.java
+++ nds-client/src/main/java/br/com/abril/nds/controllers/distribuicao/AnaliseParcialController.java
@@ -297,34 +297,7 @@ public class AnaliseParcialController extends BaseController {
     	
     	
     	
-    	if(edicoesBase != null){
-    		List<EdicoesProdutosDTO> edicoesValidadas = new ArrayList<>();
-    		List<EdicoesProdutosDTO> edicoesParaRemocao = new ArrayList<>();
-    		
-    		for (EdicoesProdutosDTO edicao : edicoesBase) {
-    			
-    			if(edicoesValidadas.isEmpty()){
-    				edicoesValidadas.add(edicao);
-    				continue;
-    			}
-    			
-    			for (EdicoesProdutosDTO edicaoValidada : edicoesValidadas) {
-    				if(edicao.getCodigoProduto().equals(edicaoValidada.getCodigoProduto()) &&
-    						edicao.getEdicao().equals(edicaoValidada.getEdicao())){
-    					if(edicao.isParcial()){
-    						if(edicao.getPeriodo().equals(edicaoValidada.getPeriodo())){
-    							edicoesParaRemocao.add(edicao);
-	    					}
-    					}else{
-    						edicoesParaRemocao.add(edicao);
-    					}
-    				}
-    			}
-    			edicoesValidadas.add(edicao);
-    		}
-    		edicoesValidadas.removeAll(edicoesParaRemocao);
-    		edicoesBase = edicoesValidadas;	
-		}
+    	edicoesBase = validarEdicoesBases(edicoesBase);
     	
         AnaliseParcialQueryDTO filtroQueryDTO = new AnaliseParcialQueryDTO();
         filtroQueryDTO.setSortName(sortname);
@@ -414,6 +387,40 @@ public class AnaliseParcialController extends BaseController {
     	
     	result.use(Results.json()).from(vo).recursive().serialize();
     }
+
+	private List<EdicoesProdutosDTO> validarEdicoesBases(List<EdicoesProdutosDTO> edicoesBase) {
+		
+		if(edicoesBase != null){
+    		List<EdicoesProdutosDTO> edicoesValidadas = new ArrayList<>();
+    		List<EdicoesProdutosDTO> edicoesParaRemocao = new ArrayList<>();
+    		
+    		for (EdicoesProdutosDTO edicao : edicoesBase) {
+    			
+    			if(edicoesValidadas.isEmpty()){
+    				edicoesValidadas.add(edicao);
+    				continue;
+    			}
+    			
+    			for (EdicoesProdutosDTO edicaoValidada : edicoesValidadas) {
+    				if(edicao.getCodigoProduto().equals(edicaoValidada.getCodigoProduto()) &&
+    						edicao.getEdicao().equals(edicaoValidada.getEdicao())){
+    					if(edicao.isParcial()){
+    						if(edicao.getPeriodo().equals(edicaoValidada.getPeriodo())){
+    							edicoesParaRemocao.add(edicao);
+	    					}
+    					}else{
+    						edicoesParaRemocao.add(edicao);
+    					}
+    				}
+    			}
+    			edicoesValidadas.add(edicao);
+    		}
+    		edicoesValidadas.removeAll(edicoesParaRemocao);
+    		edicoesBase = edicoesValidadas;	
+		}
+		
+		return edicoesBase;
+	}
 
 	private int verificarPaginacaoComFiltro(int page, String filterSortName, Double filterSortFrom, Double filterSortTo) {
 		if(session.getAttribute("filtrarPor") != null){
